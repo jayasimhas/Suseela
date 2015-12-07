958844381ba9415dc83021ab4682e4a791477d4f
distaula <distaula@gmail.com>
2015-12-07 13:40:51 -0500

    Update readme, sample views.

--- web/Website/Controllers/AccountController.cs
+++ web/Website/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace Informa.Web.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToLocal("/");
         }
 
         //
