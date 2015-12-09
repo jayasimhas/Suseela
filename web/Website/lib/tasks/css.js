56e0a435e177450d214accae4d74275b0e09c415
Samuel Fine <samuel.fine@velir.com>
2015-12-09 09:14:25 -0500

    Auto-prefixing for IE 10+, per project specs

--- web/Website/lib/tasks/css.js
+++ web/Website/lib/tasks/css.js
@@ -24,7 +24,7 @@ utils.setTaskConfig("css", {
         },
 
         autoprefixer: {
-            browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "ie >= 9"]
+            browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1", "ie >= 10"]
         }
     },
 
