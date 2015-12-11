a2a77c9640e441d20af62b2273e7e7bc8d24165b
Samuel Fine <samuel.fine@velir.com>
2015-12-11 09:24:52 -0500

    Better header/menu responsiveness

--- web/Website/js/informa.js
+++ web/Website/js/informa.js
@@ -2,4 +2,5 @@ import './vendor';
 
 $('.main-menu__toggler').on('click', function toggleMenu() {
 	$('.main-menu').toggleClass('is-active');
+	$('.main-menu__toggler').toggleClass('is-active');
 });
