beadf128094de3967a2c63fb6dc6ef1d2c66e3e2
Samuel Fine <samuel.fine@velir.com>
2015-12-16 11:48:06 -0500

    BROKEN — Menu + style updates WIP, saving work

--- web/Website/local.js
+++ web/Website/local.js
@@ -6,7 +6,7 @@ module.exports = {
     // hostname to be used with browserSync for proxy.
     // http://www.browsersync.io/docs/options/#option-proxy
     // omit or set to false for static sites
-    //"hostname": "informa.sfine.velir.com",
+    "hostname": "informa.sfine.velir.com",
 
     // port that browsersync will run on
     // http://www.browsersync.io/docs/options/#option-port
