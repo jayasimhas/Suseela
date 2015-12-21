ece4cf8f5045479892e0a0eacfe68cdf12dc311a
Samuel Fine <samuel.fine@velir.com>
2015-12-21 16:13:22 -0500

    Temporary twekas to JS asset pipeline. Will clean this up soon.

--- web/Website/js/informa.js
+++ web/Website/js/informa.js
@@ -1,22 +1,22 @@
-import './vendor';
+// TODO include this file individually to prevent one giant file
+// TODO note that this is Zepto, not jQuery
 
 /* Toggle menu visibility */
-$('.main-menu__toggler').on('click', function toggleMenu() {
+$('.js-toggle-menu').on('click', function toggleMenu() {
 	$('.main-menu').toggleClass('is-active');
-	$('.main-menu__toggler').toggleClass('is-active');
+	$('.menu-toggler').toggleClass('is-active');
 });
 
 /* Toggle menu categories */
 $('.main-menu__section-title').on('click', function toggleMenuItems(e) {
- 	console.log(e);
 	$(e.target).toggleClass('is-active');
 });
 
 /* Attach / detach sticky menu */
 $(window).on('scroll', function windowScrolled() {
 	if ($(this).scrollTop() > 100) {
-		$('.header__wrapper .main-menu__toggler').addClass('is-sticky');
+		$('.header__wrapper .menu-toggler').addClass('is-sticky');
 	} else {
-		$('.header__wrapper .main-menu__toggler').removeClass('is-sticky');
-	}  
+		$('.header__wrapper .menu-toggler').removeClass('is-sticky');
+	}
 });
