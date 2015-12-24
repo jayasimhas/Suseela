2b7fa0cd9f87350c3df983668b937ab470762da1
distaula <distaula@gmail.com>
2015-12-24 12:41:15 -0500

    Added in deployment projects.

---
+++ WordPlugin/WordPluginCustomInstallActions/Installer1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+
+namespace WordPluginCustomInstallActions
+{
+	[RunInstaller(true)]
+	public partial class Installer1 : System.Configuration.Install.Installer
+	{
+		public Installer1()
+		{
+			InitializeComponent();
+
+			_registerer = new RegisterOffice2007AddIn();
+		}
+
+		[System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+		public override void Install(IDictionary stateSaver)
+		{
+			_registerer.RegisterAddIn(_addinName);
+			base.Install(stateSaver);
+		}
+
+		[System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+		public override void Commit(IDictionary savedState)
+		{
+			base.Commit(savedState);
+		}
+
+		[System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+		public override void Rollback(IDictionary savedState)
+		{
+			base.Rollback(savedState);
+		}
+
+		[System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+		public override void Uninstall(IDictionary savedState)
+		{
+			_registerer.UnRegisterAddIn("word", _addinName);
+			base.Uninstall(savedState);
+		}
+
+		protected RegisterOffice2007AddIn _registerer;
+		protected readonly string _addinName = "InsightPlatform.WordPlugin";
+	}
+}
