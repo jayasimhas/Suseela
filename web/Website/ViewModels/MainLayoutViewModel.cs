00e81802494c21ed90f6c84e81c2fcce26c08671
Jason Bert <jason.bert@goblinrockets.com>
2016-01-12 10:34:56 +0000

    Implemented basic elements of site footer

--- web/Website/ViewModels/MainLayoutViewModel.cs
+++ web/Website/ViewModels/MainLayoutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Informa.Library.Globalization;
+﻿using Glass.Mapper.Sc.Fields;
+using Informa.Library.Globalization;
 using Informa.Library.Site;
 using Jabberwocky.Glass.Autofac.Mvc.Models;
 using Jabberwocky.Glass.Models;
@@ -7,13 +8,16 @@ namespace Informa.Web.ViewModels
 {
 	public class MainLayoutViewModel : GlassViewModel<IGlassBase>
 	{
+		protected readonly ISiteRootContext SiteRootContext;
 		protected readonly ISiteMaintenanceContext SiteMaintenanceContext;
 		protected readonly ITextTranslator TextTranslator;
 
 		public MainLayoutViewModel(
+			ISiteRootContext siteRootContext,
 			ISiteMaintenanceContext siteMaintenanceContext,
 			ITextTranslator textTranslator)
 		{
+			SiteRootContext = siteRootContext;
 			SiteMaintenanceContext = siteMaintenanceContext;
 			TextTranslator = textTranslator;
 		}
@@ -33,5 +37,27 @@ namespace Informa.Web.ViewModels
 				};
 			}
 		}
+
+		public string FooterLogoUrl => SiteRootContext.Item == null ? string.Empty : SiteRootContext.Item.Footer_Logo.Src;
+
+		public string CopyrightText => SiteRootContext.Item == null ? string.Empty : SiteRootContext.Item.Copyright_Text;
+
+		// Subscribe Link - business logic
+
+		// Local footer links
+
+		public string FollowText => TextTranslator.Translate("FooterFollow");
+
+		public Link LinkedInLink => SiteRootContext.Item == null ? null : SiteRootContext.Item.LinkedIn_Link;
+
+		public Link FacebookLink => SiteRootContext.Item == null ? null : SiteRootContext.Item.Facebook_Link;
+
+		public Link TwitterLink => SiteRootContext.Item == null ? null : SiteRootContext.Item.Twitter_Link;
+
+		public string MenuOneHeader => SiteRootContext.Item == null ? string.Empty : SiteRootContext.Item.Menu_One_Header;
+
+		// Menu one links
+
+		public string MenuTwoHeader => SiteRootContext.Item == null ? string.Empty : SiteRootContext.Item.Menu_Two_Header;
 	}
 }
