26c69a455f9511d7e15c5d8e007ab8b903fa05e2
Aakash Shah <aakash.shah@velir.com>
2016-01-14 13:38:18 -0500

    Adding IArticle Interface class

---
+++ src/Informa.Models/GlassModels/IArticle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages
+{
+	partial interface IArticle
+	{
+		[SitecoreParent]
+		[SitecoreQuery("./ancestor-or-self::*[@@templateid='{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}']", IsRelative = true)]
+		Guid Publication { get; set; }
+	}
+}
