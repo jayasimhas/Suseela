ad213a9ac04fe572f4eb07d12279c9e78f7a3700
Aakash Shah <aakash.shah@velir.com>
2016-01-15 10:26:27 -0500

    Fixing the error on View Models

--- src/Informa.Models/GlassModels/IArticle.cs
+++ src/Informa.Models/GlassModels/IArticle.cs
@@ -9,7 +9,7 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages
 {
 	partial interface IArticle
 	{
-		[SitecoreParent]
+		
 		[SitecoreQuery("./ancestor-or-self::*[@@templateid='{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}']", IsRelative = true)]
 		Guid Publication { get; set; }
 	}
