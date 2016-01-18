aba0124698d1384914ec38756562001a93e2358a
Jason Bert <jason.bert@goblinrockets.com>
2016-01-18 14:21:12 +0000

    Implemented page meta title

---
+++ src/Informa.Library/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Informa.Library.Utilities.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string StripHtml(this string source)
+		{
+			return Regex.Replace(source, "<[^>]*>", "", RegexOptions.Compiled).Replace("  ", " ");
+		}
+	}
+}
--- web/Website/ViewModels/MainLayoutViewModel.cs
+++ web/Website/ViewModels/MainLayoutViewModel.cs
@@ -1,15 +1,22 @@
-﻿using Jabberwocky.Glass.Autofac.Mvc.Models;
+﻿using Informa.Library.Site;
+using Informa.Library.Utilities.Extensions;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Base_Templates;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
 using Jabberwocky.Glass.Models;
 
 namespace Informa.Web.ViewModels
 {
 	public class MainLayoutViewModel : GlassViewModel<IGlassBase>
 	{
+		protected readonly ISiteRootContext SiteRootContext;
+
 		public MainLayoutViewModel(
+			ISiteRootContext siteRootContext,
 			IMaintenanceViewModel maintenanceViewModel,
 			ISideNavigationMenuViewModel sideNavigationMenuViewModel,
 			IFooterViewModel footerViewModel)
 		{
+			SiteRootContext = siteRootContext;
 			MaintenanceMessage = maintenanceViewModel;
 			SideNavigationMenu = sideNavigationMenuViewModel;
 			Footer = footerViewModel;
@@ -21,6 +28,33 @@ namespace Informa.Web.ViewModels
 
 		public IFooterViewModel Footer;
 
-		public string Title => "Site Title";
+		public string Title
+		{
+			get
+			{
+				var pageTitle = string.Empty;
+
+				if (GlassModel is I___BasePage)
+				{
+					var page = (I___BasePage)GlassModel;
+
+					if (!string.IsNullOrEmpty(page.Meta_Title_Override))
+					{
+						return page.Meta_Title_Override.StripHtml();
+					}
+
+					pageTitle = page.Title.StripHtml();
+				}
+				
+				if (string.IsNullOrWhiteSpace(pageTitle))
+				{
+					pageTitle = GlassModel._Name;
+				}
+
+				var publicationName = SiteRootContext.Item == null ? string.Empty : string.Format(" :: {0}", SiteRootContext.Item.Publication_Name.StripHtml());
+
+				return string.Concat(pageTitle, publicationName);
+			}
+		}
 	}
 }
