4c50fa263bb694c3d738cfd0f3ae63daabbf8d0d
Jason Bert <jason.bert@goblinrockets.com>
2016-01-19 10:47:04 +0000

    Moved hard coded text to view model

--- web/Website/ViewModels/HeaderViewModel.cs
+++ web/Website/ViewModels/HeaderViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Informa.Library.Site;
 using Jabberwocky.Glass.Autofac.Attributes;
+using System.Web;
 
 namespace Informa.Web.ViewModels
 {
@@ -20,5 +21,28 @@ namespace Informa.Web.ViewModels
 		public string LogoImageUrl => SiteRootContext.Item == null ? string.Empty : SiteRootContext.Item.Site_Logo.Src;
 
 		public string LogoUrl => SiteHomeContext.Item == null ? string.Empty : SiteHomeContext.Item._Url;
+
+		public string WelcomeText => string.Concat("Hi, ", "#Company or Account Name#");
+		public bool IsAuthenticated => false;
+		public string MyAccountLinkText => "My Account";
+		public string SignOutLinkText => "Sign Out";
+		public string RegisterText => "Register";
+		public string RegisterLinkText => "Register";
+		public string RegisterButtonText => "Register";
+		public string UsernamePlaceholderText => "Email Address (User Name)";
+		public string UsernameInvalidText => "Invalid Email Address (User Name)";
+		public string SignInText => "Sign In";
+		public string SignInLinkText => "Sign In";
+		public string SignInButtonText => "Sign In";
+		public string SignInInvalidText => "Your login and/or password information does not match our records. Please try again.";
+		public string PasswordPlaceholderText => "Password";
+		public string RememberMeText => "Remember me";
+		public string ForgotPasswordText => "Forgot your password?";
+		public string ForgotPasswordLinkText => "Forgot your password?";
+		public string ForgotPasswordHelpText => "Enter the email address associated with your account and a temporary password will be sent to you.";
+		public string ForgotPasswordButtonText => "Submit";
+		public string ForgotPasswordConfirmationText => "Thanks, we've sent a link to reset your password";
+		public IHtmlString ForgotPasswordContactText => new HtmlString("Need help? Contact us at <b>(800) 332-2181</b>, <b>+1 (908) 748-1221</b>, or <a href=\"#\">custcare@informa.com</a>");
+		public string EmailPlaceholderText => "Email Address";
 	}
 }
--- web/Website/ViewModels/IHeaderViewModel.cs
+++ web/Website/ViewModels/IHeaderViewModel.cs
@@ -1,8 +1,34 @@
-﻿namespace Informa.Web.ViewModels
+﻿using System.Web;
+
+namespace Informa.Web.ViewModels
 {
 	public interface IHeaderViewModel
 	{
 		string LogoImageUrl { get; }
 		string LogoUrl { get; }
+
+		// Account/Sign In/Registration
+		string WelcomeText { get; }
+		bool IsAuthenticated { get; }
+		string MyAccountLinkText { get; }
+		string SignOutLinkText { get; }
+		string RegisterText { get; }
+		string RegisterLinkText { get; }
+		string RegisterButtonText { get; }
+		string UsernamePlaceholderText { get; }
+		string UsernameInvalidText { get; }
+		string SignInText { get; }
+		string SignInLinkText { get; }
+		string SignInButtonText { get; }
+		string SignInInvalidText { get; }
+		string PasswordPlaceholderText { get; }
+		string RememberMeText { get; }
+		string ForgotPasswordText { get; }
+		string ForgotPasswordLinkText { get; }
+		string ForgotPasswordHelpText { get; }
+		string ForgotPasswordButtonText { get; }
+		string ForgotPasswordConfirmationText { get; }
+		IHtmlString ForgotPasswordContactText { get; }
+		string EmailPlaceholderText { get; }
 	}
 }
