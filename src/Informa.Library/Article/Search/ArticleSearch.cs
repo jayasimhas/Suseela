1b45ab6a72272eb00a199ce072aebda3175aafb2
Jason Bert <jason.bert@goblinrockets.com>
2016-01-29 15:02:53 +0000

    Re-factored creation of search context to simple factory pattern

--- src/Informa.Library/Article/Search/ArticleSearch.cs
+++ src/Informa.Library/Article/Search/ArticleSearch.cs
@@ -1,24 +1,27 @@
 ﻿using System.Linq;
 using Glass.Mapper.Sc;
 using Jabberwocky.Glass.Autofac.Attributes;
-using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.Linq;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 using Informa.Library.ContentCuration.Search.Extensions;
 using Informa.Library.Search.Extensions;
 using System.Collections.Generic;
 using System;
+using Informa.Library.Search;
 
 namespace Informa.Library.Article.Search
 {
 	[AutowireService(LifetimeScope.SingleInstance)]
 	public class ArticleSearch : IArticleSearch
 	{
+		protected readonly IProviderSearchContextFactory SearchContextFactory;
 		protected readonly ISitecoreService SitecoreContext;
 
 		public ArticleSearch(
+			IProviderSearchContextFactory searchContextFactory,
 			ISitecoreService sitecoreContext)
 		{
+			SearchContextFactory = searchContextFactory;
 			SitecoreContext = sitecoreContext;
 		}
 
@@ -33,9 +36,7 @@ namespace Informa.Library.Article.Search
 
 		public IArticleSearchResults Search(IArticleSearchFilter filter)
 		{
-			var index = ContentSearchManager.GetIndex("sitecore_web_index");
-
-			using (var context = index.CreateSearchContext())
+			using (var context = SearchContextFactory.Create())
 			{
 				var query = context.GetQueryable<ArticleSearchResultItem>()
 					.Filter(i => i.TemplateId == IArticleConstants.TemplateId)
---
+++ src/Informa.Library/Search/ProviderSearchContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore;
+using Jabberwocky.Glass.Autofac.Attributes;
+
+namespace Informa.Library.Search
+{
+	[AutowireService(LifetimeScope.SingleInstance)]
+	public class ProviderSearchContextFactory : IProviderSearchContextFactory
+	{
+		public IProviderSearchContext Create()
+		{
+			var indexName = string.Format("sitecore_{0}_index", Context.Database.Name.ToLower());
+
+			return ContentSearchManager.GetIndex(indexName).CreateSearchContext();
+		}
+	}
+}
