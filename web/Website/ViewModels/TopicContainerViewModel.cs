26d6ecee99aac2508aa4c0db0f960094b05301e6
Jason Bert <jason.bert@goblinrockets.com>
2016-02-01 09:58:12 +0000

    Created static view rendering for featured article full width; renamed several Sitecore item folders named "UserDefined" to "User Defined"; Added view model for top container rendering

---
+++ web/Website/ViewModels/TopicContainerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Informa.Models.FactoryInterface;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
+using Jabberwocky.Glass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.ViewModels
+{
+	public class TopicContainerViewModel : GlassViewModel<IGlassBase>
+	{
+		public TopicContainerViewModel(
+			)
+		{
+
+		}
+
+		public string Title => TestData.TestData.GetRandomTopic().LinkableText;
+		public ILinkable Link => new LinkableModel
+		{
+			LinkableText = "Explore this Topic",
+			LinkableUrl = "#"
+		};
+	}
+}
