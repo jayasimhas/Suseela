d792358585d28d539dcff67a8c5f4657723ddf01
Jason Bert <jason.bert@goblinrockets.com>
2016-02-04 15:12:17 +0000

    Implemented item save event for scheduled publish

--- src/Informa.Library/Publishing/Scheduled/UpdateItemScheduledPublishing.cs
+++ src/Informa.Library/Publishing/Scheduled/UpdateItemScheduledPublishing.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using Sitecore.Data.Items;
 using Jabberwocky.Glass.Autofac.Attributes;
+using Sitecore.Data.Fields;
 
 namespace Informa.Library.Publishing.Scheduled
 {
-	[AutowireService(LifetimeScope.SingleInstance)]
+	[AutowireService(LifetimeScope.Default)]
 	public class UpdateItemScheduledPublishing : IUpdateItemScheduledPublishing
 	{
+		private const string ScheduledPublishingEnabledFieldName = "Scheduled Publishing Enabled";
+
 		protected readonly IItemScheduledPublishFactory ItemScheduledPublishingFactory;
 		protected readonly IUpsertScheduledPublishes UpdateScheduledPublishes;
 
@@ -19,9 +22,19 @@ namespace Informa.Library.Publishing.Scheduled
 
 		public void Update(Item item)
 		{
+			if (Ignore(item))
+			{
+				return;
+			}
+
 			var scheduledPublish = ItemScheduledPublishingFactory.Create(item);
 
 			UpdateScheduledPublishes.Upsert(new List<IScheduledPublish> { { scheduledPublish } });
+		}
+
+		public bool Ignore(Item item)
+		{
+			return item == null || item.Fields[ScheduledPublishingEnabledFieldName] == null || !((CheckboxField)item.Fields[ScheduledPublishingEnabledFieldName]).Checked;
 		}
 	}
 }
