02234d919215a5f4535feba0a7eb0051b8ef3599
Jason Bert <jason.bert@goblinrockets.com>
2016-02-05 13:33:24 +0000

    Implemented deletion of scheduled published on item deleted

---
+++ src/Informa.Library/Publishing/Scheduled/ItemDeleteScheduledPublises.cs
@@ -0,0 +1,22 @@
+﻿using Jabberwocky.Glass.Autofac.Attributes;
+using Sitecore.Data.Items;
+
+namespace Informa.Library.Publishing.Scheduled
+{
+	[AutowireService(LifetimeScope.Default)]
+	public class ItemDeleteScheduledPublises : IItemDeleteScheduledPublises
+	{
+		protected readonly IDeleteItemScheduledPublishes DeleteScheduledPublishes;
+
+		public ItemDeleteScheduledPublises(
+			IDeleteItemScheduledPublishes deleteScheduledPublishes)
+		{
+			DeleteScheduledPublishes = deleteScheduledPublishes;
+		}
+
+		public void Delete(Item item)
+		{
+			DeleteScheduledPublishes.Delete(item.ID.Guid);
+		}
+	}
+}
---
+++ src/Informa.Library/Publishing/Scheduled/MongoDB/MongoDbDeleteItemScheduledPublishes.cs
@@ -0,0 +1,23 @@
+﻿using Jabberwocky.Glass.Autofac.Attributes;
+using MongoDB.Driver.Builders;
+using System;
+
+namespace Informa.Library.Publishing.Scheduled.MongoDB
+{
+	[AutowireService(LifetimeScope.SingleInstance)]
+	public class MongoDbDeleteItemScheduledPublishes : IDeleteItemScheduledPublishes
+	{
+		protected readonly IMongoDbScheduledPublishContext ScheduledPublishContext;
+
+		public MongoDbDeleteItemScheduledPublishes(
+			IMongoDbScheduledPublishContext scheduledPublishContext)
+		{
+			ScheduledPublishContext = scheduledPublishContext;
+		}
+
+		public void Delete(Guid itemId)
+		{
+			ScheduledPublishContext.ScheduledPublishes.Remove(Query<ScheduledPublishDocument>.Where(spd => spd.ItemId == itemId));
+		}
+	}
+}
