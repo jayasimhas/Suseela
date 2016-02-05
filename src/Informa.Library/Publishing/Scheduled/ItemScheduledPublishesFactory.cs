39bb7e5dabf85d2e3b8e6f6a0f5efd31496c3877
Jason Bert <jason.bert@goblinrockets.com>
2016-02-05 11:05:48 +0000

    First pass of creating scheduled publishes for an item

--- src/Informa.Library/Publishing/Scheduled/ItemScheduledPublishesFactory.cs
+++ src/Informa.Library/Publishing/Scheduled/ItemScheduledPublishesFactory.cs
@@ -1,7 +1,8 @@
 ﻿using Sitecore.Data.Items;
 using Jabberwocky.Glass.Autofac.Attributes;
 using System.Collections.Generic;
-using System.Linq;
+using System;
+using Sitecore.Data.Fields;
 
 namespace Informa.Library.Publishing.Scheduled
 {
@@ -10,15 +11,74 @@ namespace Informa.Library.Publishing.Scheduled
 	{
 		public IEnumerable<IScheduledPublish> Create(Item item)
 		{
-			// TODO: Create object based on item information
+			var scheduledPublishes = new List<ScheduledPublish>();
+			var itemId = item.ID.Guid;
+			var language = item.Language.Name;
+			var version = item.Version.Number.ToString();
 
-			//*Update
-			//	* Planned Publish Date -> ID, Language, Version
-			//	* Publishing Restrictions
-			//		* Item -> ID
-			//		* Version -> ID, Language, Version
+			// Planned Publish Date -> ID, Language, Version
+			var plannedPublishDateField = (DateField)item.Fields["Planned Publish Date"];
 
-			return Enumerable.Empty<IScheduledPublish>();
+			if (plannedPublishDateField != null)
+			{
+				var plannedPublishhDate = plannedPublishDateField.DateTime;
+
+				if (HasValidValue(plannedPublishhDate))
+				{
+					scheduledPublishes.Add(CreateScheduledPublish(itemId, string.Empty, string.Empty, plannedPublishhDate));
+				}
+			}
+
+			// Publishing Restrictions
+			// Item -> ID
+			// From
+			var publishDate = item.Publishing.PublishDate;
+
+			if (HasValidValue(publishDate))
+			{
+				scheduledPublishes.Add(CreateScheduledPublish(itemId, string.Empty, string.Empty, publishDate));
+			}
+			// To
+			var unpublishDate = item.Publishing.UnpublishDate;
+
+			if (HasValidValue(unpublishDate))
+			{
+				scheduledPublishes.Add(CreateScheduledPublish(itemId, string.Empty, string.Empty, unpublishDate));
+			}
+			// Version -> ID, Language, Version
+			// From
+			var validFrom = item.Publishing.ValidFrom;
+
+			if (HasValidValue(validFrom))
+			{
+				scheduledPublishes.Add(CreateScheduledPublish(itemId, language, version, validFrom));
+			}
+			// To
+			var validTo = item.Publishing.ValidTo;
+
+			if (HasValidValue(validTo))
+			{
+				scheduledPublishes.Add(CreateScheduledPublish(itemId, language, version, validTo));
+			}
+
+			return scheduledPublishes;
+		}
+
+		public bool HasValidValue(DateTime value)
+		{
+			return value != DateTime.MinValue && value != DateTime.MaxValue && value > DateTime.Now;
+		}
+
+		public ScheduledPublish CreateScheduledPublish(Guid itemId, string language, string version, DateTime publishOn)
+		{
+			return new ScheduledPublish
+			{
+				ItemId = itemId,
+				Language = language,
+				Published = false,
+				PublishOn = publishOn,
+				Version = version
+			};
 		}
 	}
 }
