655f8d68de5303ede104af6b17c8dc438574562a
Gabriel Boys <gabrielboys@gmail.com>
2016-02-09 14:20:38 -0500

    Adding some initial code for facet fields.

--- src/Informa.Library/Search/ComputedFields/Facets/ContentTypeField.cs
+++ src/Informa.Library/Search/ComputedFields/Facets/ContentTypeField.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Glass.Mapper.Sc;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Base_Templates;
 using Sitecore.Data.Items;
 using Velir.Search.Core.ComputedFields;
 
@@ -12,7 +14,19 @@ namespace Informa.Library.Search.ComputedFields.Facets
     {
         public override object GetFieldValue(Item indexItem)
         {
-            throw new NotImplementedException();
+            I___BaseTaxonomy taxonomItem = indexItem.GlassCast<I___BaseTaxonomy>(inferType: true);
+
+            if (taxonomItem?.Taxonomies != null)
+            {
+                var subjectTaxonomyItems =
+                    taxonomItem.Taxonomies.Where(
+                        x =>
+                            x._Path.ToLower()
+                                .StartsWith("/sitecore/content/scripintelligence/globals/taxonomy/content types"));
+
+                return subjectTaxonomyItems.Select(x => x.Item_Name).ToList();
+            }
+            return new List<string>();
         }
     }
 }
