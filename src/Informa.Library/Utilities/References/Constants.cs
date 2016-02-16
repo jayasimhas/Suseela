3a7eb44056ae1d670ecff1bce95ebe79842c5941
Aakash Shah <aakash.shah@velir.com>
2016-02-16 16:39:45 -0500

    IIPP-346

--- src/Informa.Library/Utilities/References/Constants.cs
+++ src/Informa.Library/Utilities/References/Constants.cs
@@ -15,10 +15,11 @@ namespace Informa.Library.Utilities.References
 		public static string MediaLibraryRoot = "/sitecore/media library/";
 		public static string MediaLibraryPath = "Documents/";
 
+		public static string ScripRootNode = "{3818C47E-4B75-4305-8F01-AB994150A1B0}";
 		public static string MSWordDocumentRootNode = "{ FDBFCAC8 -03CA-4B0B-BEFE-2171050E19C6}";
 		public static Dictionary<Guid, string> PublicationPrefixDictionary = new Dictionary<Guid, string>
 		{
-			{ new Guid("{3818C47E-4B75-4305-8F01-AB994150A1B0}"), "SC"},
+			{ new Guid(ScripRootNode), "SC"},
 		};
 	}
 }
