5dcb9711d44e13eb8894c0ffe340aadcbf8f5675
Aakash Shah <aakash.shah@velir.com>
2016-02-17 16:40:38 -0500

    IIPP-258

--- src/Informa.Library/CustomSitecore/Ribbon/DownloadWord.cs
+++ src/Informa.Library/CustomSitecore/Ribbon/DownloadWord.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sitecore.Shell.Framework.Commands;
-using System;
-using System.Configuration;
+﻿using Sitecore.Shell.Framework.Commands;
 using System.Web;
-using Glass.Mapper.Sc.Fields;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 using Sitecore.Configuration;
-using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Resources.Media;
-using Sitecore.Shell.Framework;
-using Sitecore.Shell.Framework.Commands;
-using Sitecore.Text;
 using Sitecore.Web.UI.Sheer;
 
 namespace Informa.Library.CustomSitecore.Ribbon
@@ -32,26 +20,19 @@ namespace Informa.Library.CustomSitecore.Ribbon
 			if (items.Length == 1)
 			{
 				Item article = items[0];
-				if (article != null)
+				LinkField wordDocument = article.Fields[IArticleConstants.Word_DocumentFieldName];
+				if (wordDocument != null)
 				{
-					LinkField wordDocument = article.Fields[IArticleConstants.Word_DocumentFieldName];
-					if (wordDocument != null)
+					var masterDb = Factory.GetDatabase("master");
+					Item item = masterDb.GetItem(wordDocument.TargetID);
+					if (item != null && MediaManager.HasMediaContent(item))
 					{
-						var masterDb = Factory.GetDatabase("master");
-						Item item = masterDb.GetItem(wordDocument.TargetID);
-						if (item != null && MediaManager.HasMediaContent(item))
-						{
-							string siteRoot = HttpContext.Current.Request.Url.Host;
-							Item docItem = masterDb.GetItem(wordDocument.TargetID);
+						string siteRoot = HttpContext.Current.Request.Url.Host;
+						Item docItem = masterDb.GetItem(wordDocument.TargetID);
 
-							string url = siteRoot + MediaManager.GetMediaUrl(docItem) + "?sc_mode=preview";
-							SheerResponse.Eval("window.open('http://" + url +
-											   "','_blank','height=800,width=800,menubar=no,toolbar=no,resizable=yes,scrollbars=yes,titlebar=no,location=no')");
-						}
-						else
-						{
-							Sitecore.Context.ClientPage.ClientResponse.Alert("No file has been attached");
-						}
+						string url = siteRoot + MediaManager.GetMediaUrl(docItem) + "?sc_mode=preview";
+						SheerResponse.Eval("window.open('http://" + url +
+										   "','_blank','height=800,width=800,menubar=no,toolbar=no,resizable=yes,scrollbars=yes,titlebar=no,location=no')");
 					}
 					else
 					{
