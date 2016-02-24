c7115b7534322926140a99d0e55fce50c2eb49de
Jason Bert <jason.bert@goblinrockets.com>
2016-02-24 12:31:35 +0000

    IIPP-60; Implemented basic Sitecore authentication

--- src/Informa.Library/User/Authentication/LoginWebUser.cs
+++ src/Informa.Library/User/Authentication/LoginWebUser.cs
@@ -1,4 +1,7 @@
 ﻿using Jabberwocky.Glass.Autofac.Attributes;
+using Sitecore;
+using Sitecore.Analytics;
+using Sitecore.Security.Authentication;
 
 namespace Informa.Library.User.Authentication
 {
@@ -16,10 +19,28 @@ namespace Informa.Library.User.Authentication
 		public ILoginWebUserResult Login(string username, string password, bool persist)
 		{
 			var result = AuthenticateUser.Authenticate(username, password);
-			
+			var authenticated = result.State == AuthenticateUserResultState.Success;
+
+			if (authenticated)
+			{
+				var sitecoreUsername = string.Format("{0}\\{1}", Context.Domain.Name, username);
+				var sitecoreVirtualUser = AuthenticationManager.BuildVirtualUser(sitecoreUsername, true);
+
+				sitecoreVirtualUser.Profile.Email = username;
+
+				AuthenticationManager.LoginVirtualUser(sitecoreVirtualUser);
+
+				var tracker = Tracker.Current;
+
+				if (tracker != null)
+				{
+					tracker.Session.Identify(sitecoreVirtualUser.Identity.Name);
+				}
+			}
+
 			return new LoginWebUserResult
 			{
-				Success = result.State == AuthenticateUserResultState.Success,
+				Success = authenticated,
 				Message = string.Format("ID = {0}", result.User?.Id ?? "NULL")
 			};
 		}
