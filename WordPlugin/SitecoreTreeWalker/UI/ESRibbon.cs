372c894fb3b8cdbc3ee12e21f8d4c537b84c94f1
Aakash Shah <aakash.shah@velir.com>
2016-03-01 10:53:32 -0500

    IIPP-20 - Changing the working of Save Article based on new requirements

--- WordPlugin/SitecoreTreeWalker/UI/ESRibbon.cs
+++ WordPlugin/SitecoreTreeWalker/UI/ESRibbon.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 using PluginModels;
 using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Word;
 using Microsoft.Office.Tools.Ribbon;
 using SitecoreTreeWalker.Config;
 using SitecoreTreeWalker.document;
@@ -14,6 +16,7 @@ using SitecoreTreeWalker.UI.ArticleDetailsForm;
 using SitecoreTreeWalker.UI.TreeBrowser.TreeBrowserControls;
 using SitecoreTreeWalker.Util;
 using SitecoreTreeWalker.User;
+using SitecoreTreeWalker.Util.Document;
 using SitecoreTreeWalker.WebserviceHelper;
 
 
@@ -22,9 +25,11 @@ namespace SitecoreTreeWalker.UI
 	public partial class ESRibbon
 	{
 		SitecoreUser _user = SitecoreUser.GetUser();
-        public ArticleStruct ArticleDetails = new ArticleStruct();
+		public ArticleStruct ArticleDetails = new ArticleStruct();
 		private DocumentCustomProperties _documentCustomProperties;
 		private Microsoft.Office.Tools.CustomTaskPane myCustomTaskPane;
+		private WordUtils _wordUtils;
+		private SitecoreClient _sitecoreArticle;
 
 		private void ESRibbon_Load(object sender, RibbonUIEventArgs e)
 		{
@@ -204,7 +209,7 @@ namespace SitecoreTreeWalker.UI
 		/// ArticleStruct articleStruct
 		/// </summary>
 		/// <param name="articleStruct"></param>
-        public void SetArticleDetails(ArticleStruct articleStruct)
+		public void SetArticleDetails(ArticleStruct articleStruct)
 		{
 			ArticleDetails = articleStruct;
 		}
@@ -218,7 +223,7 @@ namespace SitecoreTreeWalker.UI
 
 		private string GetPreviewUrl(bool isMobile)
 		{
-            string guid = SitecoreClient.GetArticleGuidByArticleNumber(GetArticleNumber());
+			string guid = SitecoreClient.GetArticleGuidByArticleNumber(GetArticleNumber());
 			string domain = Constants.EDITOR_ENVIRONMENT_SERVERURL;
 			string mobileUrlParam = isMobile ? "&mobile=1" : String.Empty;
 			string redirect = (domain + @"?sc_itemid={" + guid + @"}&sc_mode=preview&sc_lang=en" + mobileUrlParam);
@@ -283,13 +288,21 @@ namespace SitecoreTreeWalker.UI
 			LogoutBtn.Visible = !LogoutBtn.Visible;
 		}
 
-		private void SaveArticleBtn_Click(object sender, RibbonControlEventArgs e)
+		private void ArticlePreviewMenu_Click(object sender, RibbonControlEventArgs e)
 		{
-			//SitecoreAddin.TagActiveDocument();
-			var documentCustomProperties = new DocumentCustomProperties(SitecoreAddin.ActiveDocument);
-			if (!string.IsNullOrEmpty(documentCustomProperties.ArticleNumber))
+			CheckLoginAndPerformAction(GetPreview);
+		}
+
+		private void SaveToSitecoreBtn_Click(object sender, RibbonControlEventArgs e)
+		{
+
+
+			//var documentCustomProperties = new DocumentCustomProperties(SitecoreAddin.ActiveDocument);
+			//if (!string.IsNullOrEmpty(documentCustomProperties.ArticleNumber))
+			//{
+			if (GetArticleNumber() != null)
 			{
-				MessageBox.Show(@"Article Saved", @"Informa", MessageBoxButtons.OK, MessageBoxIcon.None);
+				CheckLoginAndPerformAction(SaveArticleData);
 			}
 			else
 			{
@@ -297,9 +310,282 @@ namespace SitecoreTreeWalker.UI
 			}
 		}
 
-		private void ArticlePreviewMenu_Click(object sender, RibbonControlEventArgs e)
+		public void SaveArticleData()
 		{
-			CheckLoginAndPerformAction(GetPreview);
+			Globals.SitecoreAddin.Log("Save and transferring");
+			SuspendLayout();
+
+			SitecoreAddin.ActiveDocument.Saved = false;
+			_wordUtils = new WordUtils();
+			try
+			{
+				var metadataParser = new ArticleDocumentMetadataParser(SitecoreAddin.ActiveDocument, _wordUtils.CharacterStyleTransformer);
+				if (PreSavePrompts(metadataParser)) return;
+				SaveArticleToSitecoreUpdateUi(metadataParser);
+			}
+			catch (WebException wex)
+			{
+				AlertConnectionFailure();
+				Globals.SitecoreAddin.LogException("Web connection error when saving and transferring article!", wex);
+			}
+			catch (Exception ex)
+			{
+				Globals.SitecoreAddin.LogException("Error when saving and transferring article!", ex);
+				MessageBox.Show(@"Error when saving and transferring article! Error recorded in logs.", @"Informa");
+			}
+			finally
+			{
+				ResumeLayout();
+			}
+			Document activeDocument = SitecoreAddin.ActiveDocument;
+			var path = activeDocument.Path;
+			if (!activeDocument.ReadOnly && !string.IsNullOrWhiteSpace(path))
+			{
+				WordUtils.Save(activeDocument);
+			}
+		}
+
+		private void SaveArticleToSitecoreUpdateUi(ArticleDocumentMetadataParser metadataParser, string body = null)
+		{
+			_documentCustomProperties.ArticleNumber = GetArticleNumber();
+			InvalidStylesHighlighter highlighter = InvalidStylesHighlighter.GetParser();
+			bool hasInvalidStyles = highlighter.HighlightAllInvalidStyles(Globals.SitecoreAddin.Application.ActiveDocument);
+			if (hasInvalidStyles && !AskContinueInvalidStyle())
+			{
+				return;
+			}
+
+			var saved = SaveArticle(metadataParser, body);
+
+			if (!saved)
+			{
+				return;
+			}
+			
+			MessageBox.Show(@"Article successfully saved to Sitecore!", @"Informa");			
+		}
+
+		/// <summary>
+		/// 
+		/// 
+		/// Be sure to catch WebException indicating server could not be contacted
+		/// Saves a locked document (and unlocks the local document)
+		/// </summary>
+		public bool SaveArticle(ArticleDocumentMetadataParser metadataParser = null, string body = null)
+		{
+			var documentCustomProperties = new DocumentCustomProperties(SitecoreAddin.ActiveDocument);
+			var articleNumber = GetArticleNumber();
+
+			if (!string.IsNullOrEmpty(documentCustomProperties.ArticleNumber) && !string.IsNullOrEmpty(articleNumber))
+			{
+				if (articleNumber != documentCustomProperties.ArticleNumber)
+				{
+					string alertMessage = "Article numbers do not match, are you sure you would like to continue? " +
+										  "This can happen accidentally if you switching between documents quickly during saving.";
+					DialogResult result = MessageBox.Show(alertMessage, "Article numbers do not match",
+														  MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+					if (result == DialogResult.No)
+					{
+						return false;
+					}
+				}
+			}
+
+			try
+			{
+				if (HasArticleNumber())
+				{
+					var copy = ArticleDetails.ArticleGuid;
+					ArticleDetails = GetArticleDetails(articleNumber,metadataParser);
+					ArticleDetails.ArticleGuid = copy;
+					_sitecoreArticle = new SitecoreClient();
+					List<string> errors = _sitecoreArticle.SaveArticle(SitecoreAddin.ActiveDocument, ArticleDetails, new Guid(), new StaffStruct[0], GetArticleNumber(), body);
+					if (errors != null && errors.Any())
+					{
+						foreach (string error in errors)
+						{
+							if (!String.IsNullOrEmpty(error))
+							{
+								MessageBox.Show(error, error.Contains("not secure") ? @" Non-Secure Multimedia Content" : @"Informa");
+							}
+						}
+						return false;
+					}					
+				}
+			}
+			catch (Exception ex)
+			{
+				Globals.SitecoreAddin.LogException("Error when saving article!", ex);
+				throw;
+			}
+
+			return true;
+		}
+
+		public ArticleStruct GetArticleDetails(string articleNumber, ArticleDocumentMetadataParser metadataParser = null)
+		{
+			var sitecoreArticleDetails = SitecoreClient.ForceReadArticleDetails(articleNumber);
+			if (metadataParser == null)
+			{
+				metadataParser = new ArticleDocumentMetadataParser(SitecoreAddin.ActiveDocument,_wordUtils.CharacterStyleTransformer);
+			}
+			string longSummary = metadataParser.LongSummary;
+			var articleDetails = new ArticleStruct
+			{
+				ArticleNumber = sitecoreArticleDetails.ArticleNumber,
+				WebPublicationDate = sitecoreArticleDetails.WebPublicationDate,
+				Title = metadataParser.Title.Trim(),
+				Summary = longSummary,
+				Subtitle = metadataParser.Subtitle,
+				Publication = sitecoreArticleDetails.Publication,
+				Authors = sitecoreArticleDetails.Authors.ToList(),
+				Label = sitecoreArticleDetails.Label,
+				MediaType = sitecoreArticleDetails.MediaType,
+				NotesToEditorial = sitecoreArticleDetails.NotesToEditorial,
+				Taxonomoy = sitecoreArticleDetails.Taxonomoy,
+				RelatedInlineArticles = sitecoreArticleDetails.RelatedInlineArticles,
+				RelatedArticles = sitecoreArticleDetails.RelatedArticles,
+				ArticleSpecificNotifications = sitecoreArticleDetails.GlobalNotifications,
+				Embargoed = sitecoreArticleDetails.Embargoed,
+				FeaturedImageCaption = sitecoreArticleDetails.FeaturedImageCaption,
+				FeaturedImageSource = sitecoreArticleDetails.FeaturedImageSource
+			};
+
+			//RelatedInlineArticles = sitecoreArticleDetails.RelatedInlineArticles.ToList(),
+			//	RelatedArticles = sitecoreArticleDetails.RelatedArticles.ToList(),
+			//	ArticleSpecificNotifications = sitecoreArticleDetails.GlobalNotifications.ToList(),
+
+			if (sitecoreArticleDetails.FeaturedImage != new Guid())
+			{
+				articleDetails.FeaturedImage = sitecoreArticleDetails.FeaturedImage;
+			}
+
+			return articleDetails;
+		}
+
+		/// <summary>
+		/// Should be equivalent to "IsDocumentLinkedToSitecoreArticleItem"
+		/// </summary>
+		/// <returns></returns>
+		private bool HasArticleNumber()
+		{
+			return (!string.IsNullOrEmpty(_documentCustomProperties.ArticleNumber));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="metadataParser"></param>
+		/// <returns>True if user wishes to break</returns>
+		private bool PreSavePrompts(ArticleDocumentMetadataParser metadataParser)
+		{
+			if (string.IsNullOrEmpty(metadataParser.Title))
+			{
+				MessageBox.Show(@"Please enter an article title.", @"Informa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return true;
+			}
+
+			if (NotifyTitleTooLong(metadataParser.Title.Trim().Length))
+			{
+				return true;
+			}
+
+			if (metadataParser.TitleCount > 1)
+			{
+				if (!AskContinueMultipleTitles())
+				{
+					return true;
+				}
+			}
+			int maxLengthLongSummary = SitecoreClient.GetMaxLengthLongSummary();
+			if (metadataParser.LongSummary.Length > maxLengthLongSummary)
+			{
+				if (!AskExceededCharacterLimit("Summary", maxLengthLongSummary))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Presents a prompt if title is too long
+		/// </summary>
+		/// <param name="titleLength"></param>
+		/// <returns>True if titleLength is too long; else false</returns>
+		protected bool NotifyTitleTooLong(int titleLength)
+		{
+			int maxLength = ApplicationConfig.GetTitleMaxCharacters();
+			if (titleLength > maxLength)
+			{
+				MessageBox.Show($"Title is too long! Title character limit is {maxLength}. Current title is {titleLength} characters long!", @"Informa");
+				return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Prompts the user to continue even though there are multiple sections styled as "title".
+		/// </summary>
+		/// <returns>True if the user wishes to continue</returns>
+		protected bool AskContinueMultipleTitles()
+		{
+			DialogResult dialogResult = MessageBox.Show
+							(@"There are multiple paragraphs that are styled as Story Title. " +
+								@"Do you wish to proceed?",
+								@"Informa",
+								MessageBoxButtons.YesNo,
+								MessageBoxIcon.Question);
+			return (dialogResult == DialogResult.Yes);
+		}
+
+		/// <summary>
+		/// Prompts the user that they have exceeded a character limit for a field
+		/// </summary>
+		/// <param name="field"></param>
+		/// <param name="characterLimit"></param>
+		/// <returns>True if the user wishes to continure</returns>
+		protected bool AskExceededCharacterLimit(string field, int characterLimit)
+		{
+			string message =
+				String.Format("You have exceed the character limit of {0} characters for the {1}. " +
+							  "The {1} will be truncated. Do you wish to continue?",
+							  characterLimit, field);
+			DialogResult dialogResult = MessageBox.Show
+							(message,
+								@"Informa",
+								MessageBoxButtons.YesNo,
+								MessageBoxIcon.Question);
+			return (dialogResult == DialogResult.Yes);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>True if user wishes to continue</returns>
+		protected bool AskContinueInvalidStyle()
+		{
+			DialogResult dialogResult = MessageBox.Show
+				(@"Unmapped styles have been identified in your document. " +
+				 @"If you wish to proceed with saving, this text will be " +
+				 @"transferred with the default story text style. Do you " +
+				 @"wish to proceed?",
+				 @"Informa",
+				 MessageBoxButtons.YesNo,
+				 MessageBoxIcon.Question);
+			return (dialogResult == DialogResult.Yes);
+		}
+
+		public DialogResult AlertConnectionFailure()
+		{
+			return MessageBox.Show
+				(@"Sitecore server could not be contacted! Please try again in a few minutes." + Environment.NewLine +
+				 Environment.NewLine + @"If the problem persists, contact your system administrator.",
+				 @"Informa",
+				 MessageBoxButtons.OK,
+				 MessageBoxIcon.Error);
 		}
 	}
 }
