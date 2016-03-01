aa0725c7fd4703eb2945172461e7cd39890ac90f
Gabriel Boys <gabrielboys@gmail.com>
2016-03-01 10:52:05 -0500

    Added search model for dictionary text and added the items for first and last page in the pagination

---
+++ web/Website/ViewModels/ISearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informa.Web.ViewModels
+{
+    public interface ISearchModel
+    {
+        string PageFirstText { get; }
+        string PageLastText { get; }
+    }
+
+}
---
+++ web/Website/ViewModels/SearchModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Informa.Library.Article.Search;
+using Informa.Library.Globalization;
+using Informa.Library.Site;
+using Jabberwocky.Glass.Autofac.Attributes;
+
+namespace Informa.Web.ViewModels
+{
+    [AutowireService(LifetimeScope.SingleInstance)]
+    public class SearchModel : ISearchModel
+    {
+        protected readonly ITextTranslator TextTranslator;
+
+        public SearchModel() { }
+
+        public SearchModel(
+          ITextTranslator textTranslator)
+        {
+            TextTranslator = textTranslator;
+        }
+
+        public string PageFirstText => TextTranslator.Translate("Search.Page.First");
+        public string PageLastText => TextTranslator.Translate("Search.Page.Last");
+
+    }
+}
