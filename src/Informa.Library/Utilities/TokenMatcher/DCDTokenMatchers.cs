d88cc901974a9303c992b16bff3499ef98138c47
Tamer Mehyar <tmehyar@tanasuk.com>
2016-03-03 16:18:33 +0200

    Implement DCD deals token matching/replacing

--- src/Informa.Library/Utilities/TokenMatcher/DCDTokenMatchers.cs
+++ src/Informa.Library/Utilities/TokenMatcher/DCDTokenMatchers.cs
@@ -17,10 +17,15 @@ namespace Informa.Library.Utilities.TokenMatcher
 
             try
             {
-                string firstStepText = processCompnayTokens(body);
+                string tempText = processCompnayTokens(body);
 
-                if (string.IsNullOrEmpty(firstStepText) == false)
-                    body = firstStepText;
+                if (string.IsNullOrEmpty(tempText) == false)
+                    body = tempText;
+
+                tempText = processDealTokens(body);
+
+                if (string.IsNullOrEmpty(tempText) == false)
+                    body = tempText;
             }
             catch (Exception ex)
             {
@@ -31,25 +36,50 @@ namespace Informa.Library.Utilities.TokenMatcher
 
         private static string processCompnayTokens(string text)
         {
-            //Find all matches with Deal token
+            //Find all matches with Company token
             Regex regex = new Regex(DCDConstants.CompanyTokenRegex);
 
-            MatchEvaluator evaluator = new MatchEvaluator(matchEval);
+            MatchEvaluator evaluator = new MatchEvaluator(companyMatchEval);
             return regex.Replace(text, evaluator);
         }
 
-        private static string matchEval(Match match)
+        private static string processDealTokens(string text)
         {
-            string value = match.Value;
+            //Find all matches with Deal token
+            Regex regex = new Regex(DCDConstants.DealTokenRegex);
 
-            //remove the token part on the sides to keep the company name and number separated by a ':'
-            value = value.Replace("[C#", string.Empty).Replace("]", string.Empty);
+            MatchEvaluator evaluator = new MatchEvaluator(dealMatchEval);
+            return regex.Replace(text, evaluator);
+        }
 
-            //Split the remaining to separate the company name from the number
-            string[] companyDet = value.Split(':');
+        private static string companyMatchEval(Match match)
+        {
+            try
+            {
+                //return a strong company name (from the token itself) to replace the token
+                return string.Format("<strong>{0}</strong>", match.Groups[1].Value.Split(':')[1]);
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Error when evaluating company match token", ex, "LogFileAppender");
+                return string.Empty;
+            }
+        }
 
-            //return a strong company name (from the token itself) to replace the token
-            return string.Format("<strong>{0}</strong>", companyDet[1]);
+        private static string dealMatchEval(Match match)
+        {
+            try
+            {
+                Deal deal = new DCDManager().GetDealByRecordNumber(match.Groups[1].Value);
+
+                //return a strong company name (from the token itself) to replace the token
+                return string.Format("[<a href=\"{0}\">See Deal</a>]", string.Format(Sitecore.Configuration.Settings.GetSetting("DCD.OldDealsURL"), deal.RecordNumber));
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Error when evaluating deal match token", ex, "LogFileAppender");
+                return string.Empty;
+            }
         }
     }
 }
