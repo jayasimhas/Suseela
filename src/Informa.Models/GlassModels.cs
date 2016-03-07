a2a538e33676c5c704fada643e8ce1c205954822
Aakash Shah <aakash.shah@velir.com>
2016-03-07 13:43:56 -0500

    IIPP-184

--- src/Informa.Models/GlassModels.cs
+++ src/Informa.Models/GlassModels.cs
@@ -3265,6 +3265,94 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects.
 	}
 
 }
+namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configuration
+{
+
+
+ 	/// <summary>
+	/// IEmail_Config Interface
+	/// <para></para>
+	/// <para>Path: /sitecore/templates/User Defined/Configuration/Email Config</para>	
+	/// <para>ID: 36068aee-c4d0-4a61-9acb-755efcd2de40</para>	
+	/// </summary>
+	[SitecoreType(TemplateId=IEmail_ConfigConstants.TemplateIdString)]
+	public partial interface IEmail_Config : IGlassBase 
+	{
+								/// <summary>
+					/// The Email A Friend Body Content field.
+					/// <para></para>
+					/// <para>Field Type: Rich Text</para>		
+					/// <para>Field ID: 184010d1-69d3-4148-9848-d6072a002bf1</para>
+					/// <para>Custom Data: </para>
+					/// </summary>
+					[SitecoreField(IEmail_ConfigConstants.Email_A_Friend_Body_ContentFieldName, Setting = SitecoreFieldSettings.InferType)]
+					string Email_A_Friend_Body_Content  {get; set;}
+								/// <summary>
+					/// The Email A Friend Footer Content field.
+					/// <para></para>
+					/// <para>Field Type: Rich Text</para>		
+					/// <para>Field ID: 23b4e9bc-9d9c-44f8-80ac-93230012f320</para>
+					/// <para>Custom Data: </para>
+					/// </summary>
+					[SitecoreField(IEmail_ConfigConstants.Email_A_Friend_Footer_ContentFieldName, Setting = SitecoreFieldSettings.InferType)]
+					string Email_A_Friend_Footer_Content  {get; set;}
+				}
+
+	
+		/// <summary>
+		/// IEmail_Config Interface
+		/// <para></para>
+		/// <para>Path: /sitecore/templates/User Defined/Configuration/Email Config</para>	
+		/// <para>ID: 36068aee-c4d0-4a61-9acb-755efcd2de40</para>	
+		/// </summary>
+		[SitecoreType]
+		public partial interface IEmail_Config__Raw : IGlassBase 
+		{
+										/// <summary>
+						/// The Email A Friend Body Content field.
+						/// <para></para>
+						/// <para>Field Type: Rich Text</para>		
+						/// <para>Field ID: 184010d1-69d3-4148-9848-d6072a002bf1</para>
+						/// <para>Custom Data: </para>
+						/// </summary>
+						[SitecoreField(IEmail_ConfigConstants.Email_A_Friend_Body_ContentFieldName, Setting = SitecoreFieldSettings.RichTextRaw)]
+						string Email_A_Friend_Body_Content  {get; set;}
+										/// <summary>
+						/// The Email A Friend Footer Content field.
+						/// <para></para>
+						/// <para>Field Type: Rich Text</para>		
+						/// <para>Field ID: 23b4e9bc-9d9c-44f8-80ac-93230012f320</para>
+						/// <para>Custom Data: </para>
+						/// </summary>
+						[SitecoreField(IEmail_ConfigConstants.Email_A_Friend_Footer_ContentFieldName, Setting = SitecoreFieldSettings.RichTextRaw)]
+						string Email_A_Friend_Footer_Content  {get; set;}
+						}
+	
+	public static partial class IEmail_ConfigConstants{
+
+			public const string TemplateIdString = "36068aee-c4d0-4a61-9acb-755efcd2de40";
+			public static readonly ID TemplateId = new ID(TemplateIdString);
+			public const string TemplateName = "Email Config";
+
+					
+			public static readonly ID Email_A_Friend_Body_ContentFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+			public const string Email_A_Friend_Body_ContentFieldName = "Email A Friend Body Content";
+
+							public static readonly ID Email_A_Friend_Body_Content__RawFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+				public const string Email_A_Friend_Body_Content__RawFieldName = "Email A Friend Body Content";
+						
+					
+			public static readonly ID Email_A_Friend_Footer_ContentFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+			public const string Email_A_Friend_Footer_ContentFieldName = "Email A Friend Footer Content";
+
+							public static readonly ID Email_A_Friend_Footer_Content__RawFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+				public const string Email_A_Friend_Footer_Content__RawFieldName = "Email A Friend Footer Content";
+						
+			
+
+	}
+
+}
 namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects.Highlights
 {
 
@@ -7959,7 +8047,7 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configur
 	/// <para>ID: 997a3064-65f5-45cd-b32f-9ec61e90a610</para>	
 	/// </summary>
 	[SitecoreType(TemplateId=ISite_ConfigConstants.TemplateIdString)]
-	public partial interface ISite_Config : IGlassBase 
+	public partial interface ISite_Config : IGlassBase , global::Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configuration.IEmail_Config
 	{
 								/// <summary>
 					/// The Global Article Filmstrip Slot ID field.
@@ -8385,7 +8473,7 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configur
 		/// <para>ID: 997a3064-65f5-45cd-b32f-9ec61e90a610</para>	
 		/// </summary>
 		[SitecoreType]
-		public partial interface ISite_Config__Raw : IGlassBase 
+		public partial interface ISite_Config__Raw : IGlassBase , global::Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configuration.IEmail_Config__Raw
 		{
 										/// <summary>
 						/// The Global Article Filmstrip Slot ID field.
@@ -9056,6 +9144,20 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configur
 			public static readonly ID From_Email_AddressFieldId = new ID("c89fa3b2-0d80-4086-a9f0-24a5a3e9d14e");
 			public const string From_Email_AddressFieldName = "From Email Address";
 
+						
+					
+			public static readonly ID Email_A_Friend_Body_ContentFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+			public const string Email_A_Friend_Body_ContentFieldName = "Email A Friend Body Content";
+
+							public static readonly ID Email_A_Friend_Body_Content__RawFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+				public const string Email_A_Friend_Body_Content__RawFieldName = "Email A Friend Body Content";
+						
+					
+			public static readonly ID Email_A_Friend_Footer_ContentFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+			public const string Email_A_Friend_Footer_ContentFieldName = "Email A Friend Footer Content";
+
+							public static readonly ID Email_A_Friend_Footer_Content__RawFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+				public const string Email_A_Friend_Footer_Content__RawFieldName = "Email A Friend Footer Content";
 						
 			
 
@@ -12417,6 +12519,20 @@ namespace Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configur
 			public static readonly ID From_Email_AddressFieldId = new ID("c89fa3b2-0d80-4086-a9f0-24a5a3e9d14e");
 			public const string From_Email_AddressFieldName = "From Email Address";
 
+						
+					
+			public static readonly ID Email_A_Friend_Body_ContentFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+			public const string Email_A_Friend_Body_ContentFieldName = "Email A Friend Body Content";
+
+							public static readonly ID Email_A_Friend_Body_Content__RawFieldId = new ID("184010d1-69d3-4148-9848-d6072a002bf1");
+				public const string Email_A_Friend_Body_Content__RawFieldName = "Email A Friend Body Content";
+						
+					
+			public static readonly ID Email_A_Friend_Footer_ContentFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+			public const string Email_A_Friend_Footer_ContentFieldName = "Email A Friend Footer Content";
+
+							public static readonly ID Email_A_Friend_Footer_Content__RawFieldId = new ID("23b4e9bc-9d9c-44f8-80ac-93230012f320");
+				public const string Email_A_Friend_Footer_Content__RawFieldName = "Email A Friend Footer Content";
 						
 			
 
