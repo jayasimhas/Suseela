64a28d800680337e15a2296ff1be51ee406e2c0b
Rose Heydt <rose.heydt@gmail.com>
2016-03-08 08:50:29 -0500

    IIPP-455 datepicker style & functionality

--- web/Website/js/search/informaSearchApp.js
+++ web/Website/js/search/informaSearchApp.js
@@ -1,36 +1,10 @@
 ﻿(function () {
     'use strict';
 
-    //** Including this ngAnimate mock instead of loading ngAnimate because this is smaller **//
-    //** (Angular-Strap requires either ngAnimate or this mock **//
-    //** source: https://github.com/mgcrea/angular-strap/wiki/ngAnimate-mock **//
-    // angular.module('ngAnimate', [])
-
-    //   .factory('$$animateReflow', ['$window', '$timeout', function($window, $timeout) {
-    //     var requestAnimationFrame = $window.requestAnimationFrame       ||
-    //                                 $window.webkitRequestAnimationFrame ||
-    //                                 function(fn) {
-    //                                   return $timeout(fn, 10, false);
-    //                                 };
-
-    //     var cancelAnimationFrame = $window.cancelAnimationFrame       ||
-    //                                $window.webkitCancelAnimationFrame ||
-    //                                function(timer) {
-    //                                  return $timeout.cancel(timer);
-    //                                };
-    //     return function(fn) {
-    //       var id = requestAnimationFrame(fn);
-    //       return function() {
-    //         cancelAnimationFrame(id);
-    //       };
-    //     };
-    //   }]);
-
     var informaSearchApp = angular.module('informaSearchApp', [
         'velir.search',
         'ui.bootstrap'
     ])
-    // RH removed 'ngAnimate' from above dependencies b/c of error "realRunner.done is not a function"
         .constant('apiEndpoints', {
             API_BASE: '/api',
             SEARCH_ENDPOINT: '/search'
@@ -59,106 +33,50 @@
             }
         }
     });
-    
-    // ANGULAR UI EXAMPLE
-    // angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
-    /*angular.module('informaSearchApp').controller('DatepickerDemoCtrl', function ($scope) {
+
+
+// TODO: This should be split off into a new file inside the controllers directory
+    informaSearchApp.controller("InformaDatesController",
+      ["$scope", function($scope){
+
+      // need to differentiate the 2 datepickers
+      $scope.datepickers = {
+        dtFrom: false,
+        dtTo: false
+      }
+ 
+      // grab today and inject into field
       $scope.today = function() {
-        $scope.dt = new Date();
+        $scope.dtFrom = new Date();
+        $scope.dtTo = new Date();
       };
+      
+      // run today() function
       $scope.today();
 
-      $scope.clear = function() {
+      // setup clear
+      $scope.clear = function () {
         $scope.dt = null;
+        // $scope.dtFrom = null;
+        // $scope.dtTo = null;
       };
 
-      $scope.inlineOptions = {
-        customClass: getDayClass,
-        minDate: new Date(),
-        showWeeks: true
+      // open min-cal
+      $scope.open = function($event, which) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.datepickers[which] = true;
       };
-
-      $scope.dateOptions = {
-        dateDisabled: disabled,
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-      };
-
-      // Disable weekend selection
-      function disabled(data) {
-        var date = data.date,
-          mode = data.mode;
-        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-      }
-
-      $scope.toggleMin = function() {
-        $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-        $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-      };
-
-      $scope.toggleMin();
-
-      $scope.open1 = function() {
-        $scope.popup1.opened = true;
-      };
-
-      $scope.open2 = function() {
-        $scope.popup2.opened = true;
-      };
-
-      $scope.setDate = function(year, month, day) {
-        $scope.dt = new Date(year, month, day);
-      };
-
+      
+      // handle formats
       $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      
+      // assign custom format
       $scope.format = $scope.formats[0];
-      $scope.altInputFormats = ['M!/d!/yyyy'];
+      
+    }]);
 
-      $scope.popup1 = {
-        opened: false
-      };
 
-      $scope.popup2 = {
-        opened: false
-      };
-
-      var tomorrow = new Date();
-      tomorrow.setDate(tomorrow.getDate() + 1);
-      var afterTomorrow = new Date();
-      afterTomorrow.setDate(tomorrow.getDate() + 1);
-      $scope.events = [
-        {
-          date: tomorrow,
-          status: 'full'
-        },
-        {
-          date: afterTomorrow,
-          status: 'partially'
-        }
-      ];
-
-      function getDayClass(data) {
-        var date = data.date,
-          mode = data.mode;
-        if (mode === 'day') {
-          var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-          for (var i = 0; i < $scope.events.length; i++) {
-            var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-            if (dayToCheck === currentDay) {
-              return $scope.events[i].status;
-            }
-          }
-        }
-
-        return '';
-      }
-    });
-    */
-
-    
 
 })(); 
