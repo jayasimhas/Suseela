118014414ede5175102b1108b334f1fdea92c62b
Jason Bert <jason.bert@goblinrockets.com>
2016-03-09 18:46:55 +0000

    IIPP-713; Changed how Salesforce service URLs are read from configuration

--- src/Informa.Library.Salesforce/SalesforceSessionFactoryConfiguration.cs
+++ src/Informa.Library.Salesforce/SalesforceSessionFactoryConfiguration.cs
@@ -2,14 +2,15 @@
 
 namespace Informa.Library.Salesforce
 {
-	// TODO-Sforce: Read values from configuration files
 	public class SalesforceSessionFactoryConfiguration : ISalesforceSessionFactoryConfiguration
 	{
+		private const string UrlConfigKey = "SalesforceSessionFactoryConfiguration.Url";
 		private const string UsernameConfigKey = "SalesforceSessionFactoryConfiguration.Username";
 		private const string PasswordConfigKey = "SalesforceSessionFactoryConfiguration.Password";
 		private const string TokenConfigKey = "SalesforceSessionFactoryConfiguration.Token";
 		private const string TimeoutConfigKey = "SalesforceSessionFactoryConfiguration.Timeout";
 
+		public string Url => Settings.GetSetting(UrlConfigKey);
 		public string Username => Settings.GetSetting(UsernameConfigKey);
 		public string Password => Settings.GetSetting(PasswordConfigKey);
 		public string Token => Settings.GetSetting(TokenConfigKey);
