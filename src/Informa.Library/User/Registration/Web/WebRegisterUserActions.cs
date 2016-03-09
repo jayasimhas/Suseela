83b584b505f5cf0ef50d0b84d560eac78424a625
Jason Bert <jason.bert@goblinrockets.com>
2016-03-09 13:35:08 +0000

    IIPP-172; Implemented white label new user confirmation email

---
+++ src/Informa.Library/User/Registration/Web/WebRegisterUserActions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Informa.Library.User.Registration.Web
+{
+	public class WebRegisterUserActions : IWebRegisterUserActions
+	{
+		protected readonly IEnumerable<IWebRegisterUserAction> Actions;
+
+		public WebRegisterUserActions(
+			IEnumerable<IWebRegisterUserAction> actions)
+		{
+			Actions = actions;
+		}
+
+		public IEnumerator<IWebRegisterUserAction> GetEnumerator()
+		{
+			return Actions == null ? Enumerable.Empty<IWebRegisterUserAction>().GetEnumerator() : Actions.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
