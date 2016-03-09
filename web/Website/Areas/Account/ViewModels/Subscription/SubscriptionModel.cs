446f3938d7949b3f043814d874cdbc009be4b647
chetnakad <chetna.kadam@xpointers.com>
2016-03-09 17:25:48 +0530

    IIPP-170 : Adding logic for 1-click newsletter subscribe link

---
+++ web/Website/Areas/Account/ViewModels/Subscription/SubscriptionModel.cs
@@ -0,0 +1,14 @@
+﻿using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Base_Templates;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.Areas.Account.ViewModels.Subscription
+{
+	public class SubscriptionModel
+	{
+		public string BodyText { get; set; }
+	}
+}
