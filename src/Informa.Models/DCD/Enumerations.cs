6b45a4825ec5cbff694097eeeadd2e8d115977f1
Tamer Mehyar <tmehyar@tanasuk.com>
2016-03-10 16:47:57 +0200

    DCD Importing: fix token matching issues and move enumeration class

--- src/Informa.Models/DCD/Enumerations.cs
+++ src/Informa.Models/DCD/Enumerations.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Informa.Library.DCD.XMLImporting
+﻿
+namespace Informa.Models.DCD
 {
     public enum ImportResult
     {
