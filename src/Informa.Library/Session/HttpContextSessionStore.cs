c8df41738f44fea6496910b23aca497c2b58315b
Jason Bert <jason.bert@goblinrockets.com>
2016-03-10 15:56:49 +0000

    IIPP-111; Added generic session store dependency

---
+++ src/Informa.Library/Session/HttpContextSessionStore.cs
@@ -0,0 +1,21 @@
+﻿using Jabberwocky.Glass.Autofac.Attributes;
+using System.Web;
+
+namespace Informa.Library.Session
+{
+	[AutowireService(LifetimeScope.SingleInstance)]
+	public class HttpContextSessionStore : ISessionStore
+	{
+		public T Get<T>(string key)
+		{
+			var obj = HttpContext.Current.Session[key];
+
+			return (T)(obj is T ? obj : null);
+		}
+
+		public void Set<T>(string key, T obj)
+		{
+			HttpContext.Current.Session[key] = obj;
+		}
+	}
+}
