e3334ae85d06a0b4425971ac6c89c805ffd5fb75
Jason Bert <jason.bert@goblinrockets.com>
2016-03-10 15:46:33 +0000

    IIPP-111; Implemented cached find company by IP address

---
+++ src/Informa.Library.Salesforce/Company/SalesforceCompany.cs
@@ -0,0 +1,13 @@
+﻿using Informa.Library.Company;
+using System.Net;
+
+namespace Informa.Library.Salesforce.Company
+{
+	public class SalesforceCompany : ISalesforceCompany, ICompany
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public IPAddress LowerIpAddress { get; set; }
+		public IPAddress UpperIpAddress { get; set; }
+	}
+}
