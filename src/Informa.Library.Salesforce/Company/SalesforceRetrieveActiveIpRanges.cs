ceaf92aa1bc23ef54e4abe1e0f49de4d618c9e6e
Jason Bert <jason.bert@goblinrockets.com>
2016-03-10 12:11:38 +0000

    IIPP-111; Started implementation for retrieving IP ranges from Salesforce

--- src/Informa.Library.Salesforce/Company/SalesforceRetrieveActiveIpRanges.cs
+++ src/Informa.Library.Salesforce/Company/SalesforceRetrieveActiveIpRanges.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Informa.Library.Company;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using Informa.Library.Salesforce.EBIWebServices;
+using Informa.Library.Threading;
 
 namespace Informa.Library.Salesforce.Company
 {
-	public class SalesforceRetrieveActiveIpRanges : ISalesforceActiveIpRanges
+	public class SalesforceRetrieveActiveIpRanges : ThreadSafe<List<EBI_IPRange>>, ISalesforceActiveIpRanges, IFindCompanyByIpAddress
 	{
+		private DateTime LastRefresh;
+
 		protected readonly ISalesforceServiceContext Service;
 
 		public SalesforceRetrieveActiveIpRanges(
@@ -16,11 +20,30 @@ namespace Informa.Library.Salesforce.Company
 			Service = service;
 		}
 
-		public void Retrieve()
+		public ICompany Find(IPAddress ipAddress)
 		{
-			var response = Service.Execute(s => s.queryAllActiveIPRanges());
-
-			var ipRanges = response.ipRanges;
+			throw new NotImplementedException();
 		}
+
+		public List<EBI_IPRange> IpRanges => SafeObject;
+
+		protected override List<EBI_IPRange> UnsafeObject
+		{
+			get
+			{
+				var response = Service.Execute(s => s.queryAllActiveIPRanges());
+
+				LastRefresh = DateTime.Now;
+
+				if (response.IsSuccess())
+				{
+					return response.ipRanges.ToList();
+				}
+
+				return Enumerable.Empty<EBI_IPRange>().ToList();
+			}
+		}
+
+		public override bool IsValid => DateTime.Now.AddHours(-2) > LastRefresh;
 	}
 }
