0507dbb6fb897f7ef918a4919b388df6a897f361
Jason Bert <jason.bert@goblinrockets.com>
2016-03-11 17:24:15 +0000

    Implemented several Entity classes to replace the scheduled publish MongoDB implementation

---
+++ src/Informa.Library/Publishing/Scheduled/Entity/EntityDeleteScheduledPublish.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Informa.Library.Publishing.Scheduled.Entity
+{
+	public class EntityDeleteScheduledPublish : IDeleteScheduledPublish
+	{
+		protected readonly IEntityScheduledPublishContextFactory ContextFactory;
+
+		public EntityDeleteScheduledPublish(
+			IEntityScheduledPublishContextFactory contextFactory)
+		{
+			ContextFactory = contextFactory;
+		}
+
+		public void Delete(IScheduledPublish scheduledPublish)
+		{
+			using (var context = ContextFactory.Create())
+			{
+				context.ScheduledPublishes.RemoveRange(context.ScheduledPublishes.Where(sp =>
+					sp.ItemId == scheduledPublish.ItemId &&
+					string.Equals(sp.Language, (scheduledPublish.Language ?? string.Empty)) &&
+					string.Equals(sp.Version, (scheduledPublish.Version ?? string.Empty)) &&
+					sp.Type == scheduledPublish.Type
+				));
+				context.SaveChanges();
+			}
+		}
+	}
+}
