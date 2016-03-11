b2ed7528473346f2642e5616897a47ba20e4e38f
Chris Smith <chris.smith@velir.com>
2016-03-11 16:52:48 -0500

    Added logging to NLM validation. Still needs an email configured

---
+++ src/Informa.Library/Services/NlmExport/ExportResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Informa.Library.Services.NlmExport.Validation;
+
+namespace Informa.Library.Services.NlmExport
+{
+    public class ExportResult
+    {
+        public bool ExportSuccessful { get; set; }
+        public ValidationResult ValidationResult { get; set; } 
+        public Exception Exception { get; set; }
+    }
+}
--- src/Informa.Library/Services/NlmExport/INlmExportService.cs
+++ src/Informa.Library/Services/NlmExport/INlmExportService.cs
@@ -5,6 +5,23 @@ namespace Informa.Library.Services.NlmExport
 {
     public interface INlmExportService
     {
-        Stream ExportNlm(ArticleItem article);
+        Stream GenerateNlm(ArticleItem article, PublicationType? type = null);
+
+        ExportResult ExportNlm(ArticleItem article, ExportType exportType, PublicationType? type = null);
+
+        bool DeleteNlm(ArticleItem article);
+    }
+
+    public enum PublicationType
+    {
+        Epub,
+        Ecorrected,
+        Eretracted
+    }
+
+    public enum ExportType
+    {
+        Manual,
+        Scheduled
     }
 }
