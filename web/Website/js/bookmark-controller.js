eb243171f3d575b44214e2760ad226b070f90ed8
Samuel Fine <samuel.fine@velir.com>
2016-03-12 10:26:00 -0500

    WIP - Connects article page bookmark links to SaveItem API endpoint

--- web/Website/js/bookmark-controller.js
+++ web/Website/js/bookmark-controller.js
@@ -5,15 +5,63 @@ function bookmarkController() {
     // * * *
     this.toggle = function(e) {
 
-        var svg = $(e).find('use')[0];
-        var currentIcon = svg.getAttributeNS('http://www.w3.org/1999/xlink', 'href').split('#')[1];
+        var flipIcon = function() {
+            var svg = $(e).find('use')[0];
+            var currentIcon = svg.getAttributeNS('http://www.w3.org/1999/xlink', 'href').split('#')[1];
 
-    	if(currentIcon === 'bookmark') {
-    		svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmarked');
-    	} else {
-    		svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmark');
-    	}
-    }
-};
+            if(currentIcon === 'bookmark') {
+                svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmarked');
+            } else {
+                svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmark');
+            }
+        };
+
+        var apiData = {
+            DocumentID: $(e).closest('.js-bookmark-article').data('bookmark-id')
+        };
+
+        if(apiData.DocumentID) {
+
+            $.ajax({
+                url: '/Account/api/SavedDocumentApi/SaveItem/',
+                type: 'POST',
+                data: apiData,
+                context: this,
+                success: function (response) {
+                    if (response.success) {
+                        flipIcon();
+                    }
+                    else {
+                        
+                        /* if (response.reasons && response.reasons.length > 0) {
+                            for (var reason in response.reasons) {
+                                this.showError(form, '.js-form-error-' + response.reasons[reason]);
+                            }
+                        } else {
+                            this.showError(form, '.js-form-error-general');
+                        }
+
+                        if (failureCallback) {
+                            failureCallback(form);
+                        } */
+                    }
+                },
+                error: function(response) {
+
+                    /* this.showError(form, '.js-form-error-general');
+                    
+                    if (failureCallback) {
+                        failureCallback(form);
+                    } */
+                },
+                complete: function() {
+                    //$(formSubmit).removeAttr('disabled');
+                }
+
+            });
+
+        }
+    };
+}
 
 export default bookmarkController;
