75ac125924dedc5d29298ab13ca1a621fa2b167e
Samuel Fine <samuel.fine@velir.com>
2016-03-12 13:57:34 -0500

    Bookmarking articles works!

--- web/Website/js/bookmark-controller.js
+++ web/Website/js/bookmark-controller.js
@@ -5,65 +5,71 @@ function bookmarkController() {
     // * * *
     this.toggle = function(e) {
 
-        var flipIcon = function() {
-            var svg = $(e).find('use')[0];
-            var currentIcon = svg.getAttributeNS('http://www.w3.org/1999/xlink', 'href').split('#')[1];
-
-            if(currentIcon === 'bookmark') {
-                svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmarked');
-            } else {
-                svg.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/dist/img/svg-sprite.svg#bookmark');
-            }
+        var bookmark = {
+            elm: $(e)
         };
 
-        var apiData = {
-            DocumentID: $(e).closest('.js-bookmark-article').data('bookmark-id')
+        // ID of the article we're bookmarking or un-bookmarking
+        bookmark.id = bookmark.elm.closest('.js-bookmark-article').data('bookmark-id');
+
+        // Stash the bookmark icon now, update with the right icon later
+        bookmark.icon = bookmark.elm.find('use')[0];
+
+        // Stash the bookmark label data now, swap label text later
+        bookmark.label = {
+            elm: bookmark.elm.find('.js-bookmark-label')
         };
+        bookmark.label.bookmark = bookmark.label.elm.data('label-bookmark');
+        bookmark.label.bookmarked = bookmark.label.elm.data('label-bookmarked');
 
-        if(/* IS BOOKMARKED */) {
-            var endpoint = '/Account/api/SavedDocumentApi/RemoveItem/';
-        } else {
-            var endpoint = '/Account/api/SavedDocumentApi/SaveItem/';
-        }
 
-        if(apiData.DocumentID) {
+        // Are we bookmarking an article, or un-bookmarking?
+        // Used later to know what API endpoint to hit, and what DOM changes are required
+        bookmark.isBookmarking = bookmark.elm.data('is-bookmarked') ? false : true;
+
+        var apiEndpoint = bookmark.isBookmarking ? 
+            '/Account/api/SavedDocumentApi/SaveItem/' :
+            '/Account/api/SavedDocumentApi/RemoveItem/';
+
+        if(bookmark.id) {
 
             $.ajax({
-                url: endpoint,
+                url: apiEndpoint,
                 type: 'POST',
-                data: apiData,
+                data: {
+                    DocumentID: bookmark.id
+                },
                 context: this,
                 success: function (response) {
                     if (response.success) {
-                        flipIcon();
-                    }
-                    else {
-                        
-                        /* if (response.reasons && response.reasons.length > 0) {
-                            for (var reason in response.reasons) {
-                                this.showError(form, '.js-form-error-' + response.reasons[reason]);
-                            }
+
+                        if(bookmark.isBookmarking) {
+
+                            bookmark.icon.setAttributeNS(
+                                'http://www.w3.org/1999/xlink',
+                                'href', 
+                                '/dist/img/svg-sprite.svg#bookmarked');
+                            bookmark.elm.data('is-bookmarked', true);
+                            bookmark.label.elm.html(bookmark.label.bookmarked);
+                                
                         } else {
-                            this.showError(form, '.js-form-error-general');
+
+                            bookmark.icon.setAttributeNS(
+                                'http://www.w3.org/1999/xlink',
+                                'href',
+                                '/dist/img/svg-sprite.svg#bookmark');
+                            bookmark.elm.data('is-bookmarked', null);
+                            bookmark.label.elm.html(bookmark.label.bookmark);
+
                         }
 
-                        if (failureCallback) {
-                            failureCallback(form);
-                        } */
+                    }
+                    else {
+
                     }
                 },
-                error: function(response) {
-
-                    /* this.showError(form, '.js-form-error-general');
-                    
-                    if (failureCallback) {
-                        failureCallback(form);
-                    } */
-                },
-                complete: function() {
-                    //$(formSubmit).removeAttr('disabled');
-                }
-
+                error: function(response) { },
+                complete: function() { }
             });
 
         }
