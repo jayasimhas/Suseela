db029f937901be55599093cc1794171a49f6ea4b
Gabriel Boys <gabrielboys@gmail.com>
2016-03-17 09:29:14 -0400

    Updating the summary field with truncating the text and replacing the tokens in teh summary

--- src/Informa.Library/Search/ComputedFields/SearchResults/SearchResultSummaryField.cs
+++ src/Informa.Library/Search/ComputedFields/SearchResults/SearchResultSummaryField.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using Glass.Mapper.Sc;
+using Informa.Library.Utilities.StringUtils;
+using Informa.Library.Utilities.TokenMatcher;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 using Sitecore.Data.Items;
 using Velir.Search.Core.ComputedFields;
@@ -17,11 +19,28 @@ namespace Informa.Library.Search.ComputedFields.SearchResults
 
             var article = indexItem.GlassCast<IArticle>(inferType: true);
 
-            var summary = article.Summary.Trim();
+            var processedText = WordUtil.TruncateArticle(article.Summary, 20);
+            //processedText = ReplaceArticleTokens(processedText);
 
-            //Remove the HTML from the summary
-            var pattern = @"<(.|\n)*?>";
-            return Regex.Replace(summary, pattern, string.Empty);
+            return processedText;
+        }
+
+        /// <summary>
+        ///     Replace article and deal/company tokens in the summary
+        /// </summary>
+        /// <param name="bodyText"></param>
+        /// <returns></returns>
+        public string ReplaceArticleTokens(string bodyText)
+        {
+            TokenReplacer tokenReplacer = new TokenReplacer();
+
+            //Companies
+            string processedText = tokenReplacer.ReplaceCompany(bodyText);
+
+            //Articlees
+            processedText = tokenReplacer.ReplaceRelatedArticles(bodyText);
+
+            return processedText;
         }
     }
 }
