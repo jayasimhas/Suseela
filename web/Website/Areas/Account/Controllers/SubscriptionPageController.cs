0d6a0c0b6dcdd4d57ac74a56c3ecdb61d5964129
Mark Stiles <mark.stiles@velir.com>
2016-03-17 14:53:22 -0400

    IIPP-170 IIPP-171 *adding one-click subscribe and unsubscribe pages

--- web/Website/Areas/Account/Controllers/SubscriptionPageController.cs
+++ web/Website/Areas/Account/Controllers/SubscriptionPageController.cs
@@ -9,6 +9,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Informa.Library.Newsletter;
+using Informa.Library.User.Authentication;
+using Informa.Library.User.Profile;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.Account;
+using Informa.Web.ViewModels;
 
 namespace Informa.Web.Areas.Account.Controllers
 {
@@ -17,10 +22,31 @@ namespace Informa.Web.Areas.Account.Controllers
 		// GET: Account/SubscriptionPage
 		private readonly ISitecoreService _sitecoreMasterService;
 		private readonly IItemReferences _itemReferences;
-		public SubscriptionPageController(Func<string, ISitecoreService> sitecoreFactory, IItemReferences itemReferences)
+        private readonly ISitecoreContext SitecoreContext;
+        public readonly IAuthenticatedUserContext UserContext;
+        public readonly ISignInViewModel SignInViewModel;
+        protected readonly IUpdateNewsletterUserOptIn NewsletterOptIn;
+        protected readonly INewsletterUserOptInFactory NewsletterUserOptInFactory;
+        protected readonly IUpdateOfferUserOptIn OffersOptIn;
+
+        public SubscriptionPageController(
+            Func<string, ISitecoreService> sitecoreFactory, 
+            IItemReferences itemReferences,
+            ISitecoreContext sitecoreContext,
+            IAuthenticatedUserContext userContext,
+            ISignInViewModel signInViewModel,
+            IUpdateNewsletterUserOptIn newsletterOptIn,
+            INewsletterUserOptInFactory newsletterUserOptInFactory,
+            IUpdateOfferUserOptIn offersOptIn)
 		{
-			_sitecoreMasterService = sitecoreFactory(Constants.MasterDb);
+            _sitecoreMasterService = sitecoreFactory(Constants.MasterDb);
 			_itemReferences = itemReferences;
+		    SitecoreContext = sitecoreContext;
+            UserContext = userContext;
+            SignInViewModel = signInViewModel;
+            NewsletterOptIn = newsletterOptIn;
+            NewsletterUserOptInFactory = newsletterUserOptInFactory;
+            OffersOptIn = offersOptIn;
 		}
 		public ActionResult Index(string pub)
         {
@@ -34,6 +60,69 @@ namespace Informa.Web.Areas.Account.Controllers
 			}
 			var emailPreferncesItem = _sitecoreMasterService.GetItem<Item>(_itemReferences.EmailPreferences);
             return Redirect("/accounts/email-preferences"); // TODO Remove harcoded path
+        }
+        
+        public ActionResult Subscribe(string Pub)
+        {
+            string OneClickView = "~/Areas/Account/Views/Management/OneClickSubscribe.cshtml";
+            var page = SitecoreContext.GetCurrentItem<ISubscribe_Page>();
+            
+            SubscriptionModel s = new SubscriptionModel();
+            s.BodyText = page.Body.Replace("#USER_EMAIL#", UserContext?.User?.Username ?? string.Empty);
+            s.IsAuthenticated = UserContext.IsAuthenticated;
+            s.SignInViewModel = SignInViewModel;
+
+            if (!UserContext.IsAuthenticated)
+            {
+                //redirect to email preferences
+                string url = page?._Parent?._Url ?? string.Empty;
+                if (!string.IsNullOrEmpty(url))
+                    return Redirect(url);
+                else
+                    return View(OneClickView, s);
+            }
+
+            //process subscribe
+            if (!string.IsNullOrEmpty(Pub) 
+                && (Pub.ToLower() == NewsletterType.Scrip.ToDescriptionString().ToLower() || Pub.ToLower() == NewsletterType.Scrip.ToString().ToLower()))
+            {
+                var userNewsletterOptIns = new List<INewsletterUserOptIn>()
+                {
+                    NewsletterUserOptInFactory.Create(NewsletterType.Scrip, true)
+                };
+                NewsletterOptIn.Update(userNewsletterOptIns, UserContext.User.Username);
+            }
+            
+            return View(OneClickView, s);
+        }
+
+        public ActionResult Unsubscribe(string User, string Type, string Pub)
+        {
+            if (UserContext.IsAuthenticated && !string.IsNullOrEmpty(UserContext?.User?.Username))
+                User = UserContext.User.Username;
+
+            string OneClickView = "~/Areas/Account/Views/Management/OneClickUnsubscribe.cshtml";
+            var page = SitecoreContext.GetCurrentItem<IUnsubscribe_Page>();
+
+            SubscriptionModel s = new SubscriptionModel();
+            s.BodyText = page.Body;
+            s.SignInViewModel = SignInViewModel;
+
+            //process unsubscribe
+            if (string.IsNullOrEmpty(Type))
+                return View(OneClickView, s);
+
+            if(Type.ToLower() == "newsletter" && !string.IsNullOrEmpty(Pub) && (Pub.ToLower() == NewsletterType.Scrip.ToDescriptionString().ToLower() || Pub.ToLower() == NewsletterType.Scrip.ToString().ToLower())) {
+                var userNewsletterOptIns = new List<INewsletterUserOptIn>() {
+                    NewsletterUserOptInFactory.Create(NewsletterType.Scrip, false)
+                };
+                NewsletterOptIn.Update(userNewsletterOptIns, User);
+            }
+            else if (Type.ToLower() == "promotions") { 
+                OffersOptIn.Update(User, false);
+            }
+
+            return View(OneClickView, s);
         }
     }
 }
