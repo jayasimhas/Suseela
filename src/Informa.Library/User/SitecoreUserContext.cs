8eef3ba2fee5c4e6ae7c6d0d76175a606495df09
distaula <distaula@gmail.com>
2016-03-18 14:44:30 -0400

    Refactored entitlements.

--- src/Informa.Library/User/SitecoreUserContext.cs
+++ src/Informa.Library/User/SitecoreUserContext.cs
@@ -6,59 +6,63 @@ using Jabberwocky.Glass.Autofac.Attributes;
 
 namespace Informa.Library.User
 {
-	[AutowireService(LifetimeScope.SingleInstance)]
-	public class SitecoreUserContext : ISitecoreUserContext
-	{
-	    private readonly IUserSession UserSession;
-	    private readonly IGetUserEntitlements GetUserEntitlements;
-	    private readonly IUserIpAddressContext UserIpAddressContext;
-	    public SitecoreUserContext(IUserSession userSession, IGetUserEntitlements getUserEntitlements, IUserIpAddressContext userIpAddressContext)
-	    {  
+    [AutowireService(LifetimeScope.SingleInstance)]
+    public class SitecoreUserContext : ISitecoreUserContext
+    {
+        private readonly IUserSession UserSession;
+        private readonly IGetUserEntitlements GetUserEntitlements;
+        private readonly IUserIpAddressContext UserIpAddressContext;
+
+        public SitecoreUserContext(IUserSession userSession, IGetUserEntitlements getUserEntitlements,
+            IUserIpAddressContext userIpAddressContext)
+        {
             UserSession = userSession;
-	        GetUserEntitlements = getUserEntitlements;
-	        UserIpAddressContext = userIpAddressContext;
-	    }
+            GetUserEntitlements = getUserEntitlements;
+            UserIpAddressContext = userIpAddressContext;
+        }
+
         private const string EntitlementSessionKey = nameof(SitecoreUserContext);
         public Sitecore.Security.Accounts.User User => Context.User;
 
         #region Implementation of IEntitlementContext
 
-	    private IList<IEntitlement> entitlements = new List<IEntitlement>();
-	    public IList<IEntitlement> Entitlements
-	    {
-	        get
-	        {
-                if(!entitlements.Any())
-	                Entitlements = GetEntitlements().FirstOrDefault(x => x.Any());
+        public void RefreshEntitlements()
+        {
+            Entitlements = new List<IEntitlement>();
+        }
+
+        private IList<IEntitlement> entitlements = new List<IEntitlement>();
+        public IList<IEntitlement> Entitlements
+        {
+            get
+            {
+                if (!entitlements.Any())
+                    Entitlements = GetEntitlements().FirstOrDefault(x => x.Any());
                 return entitlements;
-	        }
-	        private set
-	        {
-	            entitlements = value;
-	            UserSession.Set($"{EntitlementSessionKey}", entitlements);
-	        }
-	    }
+            }
+            private set
+            {
+                entitlements = value;
+                UserSession.Set($"{EntitlementSessionKey}", entitlements);
+            }
+        }
 
-	    public void RefreshEntitlements()
-	    {
-	        Entitlements = new List<IEntitlement>();
-	    }
+        private IEnumerable<IList<IEntitlement>> GetEntitlements()
+        {
+            //Get from current instance
+            yield return entitlements;
+            if (User.IsAuthenticated)
+            {
+                //Get from Session
+                yield return UserSession.Get<IList<IEntitlement>>(EntitlementSessionKey);
+                //Get from Salesforce
+                yield return
+                    GetUserEntitlements.GetEntitlements(User.LocalName, UserIpAddressContext.IpAddress.ToString());
+            }
+            //Not entitled
+            yield return new List<IEntitlement> { new Entitlement.Entitlement { ProductCode = "NONE" } };
+        }                      
+    }
 
-	    private IEnumerable<IList<IEntitlement>> GetEntitlements()
-	    {
-	        //Get from current instance
-	        yield return entitlements;
-	        if (User.IsAuthenticated)
-	        {
-	            //Get from Session
-	            yield return UserSession.Get<IList<IEntitlement>>(EntitlementSessionKey);
-	            //Get from Salesforce
-	            yield return GetUserEntitlements.GetEntitlements(User.LocalName, UserIpAddressContext.IpAddress.ToString());
-	        }                                                                                                               
-	        //Not entitled
-	        yield return new List<IEntitlement> {new Entitlement.Entitlement {ProductCode = "NONE"}};
-	    }
-
-	    #endregion
-	}
+    #endregion
 }
