73316db45537022af45bdffa42a8faff9bcc1138
Mark Stiles <mark.stiles@velir.com>
2016-03-18 11:25:32 -0400

    *adding missing interface *updating the glass model with missing properties/classes

---
+++ web/Website/ViewModels/IIndividualRenewalMessageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Informa.Library.Globalization;
+using Informa.Library.Salesforce;
+using Informa.Library.Subscription;
+using Informa.Library.User.Authentication;
+
+namespace Informa.Web.ViewModels
+{
+    public interface IIndividualRenewalMessageViewModel
+    {
+        string DismissText { get; }
+
+        bool Display { get; }
+
+        string Id { get; }
+
+        string Message { get; }
+
+        string RenewURL { get; }
+
+        string RenewURLText { get; }
+    }
+}
