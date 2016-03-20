3bfe1882790b9cd0cd2a96cab22f1f27e7780662
Tamer Mehyar <tmehyar@tanasuk.com>
2016-03-20 15:36:37 +0200

    Individual Renewal Notification: read validation values from Site config instead of being hard coded.

--- web/Website/ViewModels/IndividualRenewalMessageViewModel.cs
+++ web/Website/ViewModels/IndividualRenewalMessageViewModel.cs
@@ -23,12 +23,14 @@ namespace Informa.Web.ViewModels
         IIndividualSubscriptionRenewalMessageContext _context;
         IAuthenticatedUserContext _userContext;
         EBI_SubscriptionAndPurchase _latestSalesForceRecord;
-        public IndividualRenewalMessageViewModel(ISalesforceServiceContext service, ITextTranslator textTranslator, IIndividualSubscriptionRenewalMessageContext context, IAuthenticatedUserContext userContext)
+        ISiteRootContext _siteRootContext;
+        public IndividualRenewalMessageViewModel(ISalesforceServiceContext service, ITextTranslator textTranslator, IIndividualSubscriptionRenewalMessageContext context, IAuthenticatedUserContext userContext, ISiteRootContext siteRootContext)
         {
             _service = service;
             _context = context;
             _textTranslator = textTranslator;
             _userContext = userContext;
+            _siteRootContext = siteRootContext;
             try
             {
                 if (_userContext.IsAuthenticated)
@@ -61,16 +63,18 @@ namespace Informa.Web.ViewModels
                 if (_latestSalesForceRecord == null)
                     return false;
 
-                if (_latestSalesForceRecord.productCode.ToLower() != "scrip")
+                if (_latestSalesForceRecord.productCode.ToLower() != _siteRootContext.Item.Product_Code.ToLower())
                     return false;
 
-                if ((_latestSalesForceRecord?.expirationDate - DateTime.Now)?.TotalDays < 190)
+                if ((_latestSalesForceRecord.expirationDate - DateTime.Now)?.TotalDays > _siteRootContext.Item.Days_To_Expiration)
                     return false;
 
-                if (_latestSalesForceRecord.subscriptionType.ToLower() != "individual" && _latestSalesForceRecord.subscriptionType.ToLower() != "free-trial")
+                string[] subsTypes = _siteRootContext.Item.Subscription_Type.ToLower().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (subsTypes == null || subsTypes.Contains(_latestSalesForceRecord.subscriptionType.ToLower()) == false)
                     return false;
 
-                if (_latestSalesForceRecord.productType != "publication")
+                if (_latestSalesForceRecord.productType != _siteRootContext.Item.Product_Type.ToLower())
                     return false;
 
                 return true;
@@ -89,14 +93,13 @@ namespace Informa.Web.ViewModels
         {
             get
             {
-                // return "IsAuthenticated:" + (_userContext.IsAuthenticated ? "true" : "false") + "|" +"productCode:" + _latestSalesForceRecord.productCode + "|" + "expirationDate: " + _latestSalesForceRecord.expirationDate.Value.ToString() + "|" + "subsType:" + _latestSalesForceRecord.subscriptionType + "|" + "productType" + _latestSalesForceRecord.productType;
-
-                if (_latestSalesForceRecord?.subscriptionType.ToLower() == "individual")
-                    return _context.Message_IndividualSubscriptiong.Replace("#FIRST_NAME#", _userContext.User.Name);
-                else if (_latestSalesForceRecord?.subscriptionType.ToLower() == "free-trial")
-                    return _context.Message_FreeTrial.Replace("#FIRST_NAME#", _userContext.User.Name);
-                else
+                if (_latestSalesForceRecord == null)
                     return string.Empty;
+
+                if (_latestSalesForceRecord.subscriptionType.ToLower() == "free-trial")
+                    return _context.Message_FreeTrial.Replace("#FIRST_NAME#", _userContext.User.Name).Replace("#SUB_EXPIRATION#", _latestSalesForceRecord.expirationDate.Value.ToShortDateString());
+                else
+                    return _context.Message_IndividualSubscriptiong.Replace("#FIRST_NAME#", _userContext.User.Name).Replace("#SUB_EXPIRATION#", _latestSalesForceRecord.expirationDate.Value.ToShortDateString());
             }
         }
 
