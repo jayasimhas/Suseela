1bac40ff050b6ab2926d36460d00a344c6fa1a4e
chetnakad <chetna.kadam@xpointers.com>
2016-03-21 17:40:45 +0530

    IIPP-306: Adding initial code for tealium implementation

--- web/Website/ViewModels/MainLayoutViewModel.cs
+++ web/Website/ViewModels/MainLayoutViewModel.cs
@@ -8,82 +8,95 @@ using Jabberwocky.Glass.Autofac.Mvc.Models;
 
 namespace Informa.Web.ViewModels
 {
-    public class MainLayoutViewModel : GlassViewModel<I___BasePage>
-    {
-        protected readonly ISiteRootContext SiteRootContext;
+	public class MainLayoutViewModel : GlassViewModel<I___BasePage>
+	{
+		protected readonly ISiteRootContext SiteRootContext;
 
-        public MainLayoutViewModel(
-            ISiteRootContext siteRootContext,
-            IMaintenanceViewModel maintenanceViewModel,
-            ICompanyRegisterMessageViewModel companyRegisterMessageViewModel,
-            ISideNavigationMenuViewModel sideNavigationMenuViewModel,
-            IHeaderViewModel headerViewModel,
-            IFooterViewModel footerViewModel,
-            ISignInPopOutViewModel signInPopOutViewModel,
-            IEmailArticlePopOutViewModel emailArticlePopOutViewModel,
-            IRegisterPopOutViewModel registerPopOutViewModel,
-            IAppInsightsConfig appInsightsConfig,
-            ISiteSettings siteSettings,
+		public MainLayoutViewModel(
+			ISiteRootContext siteRootContext,
+			IMaintenanceViewModel maintenanceViewModel,
+			ICompanyRegisterMessageViewModel companyRegisterMessageViewModel,
+			ISideNavigationMenuViewModel sideNavigationMenuViewModel,
+			IHeaderViewModel headerViewModel,
+			IFooterViewModel footerViewModel,
+			ISignInPopOutViewModel signInPopOutViewModel,
+			IEmailArticlePopOutViewModel emailArticlePopOutViewModel,
+			IRegisterPopOutViewModel registerPopOutViewModel,
+			IAppInsightsConfig appInsightsConfig,
+			ISiteSettings siteSettings,
 			IToolbarViewModel debugToolbar,
 			IIndividualRenewalMessageViewModel renewalInfo)
-        {
-            SiteRootContext = siteRootContext;
-            MaintenanceMessage = maintenanceViewModel;
-            CompanyRegisterMessage = companyRegisterMessageViewModel;
-            SideNavigationMenu = sideNavigationMenuViewModel;
-            Header = headerViewModel;
-            Footer = footerViewModel;
-            SignInPopOutViewModel = signInPopOutViewModel;
-            EmailArticlePopOutViewModel = emailArticlePopOutViewModel;
-            RegisterPopOutViewModel = registerPopOutViewModel;
-            AppInsightsConfig = appInsightsConfig;
-            SiteSettings = siteSettings;
+		{
+			SiteRootContext = siteRootContext;
+			MaintenanceMessage = maintenanceViewModel;
+			CompanyRegisterMessage = companyRegisterMessageViewModel;
+			SideNavigationMenu = sideNavigationMenuViewModel;
+			Header = headerViewModel;
+			Footer = footerViewModel;
+			SignInPopOutViewModel = signInPopOutViewModel;
+			EmailArticlePopOutViewModel = emailArticlePopOutViewModel;
+			RegisterPopOutViewModel = registerPopOutViewModel;
+			AppInsightsConfig = appInsightsConfig;
+			SiteSettings = siteSettings;
 			IndividualRenewalMessageInfo = renewalInfo;
 			DebugToolbar = debugToolbar;
-        }
+		}
 
-        public IIndividualRenewalMessageViewModel IndividualRenewalMessageInfo;
-        public IMaintenanceViewModel MaintenanceMessage;
-        public ICompanyRegisterMessageViewModel CompanyRegisterMessage;
-        public ISideNavigationMenuViewModel SideNavigationMenu;
-        public IFooterViewModel Footer;
-        public IHeaderViewModel Header;
-        public ISignInPopOutViewModel SignInPopOutViewModel;
-        public IEmailArticlePopOutViewModel EmailArticlePopOutViewModel;
-        public IRegisterPopOutViewModel RegisterPopOutViewModel;
-        public IAppInsightsConfig AppInsightsConfig;
-        public ISiteSettings SiteSettings;
+		public IIndividualRenewalMessageViewModel IndividualRenewalMessageInfo;
+		public IMaintenanceViewModel MaintenanceMessage;
+		public ICompanyRegisterMessageViewModel CompanyRegisterMessage;
+		public ISideNavigationMenuViewModel SideNavigationMenu;
+		public IFooterViewModel Footer;
+		public IHeaderViewModel Header;
+		public ISignInPopOutViewModel SignInPopOutViewModel;
+		public IEmailArticlePopOutViewModel EmailArticlePopOutViewModel;
+		public IRegisterPopOutViewModel RegisterPopOutViewModel;
+		public IAppInsightsConfig AppInsightsConfig;
+		public ISiteSettings SiteSettings;
 		public IToolbarViewModel DebugToolbar;
 
-        public string Title
-        {
-            get
-            {
-                var pageTitle = string.Empty;
+		public string Title
+		{
+			get
+			{
+				var pageTitle = string.Empty;
 
-                if (GlassModel is I___BasePage)
-                {
-                    var page = (I___BasePage)GlassModel;
+				if (GlassModel is I___BasePage)
+				{
+					var page = (I___BasePage)GlassModel;
 
-                    if (!string.IsNullOrEmpty(page.Meta_Title_Override))
-                    {
-                        return page.Meta_Title_Override.StripHtml();
-                    }
+					if (!string.IsNullOrEmpty(page.Meta_Title_Override))
+					{
+						return page.Meta_Title_Override.StripHtml();
+					}
 
-                    pageTitle = page.Title.StripHtml();
-                }
+					pageTitle = page.Title.StripHtml();
+				}
 
-                if (string.IsNullOrWhiteSpace(pageTitle) && GlassModel != null)
-                {
-                    pageTitle = GlassModel._Name;
-                }
+				if (string.IsNullOrWhiteSpace(pageTitle) && GlassModel != null)
+				{
+					pageTitle = GlassModel._Name;
+				}
 
-                var publicationName = SiteRootContext.Item == null ? string.Empty : string.Format(" :: {0}", SiteRootContext.Item.Publication_Name.StripHtml());
+				var publicationName = SiteRootContext.Item == null ? string.Empty : string.Format(" :: {0}", SiteRootContext.Item.Publication_Name.StripHtml());
 
-                return string.Concat(pageTitle, publicationName);
-            }
-        }
+				return string.Concat(pageTitle, publicationName);
+			}
+		}
 
-        public string CanonicalUrl => GlassModel?.Canonical_Link?.GetLink();
-    }
+		public string SiteEnvrionment
+		{
+			get
+			{
+				return SiteSettings.GetSetting("Env.Value", string.Empty);
+			}
+		}
+
+		public string PageType { get { return Sitecore.Context.Item.TemplateName; } }
+
+		public string CountryCode { get { return "123"; } }
+
+
+		public string CanonicalUrl => GlassModel?.Canonical_Link?.GetLink();
+	}
 }
