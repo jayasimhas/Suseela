dfc8020eb73fdef4161e45a5a24473ac9ac9ec99
Samuel Fine <samuel.fine@velir.com>
2016-04-01 10:23:20 -0400

    IIPP-890

--- web/Website/js/login-controller.js
+++ web/Website/js/login-controller.js
@@ -1,4 +1,5 @@
 function loginController(requestVerificationToken) {
+
 	this.addControl = function(triggerForm, successCallback, failureCallback) {
 
 		var triggerElement = $(triggerForm).find('button[type=submit]');
@@ -41,9 +42,7 @@ function loginController(requestVerificationToken) {
 
 						var result ={};
 						$.extend(result,analytics_data,loginAnalytics);
-				        //  utag.link({
-				        //    result
-				        //});
+				        utag.link(result);
 
 
 						if (successCallback) {
@@ -52,6 +51,10 @@ function loginController(requestVerificationToken) {
 
 						if($(triggerElement).data('login-redirect-url')) {
 							window.location.href = $(triggerElement).data('login-redirect-url');
+							// If Angular, need location.reload to force page refresh
+							if(angular) {
+								angular.element($('.search-results')[0]).controller().forceRefresh();
+							}
 						} else {
 							window.location.reload(false);
 						}
@@ -64,9 +67,7 @@ function loginController(requestVerificationToken) {
 						};
 				        var result ={};
 				        $.extend(result,analytics_data,loginAnalytics);
-				        //  utag.link({
-				        //    result
-				        //});
+				        utag.link(result);
 
 
 						if (response.redirectUrl) {
