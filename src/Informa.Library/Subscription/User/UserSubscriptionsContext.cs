14bc0cfd2f04989c191f982c2e732653ad0b2ced
Jason Bert <jason.bert@goblinrockets.com>
2016-04-06 13:33:16 +0100

    Used authenticated user session to cache subscriptions data

--- src/Informa.Library/Subscription/User/UserSubscriptionsContext.cs
+++ src/Informa.Library/Subscription/User/UserSubscriptionsContext.cs
@@ -10,16 +10,21 @@ namespace Informa.Library.Subscription.User
 	[AutowireService(LifetimeScope.SingleInstance)]
 	public class UserSubscriptionsContext : IUserSubscriptionsContext
 	{
+		private const string subscriptionsSessionKey = nameof(UserSubscriptionsContext);
+
 		protected readonly IAuthenticatedUserContext UserContext;
+		protected readonly IAuthenticatedUserSession UserSession;
 		protected readonly IManageSubscriptions ManageSubscriptions;
 		protected readonly ISubscriptionProductKeyContext SubscriptionProductKeyContext;
 
 		public UserSubscriptionsContext(
 			IAuthenticatedUserContext userContext,
+			IAuthenticatedUserSession userSession,
 			IManageSubscriptions manageSubscriptions,
 			ISubscriptionProductKeyContext subscriptionProductKeyContext)
 		{
 			UserContext = userContext;
+			UserSession = userSession;
 			ManageSubscriptions = manageSubscriptions;
 			SubscriptionProductKeyContext = subscriptionProductKeyContext;
 		}
@@ -28,11 +33,25 @@ namespace Informa.Library.Subscription.User
 		{
 			get
 			{
+				var subscriptionSession = UserSession.Get<IEnumerable<ISubscription>>(subscriptionsSessionKey);
+
+				if (subscriptionSession.HasValue)
+				{
+					return subscriptionSession.Value;
+				}
+
 				var result = ManageSubscriptions.QueryItems(UserContext.User);
 				var subscriptions = (result.Success)
 					? result.Subscriptions.Where(s => s.ProductType.Equals(SubscriptionProductKeyContext.ProductKey))
 					: Enumerable.Empty<ISubscription>();
+
+				Subscriptions = subscriptions;
+
 				return subscriptions;
+			}
+			set
+			{
+				UserSession.Set(subscriptionsSessionKey, value);
 			}
 		}
 	}
