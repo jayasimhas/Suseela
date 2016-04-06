aee6f5dc4afa4957bd21db898562d141fff896d0
Mark Stiles <mark.stiles@velir.com>
2016-04-06 11:11:08 -0400

    *moved required profile information from ISalesforceUserProfile to IUserProfile *added a clear method to the IUserProfileContext *updated the contact info section to access the IUserProfileContext for displaying, updating and clearing profile data

--- src/Informa.Library/User/Profile/IUserProfile.cs
+++ src/Informa.Library/User/Profile/IUserProfile.cs
@@ -5,6 +5,29 @@
 		string FirstName { get; }
 		string LastName { get; }
 		string Email { get; }
-		string JobIndustry { get; }
-	}
+		string MiddleInitial { get; set; }
+        string NameSuffix { get; set; }
+        string Salutation { get; set; }
+        string BillCountry { get; set; }
+        string BillAddress1 { get; set; }
+        string BillAddress2 { get; set; }
+        string BillCity { get; set; }
+        string BillPostalCode { get; set; }
+        string BillState { get; set; }
+        string ShipCountry { get; set; }
+        string ShipAddress1 { get; set; }
+        string ShipAddress2 { get; set; }
+        string ShipCity { get; set; }
+        string ShipPostalCode { get; set; }
+        string ShipState { get; set; }
+        string CountryCode { get; set; }
+        string Fax { get; set; }
+        string PhoneExtension { get; set; }
+        string Phone { get; set; }
+        string PhoneType { get; set; }
+        string Company { get; set; }
+        string JobFunction { get; set; }
+        string JobIndustry { get; set; }
+        string JobTitle { get; set; }
+    }
 }
