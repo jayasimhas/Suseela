d9acd40bb274f79158f60edbe1dbcc56e0edb5c6
Samuel Fine <samuel.fine@velir.com>
2016-04-13 14:12:33 -0400

    Performance - Concatenating Search/Angular JS files, reducing external dependencies

---
+++ web/Website/js/search/search.js
@@ -0,0 +1,10 @@
+import App from './informaSearchApp.js';
+import CompaniesFactory from './factories/companiesFactory.js';
+import HeadlineToggleController from './controllers/headlineToggleController.js';
+import InformaFacetsController from './controllers/informaFacetsController.js';
+import InformaDatesController from './controllers/informaDatesController.js';
+import InformaResultsController from './controllers/informaResultsController.js';
+import EmailRssController from './controllers/emailRssController.js';
+import TypeaheadController from './controllers/TypeaheadController.js';
+
+import AngularUIBootstrap from './ui-bootstrap-custom-tpls-1.2.4.min.js';
