f7682c541107bab88b2338fbcadba49591517cdd
Kevin Mazzoni <kevin.mazzoni@velir.com>
2016-04-14 16:05:54 -0400

    Updating search summary for general content pages.

--- src/Informa.Library/Search/Utilities/SearchSummaryUtil.cs
+++ src/Informa.Library/Search/Utilities/SearchSummaryUtil.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Informa.Library.Utilities.StringUtils;
 using Informa.Library.Utilities.TokenMatcher;
 using Informa.Models.DCD;
