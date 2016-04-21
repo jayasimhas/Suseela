a2fb44b91c49b31ae0a448b938e01789b586418d
distaula <distaula@gmail.com>
2016-04-21 13:38:52 -0400

    Moved some of the salesforce Logging to a debug mode.

---
+++ src/Informa.Library/Logging/IDebugLogger.cs
@@ -0,0 +1,7 @@
+namespace Informa.Library.Logging
+{
+    public interface IDebugLogger
+    {
+        void Log(string message);
+    }
+}
---
+++ src/Informa.Library/Logging/SitecoreDebugLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Informa.Library.Logging
+{
+    public class SitecoreDebugLogger : IDebugLogger
+    {
+        public void Log(string message)
+        {
+            Sitecore.Diagnostics.Log.Debug(message);
+        }
+    }
+}
