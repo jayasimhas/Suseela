b849beb7f31d4d6a9539d116262b03b5a77f3e1c
Aakash Shah <aakash.shah@velir.com>
2016-04-28 12:02:19 -0400

    Fixing the Publications getter and adding the new TS2 Publication Initials

--- src/Informa.Library/Utilities/References/Constants.cs
+++ src/Informa.Library/Utilities/References/Constants.cs
@@ -3,38 +3,57 @@ using System.Collections.Generic;
 
 namespace Informa.Library.Utilities.References
 {
-	public static class Constants
-	{
-		public const string ArticleNumberLength = "000000";
-		public const string MasterDb = "master";
-		public const string ScripPublicationName = "Scrip Intelligence";
+    public static class Constants
+    {
+        public const string ArticleNumberLength = "000000";
+        public const string MasterDb = "master";
 
-		public const string MediaLibraryRoot = "/sitecore/media library/";
-		public const string MediaLibraryPath = "Documents/";
+        public const string ContentRootNode = "{0DE95AE4-41AB-4D01-9EB0-67441B7C2450}";
 
-		public const string DTDPath = "Util\\DTD\\";
+        public const string ScripPublicationName = "Scrip Intelligence";
 
-		public const string ScripRootNode = "{3818C47E-4B75-4305-8F01-AB994150A1B0}";
-		public const string ScripRootNodeIntials = "SC";
-		public const string ScripEmailConfig = "{077AB43D-8F0F-413E-A585-4DB65EA23234}";
+        public const string MediaLibraryRoot = "/sitecore/media library/";
+        public const string MediaLibraryPath = "Documents/";
 
-		public const string MSWordDocumentRootNode = "{FDBFCAC8 -03CA-4B0B-BEFE-2171050E19C6}";
+        public const string DTDPath = "Util\\DTD\\";
 
-		public static string EditAfterPublishWorkflowCommand = "{322D0739-B3AC-416F-BF58-5E39E716F003}";
+        public const string ScripRootNode = "{3818C47E-4B75-4305-8F01-AB994150A1B0}";
+        public const string ScripRootNodeIntials = "SC";
+        public const string ScripEmailConfig = "{077AB43D-8F0F-413E-A585-4DB65EA23234}";
 
-		public static string ScripWorkflow = "{926E6200-EB76-4AD4-8614-691D002573AC}";
-		public static readonly Dictionary<Guid, string> PublicationPrefixDictionary = new Dictionary<Guid, string>
-		{
-			{ new Guid(ScripRootNode), ScripRootNodeIntials},
-		};
+        public const string InVivoRootNode = "{920FAC0D-4DA5-465A-94F9-A8B4D03DFCE3}";
+        public const string InVivoRootNodeIntials = "IV";
 
-		public static int[] PerPageSizes = { 10, 20, 50 };
+        public const string PinkSheetRootNode = "{87B710BC-BACD-4D38-895F-90F42D762393}";
+        public const string PinkSheetRootNodeIntials = "PS";
 
-		public static class QueryString
-		{
-			public const string InProgressKey = "inprogress";
-			public const string SearchHeadlinesOnly = "headlinesOnly";
-			public const string PerPageKey = "perPage";
-		}
-	}
+        public const string MedtechInsightRootNode = "{989CA96C-8A54-4CC0-BD51-A127930935A9}";
+        public const string MedtechInsightRootNodeIntials = "MT";
+
+        public const string RoseSheetRootNode = "{87B710BC-BACD-4D38-895F-90F42D762393}";
+        public const string RoseSheetNodeIntials = "RS";
+
+        public const string MSWordDocumentRootNode = "{FDBFCAC8 -03CA-4B0B-BEFE-2171050E19C6}";
+
+        public static string EditAfterPublishWorkflowCommand = "{322D0739-B3AC-416F-BF58-5E39E716F003}";
+
+        public static string ScripWorkflow = "{926E6200-EB76-4AD4-8614-691D002573AC}";
+        public static readonly Dictionary<Guid, string> PublicationPrefixDictionary = new Dictionary<Guid, string>
+        {
+            { new Guid(ScripRootNode), ScripRootNodeIntials},
+            { new Guid(InVivoRootNode), InVivoRootNodeIntials},
+            { new Guid(PinkSheetRootNode), PinkSheetRootNodeIntials},
+            { new Guid(MedtechInsightRootNode), MedtechInsightRootNodeIntials},
+            { new Guid(RoseSheetRootNode), RoseSheetNodeIntials},
+        };
+
+        public static int[] PerPageSizes = { 10, 20, 50 };
+
+        public static class QueryString
+        {
+            public const string InProgressKey = "inprogress";
+            public const string SearchHeadlinesOnly = "headlinesOnly";
+            public const string PerPageKey = "perPage";
+        }
+    }
 }
