ed1ac81da147f87c24511365d3f80b20da2282b7
Jason Bert <jason.bert@goblinrockets.com>
2016-04-29 15:21:22 +0100

    IITS2-57; functional implementation for setting newsletter email preferences

--- web/Website/Areas/Account/Controllers/PreferencesApiController.cs
+++ web/Website/Areas/Account/Controllers/PreferencesApiController.cs
@@ -3,6 +3,7 @@ using Informa.Library.User.Offer;
 using Informa.Library.User.Newsletter;
 using Informa.Web.Areas.Account.Models.User.Management;
 using Informa.Library.Utilities.WebApi.Filters;
+using System.Linq;
 
 namespace Informa.Web.Areas.Account.Controllers
 {
@@ -10,33 +11,44 @@ namespace Informa.Web.Areas.Account.Controllers
 	{
 		protected readonly IUpdateOfferUserOptInContext OffersOptIn;
 		protected readonly IUpdateSiteNewsletterUserOptIn UpdateSiteNewsletterOptIn;
-		protected readonly IUpdateSiteNewsletterUserOptInContext UpdateSiteNewsletterOptInContext;
 		protected readonly ISiteNewsletterUserOptedInContext NewsletterOptedInContext;
+		protected readonly INewsletterUserOptInsContext NewsletterUserOptInsContext;
+		protected readonly IUpdateNewsletterUserOptInsContext UpdateNewsletterOptInsContext;
 
 		public PreferencesApiController(
 			IUpdateOfferUserOptInContext offersOptIn,
 			IUpdateSiteNewsletterUserOptIn updateSiteNewsletterOptIn,
-			IUpdateSiteNewsletterUserOptInContext updateSiteNewsletterOptInContext,
-			ISiteNewsletterUserOptedInContext newsletterOptedInContext)
+			ISiteNewsletterUserOptedInContext newsletterOptedInContext,
+			INewsletterUserOptInsContext newsletterUserOptInsContext,
+			IUpdateNewsletterUserOptInsContext updateNewsletterOptInsContext)
 		{
 			OffersOptIn = offersOptIn;
 			UpdateSiteNewsletterOptIn = updateSiteNewsletterOptIn;
-			UpdateSiteNewsletterOptInContext = updateSiteNewsletterOptInContext;
 			NewsletterOptedInContext = newsletterOptedInContext;
+			NewsletterUserOptInsContext = newsletterUserOptInsContext;
+			UpdateNewsletterOptInsContext = updateNewsletterOptInsContext;
 		}
 
 		[HttpPost]
         [ArgumentsRequired]
         public IHttpActionResult Update(PreferencesRequest request)
 		{
-			var nResp = UpdateSiteNewsletterOptInContext.Update(request.NewsletterOptIn);
-			var oResp = OffersOptIn.Update(!request.DoNotSendOffersOptIn);
+			var newsletterUpdated = true;
 
-			var success = nResp && oResp;
+			if (request.NewsletterOptIns != null)
+			{
+				var newsletterUserOptins = NewsletterUserOptInsContext.OptIns.ToList();
+
+				newsletterUserOptins.ForEach(noi => noi.OptIn = request.NewsletterOptIns.Contains(noi.NewsletterType));
+
+				newsletterUpdated = UpdateNewsletterOptInsContext.Update(newsletterUserOptins);
+			}
+
+			var offersUpdated = OffersOptIn.Update(!request.DoNotSendOffersOptIn);
 
 			return Ok(new
 			{
-				success = success
+				success = newsletterUpdated && offersUpdated
 			});
 		}
 
