d9ab7764642d7898814a3320a2bffd1f5e431e97
Jason Bert <jason.bert@goblinrockets.com>
2016-05-13 12:46:51 +0100

    IITS2-121; added archive limitation to entitlement objects

--- src/Informa.Library.Salesforce/User/Entitlement/SalesforceEntitlmentFactory.cs
+++ src/Informa.Library.Salesforce/User/Entitlement/SalesforceEntitlmentFactory.cs
@@ -6,14 +6,36 @@ namespace Informa.Library.Salesforce.User.Entitlement
 	{
 		public SalesforceEntitlement Create(EBI_Entitlement entitlement)
 		{
+			var archiveLimitedDays = ArchiveCodeToDays(entitlement.ArchiveCode);
+			var archiveLimited = archiveLimitedDays != -1;
+
 			return new SalesforceEntitlement
 			{
 				ArchiveCode = entitlement.ArchiveCode,
+				ArchiveLimited = archiveLimited,
+				ArchiveLimitedDays = archiveLimitedDays,
 				DocumentId = entitlement.documentId,
 				ProductCode = entitlement.ProductCode,
 				ProductId = entitlement.productGUID,
 				ProductType = entitlement.ProductType
 			};
+		}
+
+		public int ArchiveCodeToDays(string archiveCode)
+		{
+			switch(archiveCode.ToLower())
+			{
+				case "full":
+				case "n/a":
+					return -1;
+				case "555":
+					return 540;
+				default:
+					var days = 0;
+					int.TryParse(archiveCode, out days);
+
+					return days;
+			}
 		}
 	}
 }
