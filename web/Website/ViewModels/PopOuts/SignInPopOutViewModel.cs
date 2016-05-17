9198b79a968b42fba98fdef9f707b0a6bbf2c3fb
awajeeh <aqarn@tanasuk.com>
2016-05-17 19:02:19 +0300

    Update Sign In Popup Header Content

--- web/Website/ViewModels/PopOuts/SignInPopOutViewModel.cs
+++ web/Website/ViewModels/PopOuts/SignInPopOutViewModel.cs
@@ -1,17 +1,20 @@
-﻿using Jabberwocky.Glass.Autofac.Attributes;
+﻿using Informa.Library.Globalization;
+using Jabberwocky.Glass.Autofac.Attributes;
 
 namespace Informa.Web.ViewModels.PopOuts
 {
-	[AutowireService(LifetimeScope.SingleInstance)]
-	public class SignInPopOutViewModel : ISignInPopOutViewModel
-	{
-		public SignInPopOutViewModel(
-			ISignInViewModel signInViewModel)
-		{
-			SignInViewModel = signInViewModel;
-		}
+    [AutowireService(LifetimeScope.SingleInstance)]
+    public class SignInPopOutViewModel : ISignInPopOutViewModel
+    {
+        protected readonly ITextTranslator TextTranslator;
+        public SignInPopOutViewModel(
+            ISignInViewModel signInViewModel, ITextTranslator textTranslator)
+        {
+            SignInViewModel = signInViewModel;
+            TextTranslator = textTranslator;
+        }
 
-		public string HeaderText => "You must sign in to use this functionality";
-		public ISignInViewModel SignInViewModel { get; set; }
-	}
+        public string HeaderText => TextTranslator.Translate("Authentication.SignIn.SignInPopupHeader");
+        public ISignInViewModel SignInViewModel { get; set; }
+    }
 }
