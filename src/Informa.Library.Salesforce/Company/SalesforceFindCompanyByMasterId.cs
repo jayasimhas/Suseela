cab035b0a93f768e4509ac9fca259aa8b3ccd7d1
Jason Bert <jason.bert@goblinrockets.com>
2016-05-19 11:25:52 +0100

    IITS2-56; Implemented Salesforce find company by master ID

---
+++ src/Informa.Library.Salesforce/Company/SalesforceFindCompanyByMasterId.cs
@@ -0,0 +1,38 @@
+﻿using Informa.Library.Company;
+using Informa.Library.Salesforce.EBIWebServices;
+
+namespace Informa.Library.Salesforce.Company
+{
+	public class SalesforceFindCompanyByMasterId : IFindCompanyByMasterId
+	{
+		protected readonly ISalesforceCompanyTypeFromAccountType CompanyTypeParser;
+		protected readonly ISalesforceServiceContext Service;
+
+		public SalesforceFindCompanyByMasterId(
+			ISalesforceCompanyTypeFromAccountType companyTypeParser,
+			ISalesforceServiceContext service)
+		{
+			CompanyTypeParser = companyTypeParser;
+			Service = service;
+		}
+
+		public ICompany Find(string masterid, string masterPassword)
+		{
+			var response = Service.Execute(s => s.queryAccountByMasterId(masterid, masterPassword));
+
+			if (!response.IsSuccess())
+			{
+				return null;
+			}
+
+			var account = response.account;
+
+			return new SalesforceCompany
+			{
+				Id = account.accountId,
+				Name = account.company,
+				Type = CompanyTypeParser.Parse(account.accountType)
+			};
+		}
+	}
+}
