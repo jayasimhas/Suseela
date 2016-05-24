b131577c9e6e78742c665452a1446cf0046b3ba1
felix.steiny <felix.steiny@pennwell.com>
2016-05-24 16:58:51 -0400

    #IITS2-245 making the ET button more responsive.  It's still super slow though.

--- src/Informa.Library/Mail/ExactTarget/ExactTargetWrapper.cs
+++ src/Informa.Library/Mail/ExactTarget/ExactTargetWrapper.cs
@@ -12,12 +12,15 @@ namespace Informa.Library.Mail.ExactTarget
     {
         ExactTargetResponse CreateEmail(ET_Email etEmail);
         ExactTargetResponse UpdateEmail(ET_Email etEmail);
+        string EtFrontEndUrl { get; }
     }
 
     [AutowireService]
     public class ExactTargetWrapper : IExactTargetWrapper
     {
         private readonly IDependencies _dependencies;
+
+        private const string EtFrontEndUrlFormat = "https://mc{0}.exacttarget.com/cloud/#app/Email/C12/Default.aspx?entityID=0%23Content";
 
         [AutowireService(true)]
         public interface IDependencies
@@ -30,14 +33,18 @@ namespace Informa.Library.Mail.ExactTarget
             _dependencies = dependencies;
         }
 
+        private bool IsSandbox => _dependencies.SiteSettings.GetSetting(Constants.SettingKeys.ExactTargetUseSandbox)
+            .Equals("true", StringComparison.InvariantCultureIgnoreCase);
+
+        public string EtFrontEndUrl => string.Format(
+            EtFrontEndUrlFormat, (IsSandbox ? ".test" : string.Empty));
+
+
         private ET_Client CreateClient()
         {
-            var isSandbox = _dependencies.SiteSettings.GetSetting(Constants.SettingKeys.ExactTargetUseSandbox)
-                .Equals("true", StringComparison.InvariantCultureIgnoreCase);
-
             var clientId = _dependencies.SiteSettings.GetSetting(Constants.SettingKeys.ExactTargetClientId);
             var clientSecret = _dependencies.SiteSettings.GetSetting(Constants.SettingKeys.ExactTargetSecretKey);
-            var sandbox = isSandbox ? "true" : "false";
+            var sandbox = IsSandbox ? "true" : "false";
 
             return
                 new ET_Client(new NameValueCollection
@@ -54,6 +61,12 @@ namespace Informa.Library.Mail.ExactTarget
             etEmail.AuthStub = client;
             var response = etEmail.Post();
             var result = response.Results.FirstOrDefault();
+
+            if (string.IsNullOrEmpty(response.Message))
+            {
+                response.Message = response.Status ? "Email Created" : "Failed to create email.";
+            }
+
             return new ExactTargetResponse
             {
                 Success = response.Status && result != null,
@@ -67,6 +80,11 @@ namespace Informa.Library.Mail.ExactTarget
             var client = CreateClient();
             etEmail.AuthStub = client;
             var response = etEmail.Patch();
+
+            if (string.IsNullOrEmpty(response.Message))
+            {
+                response.Message = response.Status ? "Email Updated" : "Failed to update email.";
+            }
 
             return new ExactTargetResponse
             {
