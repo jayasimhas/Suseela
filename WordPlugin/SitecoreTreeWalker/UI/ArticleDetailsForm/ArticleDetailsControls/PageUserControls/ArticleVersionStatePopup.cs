927ec7f62969e835aa931f6026a89bdff8d0f210
Tamer Mehyar <tmehyar@tanasuk.com>
2016-05-25 13:37:11 +0300

    Read latest article update info on refresh button

--- WordPlugin/SitecoreTreeWalker/UI/ArticleDetailsForm/ArticleDetailsControls/PageUserControls/ArticleVersionStatePopup.cs
+++ WordPlugin/SitecoreTreeWalker/UI/ArticleDetailsForm/ArticleDetailsControls/PageUserControls/ArticleVersionStatePopup.cs
@@ -33,25 +33,29 @@ namespace InformaSitecoreWord.UI.ArticleDetailsForm.ArticleDetailsControls.PageU
         private void refreshState()
         {
             var documentCustomProperties = new DocumentCustomProperties(SitecoreAddin.ActiveDocument);
-
-            var localDocVersion = documentCustomProperties.WordSitecoreVersionNumber;
-            var sitecoreDocVersion = SitecoreClient.GetWordVersionNumber(_parentForm.ArticleDetails.ArticleGuid);
-
-            if (sitecoreDocVersion > localDocVersion)
+            using (ArticleDetailFieldsUpdateDisabler disabler = new ArticleDetailFieldsUpdateDisabler())
             {
-                Color color = (Color)new ColorConverter().ConvertFromString("#F4CCCC");
-                this.BackColor = color;
-                lblTitle.Text = "This document is Outdated";
-            }
-            else
-            {
-                Color color = (Color)new ColorConverter().ConvertFromString("#d9ead3");
-                this.BackColor = color;
-                lblTitle.Text = "This document is Up to Date";
-            }
+                var updatedArticleDetail = new ArticleDetail();
 
-            lblBy.Text = _parentForm.ArticleDetails.WordDocLastUpdatedBy;
-            lblUpdatedOn.Text = SitecoreWordUtil.FormatUserName(Convert.ToDateTime(_parentForm.ArticleDetails.WordDocLastUpdateDate).ToLocalTime().ToString(CultureInfo.InvariantCulture));
+                var localDocVersion = documentCustomProperties.WordSitecoreVersionNumber;
+                var sitecoreDocVersion = SitecoreClient.GetWordVersionNumber(_parentForm.ArticleDetails.ArticleGuid);
+
+                if (sitecoreDocVersion > localDocVersion)
+                {
+                    Color color = (Color)new ColorConverter().ConvertFromString("#F4CCCC");
+                    this.BackColor = color;
+                    lblTitle.Text = "This document is Outdated";
+                }
+                else
+                {
+                    Color color = (Color)new ColorConverter().ConvertFromString("#d9ead3");
+                    this.BackColor = color;
+                    lblTitle.Text = "This document is Up to Date";
+                }
+
+                lblBy.Text = updatedArticleDetail.ArticleDetails.WordDocLastUpdatedBy;
+                lblUpdatedOn.Text = SitecoreWordUtil.FormatUserName(Convert.ToDateTime(updatedArticleDetail.ArticleDetails.WordDocLastUpdateDate).ToLocalTime().ToString(CultureInfo.InvariantCulture));
+            }
         }
 
         private void btnRefresh_Click(object sender, EventArgs e)
