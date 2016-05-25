6a6565ff806ec816cc3d9a968f7c8f8cdd4859d8
Tamer Mehyar <tmehyar@tanasuk.com>
2016-05-25 11:36:01 +0300

    Change Word Plugin Document's Version Popup design

---
+++ WordPlugin/SitecoreTreeWalker/UI/ArticleDetailsForm/ArticleDetailsControls/PageUserControls/ArticleVersionStatePopup.cs
@@ -0,0 +1,62 @@
+﻿using InformaSitecoreWord.document;
+using InformaSitecoreWord.Sitecore;
+using InformaSitecoreWord.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InformaSitecoreWord.UI.ArticleDetailsForm.ArticleDetailsControls.PageUserControls
+{
+    public partial class ArticleVersionStatePopup : Form
+    {
+        ArticleDetail _parentForm;
+        public ArticleVersionStatePopup(ArticleDetail parent)
+        {
+            InitializeComponent();
+            _parentForm = parent;
+            //d9ead3 green
+            //F4CCCC
+        }
+
+        private void ArticleVersionStatePopup_Load(object sender, EventArgs e)
+        {
+            refreshState();
+        }
+
+        private void refreshState()
+        {
+            var documentCustomProperties = new DocumentCustomProperties(SitecoreAddin.ActiveDocument);
+
+            var localDocVersion = documentCustomProperties.WordSitecoreVersionNumber;
+            var sitecoreDocVersion = SitecoreClient.GetWordVersionNumber(_parentForm.ArticleDetails.ArticleGuid);
+
+            if (sitecoreDocVersion > localDocVersion)
+            {
+                Color color = (Color)new ColorConverter().ConvertFromString("#F4CCCC");
+                this.BackColor = color;
+                lblTitle.Text = "This document is Outdated";
+            }
+            else
+            {
+                Color color = (Color)new ColorConverter().ConvertFromString("#d9ead3");
+                this.BackColor = color;
+                lblTitle.Text = "This document is Up to Date";
+            }
+
+            lblBy.Text = _parentForm.ArticleDetails.WordDocLastUpdatedBy;
+            lblUpdatedOn.Text = SitecoreWordUtil.FormatUserName(Convert.ToDateTime(_parentForm.ArticleDetails.WordDocLastUpdateDate).ToLocalTime().ToString(CultureInfo.InvariantCulture));
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            refreshState();
+        }
+    }
+}
