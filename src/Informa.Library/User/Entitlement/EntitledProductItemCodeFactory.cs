cd381cc983629233de929a16614378927445f2cf
Jason Bert <jason.bert@goblinrockets.com>
2016-05-27 11:57:50 +0100

    iITS2-229; updated entitled product item factory to use publication code factory

--- src/Informa.Library/User/Entitlement/EntitledProductItemCodeFactory.cs
+++ src/Informa.Library/User/Entitlement/EntitledProductItemCodeFactory.cs
@@ -1,4 +1,5 @@
-﻿using Informa.Library.Site;
+﻿using Informa.Library.Publication;
+using Informa.Library.Site;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Entitlement;
 using Jabberwocky.Autofac.Attributes;
 using System.Linq;
@@ -9,15 +10,21 @@ namespace Informa.Library.User.Entitlement
 	public class EntitledProductItemCodeFactory : IEntitledProductItemCodeFactory
 	{
 		protected readonly ISiteRootsContext SiteRootsContext;
+		protected readonly ISitePublicationCodeFactory PublicationCodeFactory;
+
 		public EntitledProductItemCodeFactory(
-			ISiteRootsContext siteRootsContext)
+			ISiteRootsContext siteRootsContext,
+			ISitePublicationCodeFactory publicationCodeFactory)
 		{
 			SiteRootsContext = siteRootsContext;
+			PublicationCodeFactory = publicationCodeFactory;
 		}
 
 		public string Create(IEntitled_Product item)
 		{
-			return SiteRootsContext.SiteRoots.FirstOrDefault(sr => item._Path.StartsWith(sr._Path))?.Publication_Name;
+			var siteRoot = SiteRootsContext.SiteRoots.FirstOrDefault(sr => item._Path.StartsWith(sr._Path));
+
+			return PublicationCodeFactory.Create(siteRoot);
 		}
 	}
 }
