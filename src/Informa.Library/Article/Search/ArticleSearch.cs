75eafcadbb1fe3a9c00423a771e5ae2379fc961f
Tamer Mehyar <tmehyar@tanasuk.com>
2016-05-31 16:22:21 +0300

    Revert "Make the URL read from article title instead of item name and allow for title updates"
    This reverts commit 95e4a03d749b043961cbdb128194f0e6a72a1319.

--- src/Informa.Library/Article/Search/ArticleSearch.cs
+++ src/Informa.Library/Article/Search/ArticleSearch.cs
@@ -14,187 +14,182 @@ using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects;
 
 namespace Informa.Library.Article.Search
 {
-    [AutowireService]
-    public class ArticleSearch : IArticleSearch
-    {
-        protected readonly IProviderSearchContextFactory SearchContextFactory;
-        protected readonly ISitecoreService SitecoreContext;
-        protected readonly Func<string, ISitecoreService> SitecoreFactory;
-        protected readonly IItemReferences ItemReferences;
+	[AutowireService]
+	public class ArticleSearch : IArticleSearch
+	{
+		protected readonly IProviderSearchContextFactory SearchContextFactory;
+		protected readonly ISitecoreService SitecoreContext;
+		protected readonly Func<string, ISitecoreService> SitecoreFactory;
+		protected readonly IItemReferences ItemReferences;
 
-        public ArticleSearch(
-            IProviderSearchContextFactory searchContextFactory,
-            ISitecoreService sitecoreContext, Func<string, ISitecoreService> sitecoreFactory,
-             IItemReferences itemReferences
-            )
-        {
-            SearchContextFactory = searchContextFactory;
-            SitecoreContext = sitecoreContext;
-            SitecoreFactory = sitecoreFactory;
-            ItemReferences = itemReferences;
-        }
+		public ArticleSearch(
+			IProviderSearchContextFactory searchContextFactory,
+			ISitecoreService sitecoreContext, Func<string, ISitecoreService> sitecoreFactory,
+			 IItemReferences itemReferences
+			)
+		{
+			SearchContextFactory = searchContextFactory;
+			SitecoreContext = sitecoreContext;
+			SitecoreFactory = sitecoreFactory;
+			ItemReferences = itemReferences;
+		}
 
-        public IArticleSearchFilter CreateFilter()
-        {
-            return new ArticleSearchFilter
-            {
-                ExcludeManuallyCuratedItems = new List<Guid>(),
-                TaxonomyIds = new List<Guid>(),
-                ArticleNumber = string.Empty
-            };
-        }
+		public IArticleSearchFilter CreateFilter()
+		{
+			return new ArticleSearchFilter
+			{
+				ExcludeManuallyCuratedItems = new List<Guid>(),
+				TaxonomyIds = new List<Guid>(),
+				ArticleNumber = string.Empty
+			};
+		}
 
-        public IArticleSearchResults Search(IArticleSearchFilter filter)
-        {
-            using (var context = SearchContextFactory.Create())
-            {
-                var query = context.GetQueryable<ArticleSearchResultItem>()
-                    .Filter(i => i.TemplateId == IArticleConstants.TemplateId)
-                    .FilterTaxonomies(filter)
-                    .ExcludeManuallyCurated(filter)
-                    .FilteryByArticleNumber(filter)
-                    .FilteryByEScenicID(filter)
-                    .FilteryByRelatedId(filter)
-                                        .FilteryByLatestVersionAndCurrentLanguage();
+		public IArticleSearchResults Search(IArticleSearchFilter filter)
+		{
+			using (var context = SearchContextFactory.Create())
+			{
+				var query = context.GetQueryable<ArticleSearchResultItem>()
+					.Filter(i => i.TemplateId == IArticleConstants.TemplateId)
+					.FilterTaxonomies(filter)
+					.ExcludeManuallyCurated(filter)
+					.FilteryByArticleNumber(filter)
+					.FilteryByEScenicID(filter)
+					.FilteryByRelatedId(filter)
+										.FilteryByLatestVersionAndCurrentLanguage();
 
-                if (filter.PageSize > 0)
-                {
-                    query = query.Page(filter.Page > 0 ? filter.Page - 1 : 0, filter.PageSize);
-                }
+				if (filter.PageSize > 0)
+				{
+					query = query.Page(filter.Page > 0 ? filter.Page - 1 : 0, filter.PageSize);
+				}
 
-                query = query.OrderByDescending(i => i.ActualPublishDate);
+				query = query.OrderByDescending(i => i.ActualPublishDate);
 
-                var results = query.GetResults();
+				var results = query.GetResults();
 
-                return new ArticleSearchResults
-                {
-                    Articles = results.Hits.Select(h => SitecoreContext.GetItem<IArticle>(h.Document.ItemId.Guid))
-                };
-            }
-        }
+				return new ArticleSearchResults
+				{
+					Articles = results.Hits.Select(h => SitecoreContext.GetItem<IArticle>(h.Document.ItemId.Guid))
+				};
+			}
+		}
 
-        /// <summary>
-        /// This is a search implementatation where you want to pass the database name along with the filter.
-        /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="database"></param>
-        /// <returns></returns>
-        public IArticleSearchResults SearchCustomDatabase(IArticleSearchFilter filter, string database)
-        {
-            using (var context = SearchContextFactory.Create(database))
-            {
-                var query = context.GetQueryable<ArticleSearchResultItem>()
-                    .Filter(i => i.TemplateId == IArticleConstants.TemplateId)
-                    .FilterTaxonomies(filter)
-                    .ExcludeManuallyCurated(filter)
-                    .FilteryByArticleNumber(filter)
-                    .FilteryByEScenicID(filter)
-                                        .FilteryByLatestVersionAndCurrentLanguage();
+		/// <summary>
+		/// This is a search implementatation where you want to pass the database name along with the filter.
+		/// </summary>
+		/// <param name="filter"></param>
+		/// <param name="database"></param>
+		/// <returns></returns>
+		public IArticleSearchResults SearchCustomDatabase(IArticleSearchFilter filter, string database)
+		{
+			using (var context = SearchContextFactory.Create(database))
+			{
+				var query = context.GetQueryable<ArticleSearchResultItem>()
+					.Filter(i => i.TemplateId == IArticleConstants.TemplateId)
+					.FilterTaxonomies(filter)
+					.ExcludeManuallyCurated(filter)
+					.FilteryByArticleNumber(filter)
+					.FilteryByEScenicID(filter)
+										.FilteryByLatestVersionAndCurrentLanguage();
 
-                if (filter.PageSize > 0)
-                {
-                    query = query.Page(filter.Page > 0 ? filter.Page - 1 : 0, filter.PageSize);
-                }
+				if (filter.PageSize > 0)
+				{
+					query = query.Page(filter.Page > 0 ? filter.Page - 1 : 0, filter.PageSize);
+				}
 
-                query = query.OrderByDescending(i => i.ActualPublishDate);
-                ISitecoreService localSearchContext = SitecoreFactory(database);
-                var results = query.GetResults();
-                return new ArticleSearchResults
-                {
-                    Articles = results.Hits.Select(h => localSearchContext.GetItem<IArticle>(h.Document.ItemId.Guid))
-                };
-            }
-        }
+				query = query.OrderByDescending(i => i.ActualPublishDate);
+				ISitecoreService localSearchContext = SitecoreFactory(database);
+				var results = query.GetResults();
+				return new ArticleSearchResults
+				{
+					Articles = results.Hits.Select(h => localSearchContext.GetItem<IArticle>(h.Document.ItemId.Guid))
+				};
+			}
+		}
 
-        public long GetNextArticleNumber(Guid publicationGuid)
-        {
-            using (var context = SearchContextFactory.Create("master"))
-            {
-                var filter = CreateFilter();
+		public long GetNextArticleNumber(Guid publicationGuid)
+		{
+			using (var context = SearchContextFactory.Create("master"))
+			{
+				var filter = CreateFilter();
 
-                var query = context.GetQueryable<ArticleSearchResultItem>()
-                    .Filter(i => i.TemplateId == IArticleConstants.TemplateId)
-                    .FilterTaxonomies(filter)
-                    //.Max(x => x.ArticleIntegerNumber);
-                    .OrderByDescending(i => i.ArticleIntegerNumber)
-                    .Take(1);
+				var query = context.GetQueryable<ArticleSearchResultItem>()
+					.Filter(i => i.TemplateId == IArticleConstants.TemplateId)
+					.FilterTaxonomies(filter)
+					//.Max(x => x.ArticleIntegerNumber);
+					.OrderByDescending(i => i.ArticleIntegerNumber)
+					.Take(1);
 
-                var results = query.GetResults();
-
-
-                return results.Hits.FirstOrDefault().Document.ArticleIntegerNumber + 1;
+				var results = query.GetResults();
 
 
-                //var results = articleSearchResultItem.GetResults();
+				return results.Hits.FirstOrDefault().Document.ArticleIntegerNumber + 1;
 
 
-                //var articleResults2 = context.GetQueryable<ArticleSearchResultItem>()     Max(x => x.ArticleIntegerNumber);
+				//var results = articleSearchResultItem.GetResults();
 
 
-                //return results.Hits.FirstOrDefault().Document.ArticleIntegerNumber;
-            }
-            return 0;
-        }
-
-        /// <summary>
-        /// This method gets the Publication Prefix which is used in Article Number Generation.
-        /// </summary>
-        /// <param name="publicationGuid"></param>
-        /// <returns></returns>
-        public static string GetPublicationPrefix(Guid publicationGuid)
-        {
-            string value;
-            return Constants.PublicationPrefixDictionary.TryGetValue(publicationGuid, out value) ? value : null;
-        }
-
-        public static string GetCleansedArticleTitle(IArticle a)
-        {
-            return Sitecore.Data.Items.ItemUtil.ProposeValidItemName(a.Title.Trim()).Replace(" ", "-");
-        }
-
-        public static string GetArticleCustomPath(IArticle a)
-        {
-            string procName = GetCleansedArticleTitle(a);
-            return $"/{a.Article_Number}/{procName}";
-        }
-
-        public string GetArticleAuthors(Guid id)
-        {
-            var item = SitecoreContext.GetItem<ArticleItem>(id);
-            if (item?.Authors != null)
-            {
-                StringBuilder str = new StringBuilder();
-                foreach (IStaff_Item author in item.Authors)
-                {
-                    if (str.Length > 0)
-                        str.Append(",");
-                    str.Append($"'{author._Name.Trim()}'");
-                }
-                return $"[{str}]";
-            }
-
-            return string.Empty;
-        }
-
-        public string GetArticleTaxonomies(Guid id, Guid taxonomyParent)
-        {
-            var article = SitecoreContext.GetItem<ArticleItem>(id);
-            var taxonomyItems = article?.Taxonomies?.Where(x => x._Parent._Id.Equals(taxonomyParent));
-            if (taxonomyItems != null)
-            {
-                StringBuilder str = new StringBuilder();
-                foreach (ITaxonomy_Item taxonomyItem in taxonomyItems)
-                {
-                    if (str.Length > 0)
-                        str.Append(",");
-                    str.Append($"'{taxonomyItem.Item_Name.Trim()}'");
-                }
-                return $"[{str}]";
-            }
-
-            return string.Empty;
-        }
+				//var articleResults2 = context.GetQueryable<ArticleSearchResultItem>()     Max(x => x.ArticleIntegerNumber);
 
 
-    }
+				//return results.Hits.FirstOrDefault().Document.ArticleIntegerNumber;
+			}
+			return 0;
+		}
+
+		/// <summary>
+		/// This method gets the Publication Prefix which is used in Article Number Generation.
+		/// </summary>
+		/// <param name="publicationGuid"></param>
+		/// <returns></returns>
+		public static string GetPublicationPrefix(Guid publicationGuid)
+		{
+			string value;
+			return Constants.PublicationPrefixDictionary.TryGetValue(publicationGuid, out value) ? value : null;
+		}
+
+		public static string GetArticleCustomPath(IArticle a)
+		{
+			string procName = a._Name.Replace(" ", "-");
+			return $"/{a.Article_Number}/{procName}";
+		}
+
+		public string GetArticleAuthors(Guid id)
+		{
+			var item = SitecoreContext.GetItem<ArticleItem>(id);
+			if (item?.Authors != null)
+			{
+				StringBuilder str = new StringBuilder();
+				foreach (IStaff_Item author in item.Authors)
+				{
+					if (str.Length > 0)
+						str.Append(",");
+					str.Append($"'{author._Name.Trim()}'");
+				}
+				return $"[{str}]";
+			}
+
+			return string.Empty;
+		}
+
+		public string GetArticleTaxonomies(Guid id, Guid taxonomyParent)
+		{
+			var article = SitecoreContext.GetItem<ArticleItem>(id);
+			var taxonomyItems = article?.Taxonomies?.Where(x => x._Parent._Id.Equals(taxonomyParent));
+			if (taxonomyItems != null)
+			{
+				StringBuilder str = new StringBuilder();
+				foreach (ITaxonomy_Item taxonomyItem in taxonomyItems)
+				{
+					if (str.Length > 0)
+						str.Append(",");
+					str.Append($"'{taxonomyItem.Item_Name.Trim()}'");
+				}
+				return $"[{str}]";
+			}
+
+			return string.Empty;
+		}
+
+
+	}
 }
--- src/Informa.Library/CustomSitecore/Pipelines/HttpRequest/ArticleItemResolver.cs
+++ src/Informa.Library/CustomSitecore/Pipelines/HttpRequest/ArticleItemResolver.cs
@@ -12,57 +12,57 @@ using Sitecore.Pipelines.HttpRequest;
 
 namespace Informa.Library.CustomSitecore.Pipelines.HttpRequest
 {
-    public class ArticleItemResolver : IProcessor<HttpRequestArgs>
-    {
+	public class ArticleItemResolver : IProcessor<HttpRequestArgs>
+	{
 
-        protected readonly IArticleSearch ArticleSearcher;
-        protected readonly ISitecoreContext SitecoreContext;
+		protected readonly IArticleSearch ArticleSearcher;
+		protected readonly ISitecoreContext SitecoreContext;
 
-        public ArticleItemResolver(IArticleSearch searcher, ISitecoreContext context)
-        {
-            ArticleSearcher = searcher;
-            SitecoreContext = context;
-        }
+		public ArticleItemResolver(IArticleSearch searcher, ISitecoreContext context)
+		{
+			ArticleSearcher = searcher;
+			SitecoreContext = context;
+		}
 
-        public void Process(HttpRequestArgs args)
-        {
-            Assert.ArgumentNotNull((object)args, "args");
-            if (Context.Item != null || Context.Database == null || args.Url.ItemPath.Length == 0)
-                return;
+		public void Process(HttpRequestArgs args)
+		{
+			Assert.ArgumentNotNull((object)args, "args");
+			if (Context.Item != null || Context.Database == null || args.Url.ItemPath.Length == 0)
+				return;
 
-            Regex r = new Regex(@"^(.*)(/home/)(sc\d{6})(/)(.*)");
-            MatchCollection mc = r.Matches(args.Url.ItemPath);
-            if (mc.Count < 1 || mc[0].Groups.Count < 6)
-                return;
+			Regex r = new Regex(@"^(.*)(/home/)(sc\d{6})(/)(.*)");
+			MatchCollection mc = r.Matches(args.Url.ItemPath);
+			if (mc.Count < 1 || mc[0].Groups.Count < 6)
+				return;
 
-            string numFormat = mc[0].Groups[3].Value;
+			string numFormat = mc[0].Groups[3].Value;
 
-            //find the new article page
-            IArticleSearchFilter filter = ArticleSearcher.CreateFilter();
-            filter.PageSize = 1;
-            filter.Page = 1;
-            filter.ArticleNumber = numFormat;
+			//find the new article page
+			IArticleSearchFilter filter = ArticleSearcher.CreateFilter();
+			filter.PageSize = 1;
+			filter.Page = 1;
+			filter.ArticleNumber = numFormat;
 
-            var results = ArticleSearcher.Search(filter);
-            if (!results.Articles.Any())
-                return;
+			var results = ArticleSearcher.Search(filter);
+			if (!results.Articles.Any())
+				return;
 
-            IArticle a = results.Articles.First();
-            if (a == null)
-                return;
+			IArticle a = results.Articles.First();
+			if (a == null)
+				return;
 
-            string matchTitle = mc[0].Groups[5].Value;
-            string urlTitle = ArticleSearch.GetCleansedArticleTitle(a);
-            if (!urlTitle.Equals(matchTitle, System.StringComparison.InvariantCultureIgnoreCase))
-                HttpContext.Current.Response.RedirectPermanent(urlTitle);
+			string matchTitle = mc[0].Groups[5].Value;
+			string urlTitle = a._Name.ToLower().Replace(" ", "-");
+			if (!urlTitle.Equals(matchTitle))
+				HttpContext.Current.Response.RedirectPermanent(ArticleSearch.GetArticleCustomPath(a));
 
-            Item i = SitecoreContext.GetItem<Item>(a._Id);
-            if (i == null)
-                return;
+			Item i = SitecoreContext.GetItem<Item>(a._Id);
+			if (i == null)
+				return;
 
-            Context.Item = i;
-            args.Url.ItemPath = i.Paths.FullPath;
-            Context.Request.ItemPath = i.Paths.FullPath;
-        }
-    }
+			Context.Item = i;
+			args.Url.ItemPath = i.Paths.FullPath;
+			Context.Request.ItemPath = i.Paths.FullPath;
+		}
+	}
 }
