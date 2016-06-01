abf7a3f29e2e420acc7ef1e005b2f2f3bb7d9c79
Kevin Mazzoni <kevin.mazzoni@velir.com>
2016-06-01 16:41:58 -0400

    Removing caching from services due to NREs.
    Not sure extra caching is necessary as the companies and deals are cached and the component has HTML cache enabled.

--- src/Informa.Library/Article/Companies/IRelatedDealsService.cs
+++ src/Informa.Library/Article/Companies/IRelatedDealsService.cs
@@ -1,52 +1,11 @@
 using System.Collections.Generic;
-using System.Linq;
 using Glass.Mapper.Sc.Fields;
-using Informa.Library.Utilities.Settings;
-using Informa.Model.DCD;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
-using Jabberwocky.Autofac.Attributes;
-using Jabberwocky.Core.Caching;
 
 namespace Informa.Library.Article.Companies
 {
 	public interface IRelatedDealsService
 	{
 		IEnumerable<Link> GetRelatedDeals(IArticle article);
-	}
-
-	[AutowireService(LifetimeScope.PerScope)]
-	public class RelatedDealsService : IRelatedDealsService
-	{
-		private readonly IDCDReader _reader;
-		private readonly string _oldDealsUrl;
-	    private readonly ICacheProvider _cacheProvider;
-
-	    public RelatedDealsService(
-            IDCDReader reader, 
-            ISiteSettings siteSettings,
-            ICacheProvider cacheProvider)
-		{
-			_reader = reader;
-			_oldDealsUrl = siteSettings.OldDealsUrl;
-		    _cacheProvider = cacheProvider;
-
-		}
-
-	    public IEnumerable<Link> GetRelatedDeals(IArticle article)
-	    {
-            string cacheKey = $"{nameof(RelatedDealsService)}-RelatedDeals-{article._Id}";
-            return _cacheProvider.GetFromCache(cacheKey, () => BuildRelatedDeals(article));
-        }
-
-        private IEnumerable<Link> BuildRelatedDeals(IArticle article) {
-
-            var dealIds = article?.Referenced_Deals?.Split(',') ?? new string[0];
-
-			return dealIds.Where(id => !string.IsNullOrEmpty(id)).Select(id => _reader.GetDealByRecordNumber(id)).Select(c => new Link
-			{
-				Text = c.Title,
-				Url = string.Format(_oldDealsUrl, c.RecordNumber)
-			}).ToArray();
-		}
 	}
 }
--- src/Informa.Library/Article/Companies/RelatedCompaniesService.cs
+++ src/Informa.Library/Article/Companies/RelatedCompaniesService.cs
@@ -5,7 +5,6 @@ using Informa.Library.Utilities.Settings;
 using Informa.Model.DCD;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 using Jabberwocky.Autofac.Attributes;
-using Jabberwocky.Core.Caching;
 
 namespace Informa.Library.Article.Companies
 {
@@ -14,28 +13,19 @@ namespace Informa.Library.Article.Companies
 	{
 		private readonly IDCDReader _reader;
 		private readonly string _oldCompaniesUrl;
-	    private readonly ICacheProvider _cacheProvider;
-
+		
 		public RelatedCompaniesService(
-            IDCDReader reader, 
-            ISiteSettings siteSettings,
-            ICacheProvider cacheProvider)
+						IDCDReader reader,
+						ISiteSettings siteSettings)
 		{
 			_reader = reader;
 			_oldCompaniesUrl = siteSettings.OldCompaniesUrl;
-		    _cacheProvider = cacheProvider;
 
 		}
-        
-        public IEnumerable<Link> GetRelatedCompanyLinks(IArticle article)
-	    {
-            string cacheKey = $"{nameof(RelatedCompaniesService)}-RelatedCompanyLinks-{article._Id}";
-            return _cacheProvider.GetFromCache(cacheKey, () => BuildRelatedCompanyLinks(article));
-        }
 
-        private IEnumerable<Link> BuildRelatedCompanyLinks(IArticle article) { 
-
-            var companyIds = article?.Referenced_Companies?.Split(',') ?? new string[0];
+		public IEnumerable<Link> GetRelatedCompanyLinks(IArticle article)
+		{
+			var companyIds = article?.Referenced_Companies?.Split(',') ?? new string[0];
 
 			return companyIds.Where(id => !string.IsNullOrEmpty(id)).Select(id => _reader.GetCompanyByRecordNumber(id)).Select(c => new Link
 			{
