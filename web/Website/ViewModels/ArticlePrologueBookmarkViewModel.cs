c2dbff47f1c1cef687e55845a5b08cf256f211e2
Huxu Tian <huxu.tian@velir.com>
2016-06-07 16:40:20 -0400

    [IITS2-266]: Added bookmark_title and book_publication to view model.

--- web/Website/ViewModels/ArticlePrologueBookmarkViewModel.cs
+++ web/Website/ViewModels/ArticlePrologueBookmarkViewModel.cs
@@ -23,12 +23,34 @@ namespace Informa.Web.ViewModels
 			Article = articleRenderingContext.Get<IArticle>();
 			IsUserAuthenticated = authenticatedUserContext.IsAuthenticated;
 			IsArticleBookmarked = IsUserAuthenticated && isSavedDocuementContext.IsSaved(Article._Id);
+			BookmarkPublication = GetPublicationType();
+            BookmarkTitle = Article?.Title;
 		}
 
 		public IArticle Article { get; set; }
 		public bool IsUserAuthenticated { get; set; }
 		public bool IsArticleBookmarked { get; set; }
 		public string BookmarkText => TextTranslator.Translate("Bookmark");
+		public string BookmarkTitle { get; }
+		public string BookmarkPublication { get; }
 		public string BookmarkedText => TextTranslator.Translate("Bookmarked");
+
+		private string GetPublicationType()
+		{
+			var prefix = Article?.Article_Number.Substring(0, 2);
+			switch (prefix)
+			{
+				case "IV":
+					return "InVivo";
+				case "PS":
+					return "Pink";
+				case "MT":
+					return "Medtech";
+				case "RS":
+					return "Rose";
+				default:
+					return "";
+			}
+		}
 	}
 }
