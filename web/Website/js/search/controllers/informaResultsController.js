7299a9e28961e0609465c23fcb56a44deaae6047
Samuel Fine <samuel.fine@velir.com>
2016-06-08 11:58:59 -0400

    IITS2-334

--- web/Website/js/search/controllers/informaResultsController.js
+++ web/Website/js/search/controllers/informaResultsController.js
@@ -37,19 +37,9 @@ var InformaResultsController = function InformaResultsController($scope, $saniti
     };
 
     $scope.fireBookmark = function (article, event, key) {
-        /*  Global bookmark controller fires a second click if clicked element
-            isn't the element with the actual click event. (i.e. a child element)
-            To make sure we only catch the second click, make sure the element
-            has the appropriate class name. */
-        if (event.target.nodeName === 'DIV' && event.target.className.indexOf('result__bookmark') >= 0) {
-            /*  Angular is faster than the generic bookmark controller; it has to
-                wait for an AJAX response before updating the bookmark UI. Use
-                $timeout to prevent a race condition.
-                TODO - better way to pass bookmark state to generic controller instead */
-            $timeout(function () {
-                vm.docs[key].isArticleBookmarked = vm.docs[key].isArticleBookmarked ? false : true;
-            }, 250);
-        }
+        $timeout(function () {
+			vm.docs[key].isArticleBookmarked = vm.docs[key].isArticleBookmarked ? false : true;
+		}, 500);
     };
 
     $scope.$on('refreshPopOuts', function (ngRepeatFinishedEvent) {
