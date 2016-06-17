539fe2a830bf4584cec405816aa264afc5621eda
Huxu Tian <huxu.tian@velir.com>
2016-06-17 13:59:57 -0400

    [IITS2A-181]: Add ArchiveIssue function to IssueService.

--- src/Informa.Library/VirtualWhiteboard/IssuesService.cs
+++ src/Informa.Library/VirtualWhiteboard/IssuesService.cs
@@ -7,6 +7,7 @@ using Informa.Library.VirtualWhiteboard.Models;
 using Informa.Library.Wrappers;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Virtual_Whiteboard;
 using Jabberwocky.Autofac.Attributes;
+using Jabberwocky.Glass.Models;
 
 namespace Informa.Library.VirtualWhiteboard
 {
@@ -15,32 +16,32 @@ namespace Informa.Library.VirtualWhiteboard
         VwbResponseModel CreateIssueFromModel(IssueModel model);
     }
 
-    [AutowireService]
-    public class IssuesService : IIssuesService
-    {
-        private readonly IDependencies _dependencies;
+	[AutowireService]
+	public class IssuesService : IIssuesService
+	{
+		private readonly IDependencies _dependencies;
 
-        [AutowireService(IsAggregateService = true)]
-        public interface IDependencies
-        {
-            ISitecoreServiceMaster SitecoreServiceMaster { get; }
-            ISitecoreSecurityWrapper SitecoreSecurityWrapper { get; }
-            ISitecoreClonesWrapper SitecoreClonesWrapper { get; }
-        }
+		[AutowireService(IsAggregateService = true)]
+		public interface IDependencies
+		{
+			ISitecoreServiceMaster SitecoreServiceMaster { get; }
+			ISitecoreSecurityWrapper SitecoreSecurityWrapper { get; }
+			ISitecoreClonesWrapper SitecoreClonesWrapper { get; }
+		}
 
-        public IssuesService(IDependencies dependencies)
-        {
-            _dependencies = dependencies;
-        }
+		public IssuesService(IDependencies dependencies)
+		{
+			_dependencies = dependencies;
+		}
 
-        public VwbResponseModel CreateIssueFromModel(IssueModel model)
-        {
-            model.Title = model.Title.HasContent() ? model.Title : "New Issue";
+		public VwbResponseModel CreateIssueFromModel(IssueModel model)
+		{
+			model.Title = model.Title.HasContent() ? model.Title : "New Issue";
 
             try
             {
-                var issueId = CreateIssueItem(model.Title);
-                UpdateIssueItem(model, issueId);
+			var issueId = CreateIssueItem<IIssue, IIssue_Folder>(model.Title, Constants.VirtualWhiteboardIssuesFolder);
+            UpdateIssueItem(model, issueId);
             }
             catch (Exception ex)
             {
@@ -55,43 +56,89 @@ namespace Informa.Library.VirtualWhiteboard
             }
 
             return new VwbResponseModel {IsSuccess = true};
-        }
+		}
 
-        public Guid CreateIssueItem(string newIssueName)
-        {
-            var issuesFolder =
-                _dependencies.SitecoreServiceMaster.GetItem<IIssue_Folder>(Constants.VirtualWhiteboardIssuesFolder);
+		public Guid CreateIssueItem<I, F>(string newIssueName, string folderId) where I : class, IGlassBase
+			where F : class, IGlassBase
+		{
+			var issuesFolder =
+				_dependencies.SitecoreServiceMaster.GetItem<F>(folderId);
 
-            var newIssue = _dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
-                _dependencies.SitecoreServiceMaster.Create<IIssue, IIssue_Folder>(issuesFolder, newIssueName));
+			var newIssue = _dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
+				_dependencies.SitecoreServiceMaster.Create<I, F>(issuesFolder, newIssueName));
 
-            return newIssue._Id;    
-            //Due to a limitation with Glass & RTE fields, this item must be refetched before editing.
-            //Thus we only return the Guid.
-        }
+			return newIssue._Id;
+			//Due to a limitation with Glass & RTE fields, this item must be refetched before editing.
+			//Thus we only return the Guid.
+		}
 
-        public void UpdateIssueItem(IssueModel model, Guid issueId)
-        {
+		public void UpdateIssueItem(IssueModel model, Guid issueId)
+		{
             var issue = _dependencies.SitecoreServiceMaster.GetItem<IIssue__Raw>(issueId);
 
-            if(issue == null) { return; }
+			if (issue == null)
+			{
+				return;
+			}
 
-            issue.Title = model.Title;
-            issue.Published_Date = model.PublishedDate;
-            issue.Notes = model.Notes;
+			issue.Title = model.Title;
+			issue.Published_Date = model.PublishedDate;
+			issue.Notes = model.Notes;
 
-            _dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
-                _dependencies.SitecoreServiceMaster.Save(issue));
+			_dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
+				_dependencies.SitecoreServiceMaster.Save(issue));
 
             AddArticlesToIssue(issue._Id, model.ArticleIds);
-        }
+		}
 
         public void AddArticlesToIssue(Guid issueId, IEnumerable<Guid> itemIds)
-        {
-            if (itemIds == null) return;
+		{
+			if (itemIds == null) return;
 
-            _dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
+			_dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() =>
                 itemIds.Each(id => _dependencies.SitecoreClonesWrapper.CreateClone(issueId, id)));
-        }
-    }
+		}
+
+		public VwbResponseModel ArchiveIssue(Guid issueId)
+		{
+			var issue = _dependencies.SitecoreServiceMaster.GetItem<IIssue>(issueId);
+			if (issue == null)
+			{
+				return new VwbResponseModel
+				{
+					DebugErrorMessage = "Issue is null.",
+					FrientdlyErrorMessage = "Can't find issue.",
+					IsSuccess = false
+				};
+			}
+
+			// Create archived issue item
+			var newIssueId = CreateIssueItem<IArchived_Issue, IArchived_Issue_Folder>(issue._Name,
+				Constants.VirtualWhiteboardIssuesFolder);
+			var newIssue = _dependencies.SitecoreServiceMaster.GetItem<IArchived_Issue>(newIssueId);
+			if (newIssue == null)
+			{
+				return new VwbResponseModel
+				{
+					DebugErrorMessage = "newIssue is null.",
+					FrientdlyErrorMessage = "Create archived issue failed.",
+					IsSuccess = false
+				};
+			}
+
+			// Set values
+			newIssue.Title = issue.Title;
+			newIssue.Published_Date = issue.Published_Date;
+			newIssue.Notes = issue.Notes;
+			newIssue.Issue_Articles = issue._ChildrenWithInferType;
+
+			// Delete issue with children
+			_dependencies.SitecoreSecurityWrapper.WithSecurityDisabled(() => _dependencies.SitecoreServiceMaster.Delete(issue));
+
+			return new VwbResponseModel
+			{
+				IsSuccess = true
+			};
+		}
+	}
 }
