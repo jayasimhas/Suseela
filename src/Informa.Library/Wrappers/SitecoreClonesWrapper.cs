a064a2bd6a2d097343e6c44408a50aaa94026f21
felix.steiny <felix.steiny@pennwell.com>
2016-06-21 15:41:41 -0400

    Add IsDeep option to Clones wrapper.  Defaults false.

--- src/Informa.Library/Wrappers/SitecoreClonesWrapper.cs
+++ src/Informa.Library/Wrappers/SitecoreClonesWrapper.cs
@@ -6,18 +6,18 @@ namespace Informa.Library.Wrappers
 {
     public interface ISitecoreClonesWrapper
     {
-        void CreateClone(Guid sourceId, Guid desitnationParentId, bool IsDeep = false);
+        void CreateClone(Guid sourceId, Guid desitnationParentId, bool isDeep = false);
     }
 
     [AutowireService]
     public class SitecoreClonesWrapper : BaseSitecoreWrapper, ISitecoreClonesWrapper
     {
-        public void CreateClone(Guid sourceId, Guid desitnationParentId, bool IsDeep=false)
+        public void CreateClone(Guid sourceId, Guid desitnationParentId, bool isDeep=false)
         {
             var sourceItem = GetItem(sourceId, Constants.MasterDb);
             var destinationItem = GetItem(desitnationParentId, Constants.MasterDb);
 
-            sourceItem.CloneTo(destinationItem, IsDeep);
+            sourceItem.CloneTo(destinationItem, isDeep);
         }
     }
 }
