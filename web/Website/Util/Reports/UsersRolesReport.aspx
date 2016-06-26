5d779f7d7e3cb670d9544368158e910cbd254808
Tamer Mehyar <tmehyar@tanasuk.com>
2016-06-26 17:39:47 +0300

    Add Users/Roles Report

---
+++ web/Website/Util/Reports/UsersRolesReport.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UsersRolesReport.aspx.cs" Inherits="Informa.Web.Util.Reports.UsersRolesReport" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="../DCD/styles/style.css" rel="stylesheet" />
+    <style>
+        #content table.data th a {
+            color: White;
+        }
+
+        #content table.data th.sortup a {
+            background: url("/img/arrow_sort_up_active.png") no-repeat right center;
+            padding-right: 15px;
+        }
+
+        #content table.data th.sortdown a {
+            background: url("/img/arrow_sort_down_active.png") no-repeat right center;
+            padding-right: 15px;
+        }
+
+        #content table.data.clickable tr:hover td {
+            background: #F4BC50;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    </style>
+    <title></title>
+
+</head>
+<body class="fullpage">
+    <form id="form2" runat="server">
+        <div>
+            <div id="wrapper">
+                <div id="innerwrapper">
+                    <!-- Header -->
+                    <div id="header">
+                        <!-- Logo -->
+
+                    </div>
+                    <!-- End header -->
+                    <div id="contentwrapper">
+                        <div id="breadcrumb">
+                        </div>
+                        <div id="innercontentwrapper">
+                            <div id="content">
+                                <h2 class="title">Users/Roles Report</h2>
+                                <div>
+                                    <asp:DataGrid runat="server" AutoGenerateColumns="true" ID="grdUsersRoles"></asp:DataGrid>
+                                </div>
+                                <asp:LinkButton runat="server" ID="lnkExport" Text="Download as Excel" OnClick="lnkExport_Click"></asp:LinkButton>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ web/Website/Util/Reports/UsersRolesReport.aspx.cs
@@ -0,0 +1,92 @@
+﻿using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Informa.Web.Util.Reports
+{
+    public partial class UsersRolesReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var allUsers = DomainManager.GetDomain("sitecore").GetUsers();
+                List<UsersRolesReportViewModel> lstUsers = new List<UsersRolesReportViewModel>();
+
+                foreach (var item in allUsers)
+                {
+                    var obj = new UsersRolesReportViewModel();
+                    obj.UserName = item.Name;
+
+                    var roles = Sitecore.Security.Accounts.RolesInRolesManager.GetRolesForUser(item, true);
+                    obj.Roles = string.Join(", ", roles.Select(s => s.DisplayName));
+
+                    lstUsers.Add(obj);
+                }
+
+                grdUsersRoles.DataSource = lstUsers;
+                grdUsersRoles.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error(ex.ToString(), this);
+            }
+        }
+
+        void ExportToExcel(DataTable dt, string FileName)
+        {
+            if (dt.Rows.Count > 0)
+            {
+                string filename = FileName + ".xls";
+                System.IO.StringWriter tw = new System.IO.StringWriter();
+                System.Web.UI.HtmlTextWriter hw = new System.Web.UI.HtmlTextWriter(tw);
+                DataGrid dgGrid = new DataGrid();
+                dgGrid.DataSource = dt;
+                dgGrid.DataBind();
+
+                //Get the HTML for the control.
+                dgGrid.RenderControl(hw);
+                //Write the HTML back to the browser.
+                //Response.ContentType = application/vnd.ms-excel;
+                Response.ContentType = "application/vnd.ms-excel";
+                Response.AppendHeader("Content-Disposition",
+                                      "attachment; filename=" + filename + "");
+                this.EnableViewState = false;
+                Response.Write(tw.ToString());
+                Response.End();
+            }
+        }
+
+        protected void lnkExport_Click(object sender, EventArgs e)
+        {
+            var allUsers = DomainManager.GetDomain("sitecore").GetUsers();
+            DataTable dt = new DataTable("Users/Roles Report");
+            dt.Columns.Add("UserName");
+            dt.Columns.Add("Roles");
+
+            foreach (var item in allUsers)
+            {
+                var drow = dt.NewRow();
+                drow["UserName"] = item.Name;
+
+                var roles = Sitecore.Security.Accounts.RolesInRolesManager.GetRolesForUser(item, true);
+                drow["Roles"] = string.Join(", ", roles.Select(s => s.DisplayName));
+
+                dt.Rows.Add(drow);
+            }
+
+            ExportToExcel(dt, "UsersRolesReport");
+        }
+    }
+
+    public class UsersRolesReportViewModel
+    {
+        public string UserName { get; set; }
+        public string Roles { get; set; }
+    }
+}
