4ee0b8064acffe9d5d8bcb5ec6137f024872336b
felix.steiny <felix.steiny@pennwell.com>
2016-07-01 20:35:08 -0400

    Initial commit, untested due to SF issues.

---
+++ web/Website/ViewModels/Authors/AuthorProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Jabberwocky.Autofac.Attributes;
+
+namespace Informa.Web.ViewModels.Authors
+{
+    public class AuthorProfileViewModel
+    {
+        private readonly IDependencies _dependencies;
+
+        [AutowireService(IsAggregateService = true)]
+        public interface IDependencies
+        {
+
+        }
+        public AuthorProfileViewModel(IDependencies dependencies)
+        {
+            _dependencies = dependencies;
+        }
+
+
+    }
+}
