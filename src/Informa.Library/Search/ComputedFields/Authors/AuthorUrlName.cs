25585176caff63b3fe59a9f3bc55d687b1cfa75a
felix.steiny <felix.steiny@pennwell.com>
2016-07-05 17:06:37 -0400

    Working small (enough) authors index!  Still need to figure out how to filter out inactive authors.

---
+++ src/Informa.Library/Search/ComputedFields/Authors/AuthorUrlName.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+
+namespace Informa.Library.Search.ComputedFields.Authors
+{
+    public class AuthorUrlName : IComputedIndexField
+	{
+		public string FieldName { get; set; }
+		public string ReturnType { get; set; }
+
+		public object ComputeFieldValue(IIndexable indexable)
+		{
+            Item item = indexable as SitecoreIndexableItem;
+            if (item == null)
+            {
+                return null;
+            }
+
+		    var name = item.DisplayName.ToLower();
+		    var nonChars = new Regex(@"[^a-z]+");
+		    name = nonChars.Replace(name, "-");
+		    name = name.Trim('-');
+            
+			return name;
+		}
+	}
+}
