b99d520e37bb8026c945ae19fd33952b3b0f8689
felix.steiny <felix.steiny@pennwell.com>
2016-07-06 15:21:28 -0400

    Updating ViewModel to use Author

--- web/Website/ViewModels/Authors/AuthorProfileViewModel.cs
+++ web/Website/ViewModels/Authors/AuthorProfileViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Web;
 using Informa.Library.Authors;
-using System;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects;
 using Jabberwocky.Autofac.Attributes;
 using Jabberwocky.Glass.Autofac.Mvc.Models;
@@ -20,37 +19,41 @@ namespace Informa.Web.ViewModels.Authors
         public AuthorProfileViewModel(IDependencies dependencies)
         {
             _dependencies = dependencies;
+
             Author = _dependencies.AuthorIndexClient.GetAuthorByUrlName(NameFromUrl);
-            if (Author == null)
-            {
-                var curUrl = HttpContext.Current.Request.RawUrl;
-                HttpContext.Current.Response.Redirect($"/404?url={curUrl}", true);
-            }
+
+            if( Author == null ) { HandleNullAuthor(); }
+        }
+
+        private void HandleNullAuthor()
+        {
+            var curUrl = HttpContext.Current.Request.RawUrl;
+            HttpContext.Current.Response.Redirect($"/404?url={curUrl}", true);
         }
 
         public IStaff_Item Author { get; }
         private string NameFromUrl => HttpContext.Current.Request.Url.Segments.Last();
         
-	    public string FirstName => GlassModel.First_Name ?? string.Empty;
-		public string LastName => GlassModel.Last_Name ?? string.Empty;
-		public string Credentials => GlassModel.Credentials ?? string.Empty;
-	    public string Location => GlassModel.Location ?? string.Empty;
+	    public string FirstName => Author.First_Name ?? string.Empty;
+		public string LastName => Author.Last_Name ?? string.Empty;
+		public string Credentials => Author.Credentials ?? string.Empty;
+	    public string Location => Author.Location ?? string.Empty;
 
-	    public bool HasTwitter => GlassModel.Twitter?.Length > 0;
+	    public bool HasTwitter => Author.Twitter?.Length > 0;
 		public string TwitterHandle => HasTwitter ? 
-			( "@" + GlassModel.Twitter.Replace("@","")) : string.Empty;
+			( "@" + Author.Twitter.Replace("@","")) : string.Empty;
 		public string TwitterUrl => HasTwitter ?
-			("https://twitter.com/" + GlassModel.Twitter.Replace("@", "")) : string.Empty;
+			("https://twitter.com/" + Author.Twitter.Replace("@", "")) : string.Empty;
 		
-		public bool HasEmail => GlassModel.Email_Address?.Length > 0;
-		public string Email => GlassModel.Email_Address ?? string.Empty;
+		public bool HasEmail => Author.Email_Address?.Length > 0;
+		public string Email => Author.Email_Address ?? string.Empty;
 
-	    public bool HasImage => GlassModel.Image != null;
-		public string ImageUrl => HasImage ? GlassModel.Image.Src : string.Empty;
+	    public bool HasImage => Author.Image != null;
+		public string ImageUrl => HasImage ? Author.Image.Src : string.Empty;
 
-	    public bool HasLinkedIn => GlassModel.LinkedIn != null;
-	    public string LinkedInUrl => HasLinkedIn ? (GlassModel.LinkedIn.Url) : string.Empty;
+	    public bool HasLinkedIn => Author.LinkedIn != null;
+	    public string LinkedInUrl => HasLinkedIn ? (Author.LinkedIn.Url) : string.Empty;
 
-	    public string Bio => GlassModel.Bio;
+	    public string Bio => Author.Bio;
     }
 }
