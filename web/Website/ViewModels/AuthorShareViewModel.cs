d6e876f49f73bd57f9a5e17c278c21a3f420d55c
ashwin-keshwaram <ashwin.keshwaram@velir.com>
2016-07-08 15:13:22 -0400

    Save work in progress for the share bar

---
+++ web/Website/ViewModels/AuthorShareViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Informa.Models.FactoryInterface;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
+using Jabberwocky.Glass.Models;
+
+namespace Informa.Web.ViewModels
+{
+	public class AuthorShareViewModel : GlassViewModel<IGlassBase>
+	{
+		public AuthorShareViewModel(
+			IArticlePrologueEmailViewModel articlePrologueEmailViewModel,
+			IArticlePrologueShareViewModel articlePrologueShareViewModel
+			)
+		{
+			EmailViewModel = articlePrologueEmailViewModel;
+			ShareViewModel = articlePrologueShareViewModel;
+		}
+
+		public IArticlePrologueEmailViewModel EmailViewModel;
+
+		public IArticlePrologueShareViewModel ShareViewModel;
+	}
+}
