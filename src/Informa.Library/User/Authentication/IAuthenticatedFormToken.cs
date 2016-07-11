e0d866998250bc0da07a2ffc8b56264ca6f70a30
sshafei <sshafei@tanasuk.com>
2016-07-11 17:22:21 +0300

    Added new authentication system to avoid phising hacks

---
+++ src/Informa.Library/User/Authentication/IAuthenticatedFormToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informa.Library.User.Authentication
+{
+    public interface IAuthenticatedFormToken
+    {
+        string GenerateMD5Token();
+        string currentToken { get; set; }
+    }
+}
