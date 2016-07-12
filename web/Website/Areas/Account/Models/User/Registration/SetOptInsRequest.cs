27f060053d122aec7ff59923234a4b46665c332d
Tamer Mehyar <tmehyar@tanasuk.com>
2016-07-12 12:30:27 +0300

    Show all publications optin checkbox on registration

--- web/Website/Areas/Account/Models/User/Registration/SetOptInsRequest.cs
+++ web/Website/Areas/Account/Models/User/Registration/SetOptInsRequest.cs
@@ -1,8 +1,16 @@
-﻿namespace Informa.Web.Areas.Account.Models.User.Registration
+﻿using System.Collections.Generic;
+
+namespace Informa.Web.Areas.Account.Models.User.Registration
 {
-	public class SetOptInsRequest
-	{
-		public bool Offers { get; set; }
-		public bool Newsletters { get; set; }
-	}
+    public class SetOptInsRequest
+    {
+        public bool Offers { get; set; }
+        public IEnumerable<PublicationNewslettersOptIns> Newsletters { get; set; }
+    }
+
+    public class PublicationNewslettersOptIns
+    {
+        public string PublicationCode { get; set; }
+        public bool NewsletterChecked { get; set; }
+    }
 }
