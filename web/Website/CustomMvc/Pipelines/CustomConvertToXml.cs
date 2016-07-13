5efb37e01a6b07852287df7983fa1d18a247c753
ashwin-keshwaram <ashwin.keshwaram@velir.com>
2016-07-13 17:16:40 -0400

    Add custom parser to parse p tags

---
+++ web/Website/CustomMvc/Pipelines/CustomConvertToXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using Sitecore.Layouts;
+using Sitecore.PrintStudio.PublishingEngine.Pipelines.ConvertHtmlToXml;
+using Sitecore.PrintStudio.PublishingEngine.Text.Parsers.Html;
+
+namespace Informa.Web.CustomMvc.Pipelines
+{
+	public class CustomConvertToXml
+	{
+		public void Process(HtmlConvertPipelineArgs args)
+		{
+			if (string.IsNullOrEmpty(args.InputText))
+				return;
+			new CustomHtmlNodeParser().ParseNode(XHtml.LoadHtmlDocument(args.InputText, false).DocumentNode, args.OutputXml, args.ParseContext, (XElement)null);
+		}
+	}
+}
