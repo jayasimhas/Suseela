bc87a6f0c5eb3722e21941083253daa9481c001b
Mark Stiles <mark.stiles@velir.com>
2016-07-13 17:07:08 -0400

    IITS2A-220 *replacing all references to the <%= with string values or web controls

--- web/Website/VWB/default.aspx.designer.cs
+++ web/Website/VWB/default.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace Elsevier.Web.VWB {
         protected global::System.Web.UI.WebControls.Button NewIssueSubmitButton;
         
         /// <summary>
+        /// imgLogo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgLogo;
+        
+        /// <summary>
         /// rbNoDate control.
         /// </summary>
         /// <remarks>
