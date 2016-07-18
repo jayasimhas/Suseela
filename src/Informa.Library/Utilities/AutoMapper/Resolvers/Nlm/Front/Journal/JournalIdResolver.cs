fa442525c2063286f67e38771324d6f3002074dc
Chris Sulham <chris.sulham@velir.com>
2016-07-18 14:09:47 -0400

    Throwing exceptions when the journal ID or journal title can not be resolved when generating NLM.  This is per Informa's request.

--- src/Informa.Library/Utilities/AutoMapper/Resolvers/Nlm/Front/Journal/JournalIdResolver.cs
+++ src/Informa.Library/Utilities/AutoMapper/Resolvers/Nlm/Front/Journal/JournalIdResolver.cs
@@ -6,28 +6,38 @@ using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 
 namespace Informa.Library.Utilities.AutoMapper.Resolvers.Nlm.Front.Journal
 {
-    public class JournalIdResolver : BaseValueResolver<ArticleItem, NlmJournalIdModel>
-    {
-        private readonly IGlobalSitecoreService _globalService;
+	public class JournalIdResolver : BaseValueResolver<ArticleItem, NlmJournalIdModel>
+	{
+		private readonly IGlobalSitecoreService _globalService;
 
-        public JournalIdResolver(IGlobalSitecoreService globalService)
-        {
-            if (globalService == null) throw new ArgumentNullException(nameof(globalService));
-            _globalService = globalService;
-        }
+		public JournalIdResolver(IGlobalSitecoreService globalService)
+		{
+			if (globalService == null) throw new ArgumentNullException(nameof(globalService));
+			_globalService = globalService;
+		}
 
-        protected override NlmJournalIdModel Resolve(ArticleItem source, ResolutionContext context)
-        {
-            var pubRoot = _globalService.GetSiteRootAncestor(source._Id);
-            var id = (pubRoot != null) 
-                ? pubRoot.Journal_ID
-                : string.Empty;
+		protected override NlmJournalIdModel Resolve(ArticleItem source, ResolutionContext context)
+		{
+			var pubRoot = _globalService.GetSiteRootAncestor(source._Id);
+			if (pubRoot == null)
+			{
+				throw new ArgumentNullException("pubRoot", $"Journal ID could not be resolved for {source._Path} because the site root could not be located.");
+			}
 
-            return new NlmJournalIdModel
-            {
-                IdType = "publisher-id",
-                Value = id
-            };
-        }
-    }
+			var id = (pubRoot != null)
+				? pubRoot.Journal_ID
+				: string.Empty;
+
+			if (string.IsNullOrEmpty(id))
+			{
+				throw new ArgumentNullException("journalId", $"Journal ID could not be resolved for {source._Path}");
+			}
+
+			return new NlmJournalIdModel
+			{
+				IdType = "publisher-id",
+				Value = id
+			};
+		}
+	}
 }
--- src/Informa.Library/Utilities/AutoMapper/Resolvers/Nlm/Front/Journal/JournalTitleResolver.cs
+++ src/Informa.Library/Utilities/AutoMapper/Resolvers/Nlm/Front/Journal/JournalTitleResolver.cs
@@ -6,27 +6,37 @@ using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 
 namespace Informa.Library.Utilities.AutoMapper.Resolvers.Nlm.Front.Journal
 {
-    public class JournalTitleResolver : BaseValueResolver<ArticleItem, NlmJournalTitleModel>
-    {
-        private readonly IGlobalSitecoreService _globalService;
+	public class JournalTitleResolver : BaseValueResolver<ArticleItem, NlmJournalTitleModel>
+	{
+		private readonly IGlobalSitecoreService _globalService;
 
-        public JournalTitleResolver(IGlobalSitecoreService globalService)
-        {
-            if (globalService == null) throw new ArgumentNullException(nameof(globalService));
-            _globalService = globalService;
-        }
+		public JournalTitleResolver(IGlobalSitecoreService globalService)
+		{
+			if (globalService == null) throw new ArgumentNullException(nameof(globalService));
+			_globalService = globalService;
+		}
 
-        protected override NlmJournalTitleModel Resolve(ArticleItem source, ResolutionContext context)
-        {
-            var pubRoot = _globalService.GetSiteRootAncestor(source._Id);
-            var title = (pubRoot != null)
-                ? pubRoot.Journal_Title
-                : string.Empty;
-            
-            return new NlmJournalTitleModel
-            {
-                Title = title
-            };
-        }
-    }
+		protected override NlmJournalTitleModel Resolve(ArticleItem source, ResolutionContext context)
+		{
+			var pubRoot = _globalService.GetSiteRootAncestor(source._Id);
+			if (pubRoot == null)
+			{
+				throw new ArgumentNullException("pubRoot", $"Journal Title could not be resolved for {source._Path} because the site root could not be located.");
+			}
+
+			var title = (pubRoot != null)
+				? pubRoot.Journal_Title
+				: string.Empty;
+
+			if (string.IsNullOrEmpty(title))
+			{
+				throw new ArgumentNullException("journalTitle", $"Journal Title could not be resolved for {source._Path}");
+			}
+
+			return new NlmJournalTitleModel
+			{
+				Title = title
+			};
+		}
+	}
 }
