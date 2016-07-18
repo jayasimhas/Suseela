257edec1e7c2b916ad5e85b3fcec0f45c48287a0
Tamer Mehyar <tmehyar@tanasuk.com>
2016-07-18 18:20:00 +0300

    Add CheckBoxList in VWB for Pubs selection

--- web/Website/VWB/default.aspx.cs
+++ web/Website/VWB/default.aspx.cs
@@ -11,284 +11,325 @@ using Sitecore.Shell.Applications.ContentEditor;
 using Sitecore.Web;
 using Sitecore.Web.Authentication;
 using DateTime = System.DateTime;
+using System.Data;
+using System.Linq;
 
 namespace Elsevier.Web.VWB
 {
-	public struct ItemStruct
-	{
-		public string Name { get; set; }
-		public Guid ID { get; set; }
-	}
+    public struct ItemStruct
+    {
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+    }
 
-	/// <summary>
-	/// If a non-button control on this page should cause the report to be built, 
-	/// it should redirect on PostBack. If a button should not build the report, 
-	/// add it to the blacklist.
-	/// </summary>
-	public partial class _default : Page
-	{
-		private VwbQuery _vwbQuery;
-		private ReportBuilder _reportBuilder;
-		private readonly List<Control> ReportBuilderBlacklist = new List<Control>();
-		protected string LogoUrl;
+    /// <summary>
+    /// If a non-button control on this page should cause the report to be built, 
+    /// it should redirect on PostBack. If a button should not build the report, 
+    /// add it to the blacklist.
+    /// </summary>
+    public partial class _default : Page
+    {
+        private VwbQuery _vwbQuery;
+        private ReportBuilder _reportBuilder;
+        private readonly List<Control> ReportBuilderBlacklist = new List<Control>();
+        protected string LogoUrl;
 
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			if (!Sitecore.Context.User.IsAuthenticated)
-			{
-				Response.Redirect(WebUtil.GetFullUrl(Factory.GetSiteInfo("shell").LoginPage) + "?returnUrl=" + Request.RawUrl);
-			}
-			if (IsPostBack)
-			{
-				//let the event handlers for the control causing postback
-				//execute
-				return;
-			}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Sitecore.Context.User.IsAuthenticated)
+            {
+                Response.Redirect(WebUtil.GetFullUrl(Factory.GetSiteInfo("shell").LoginPage) + "?returnUrl=" + Request.RawUrl);
+            }
+            if (IsPostBack)
+            {
+                //let the event handlers for the control causing postback
+                //execute
+                return;
+            }
 
-			if (Request.QueryString.Count == 0 || (Request.QueryString.Count == 1 && Request.QueryString["sc_lang"] != null))
-			{
-				RunQuery(true);
-			}
+            fillPublicationsList();
 
-			LogoUrl = $"{HttpContext.Current.Request.Url.Scheme}://{Factory.GetSiteInfo("website")?.HostName ?? WebUtil.GetHostName()}/-/media/scriplogo.jpg";
+            if (Request.QueryString.Count == 0 || (Request.QueryString.Count == 1 && Request.QueryString["sc_lang"] != null))
+            {
+                RunQuery(true);
+            }
 
-			const string defaultTime = "12:00 AM";
-			txtStartTime.Text = defaultTime;
-			txtEndTime.Text = defaultTime;
+            LogoUrl = $"{HttpContext.Current.Request.Url.Scheme}://{Factory.GetSiteInfo("website")?.HostName ?? WebUtil.GetHostName()}/-/media/scriplogo.jpg";
 
-			UpdateFields();
-			BuildOptionalColumnDropdown();
+            const string defaultTime = "12:00 AM";
+            txtStartTime.Text = defaultTime;
+            txtEndTime.Text = defaultTime;
 
+            UpdateFields();
+            BuildOptionalColumnDropdown();
+        }
 
-		}
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            ReportBuilderBlacklist.Add(btnReset);
 
-		protected void Page_Init(object sender, EventArgs e)
-		{
-			ReportBuilderBlacklist.Add(btnReset);
+            _vwbQuery = new VwbQuery(Request);
+            if (!IsPostBack
+                || (Request.Params.Get("__EVENTTARGET") == ""
+                && !ReportBuilderBlacklist.Contains(GetPostBackControl())))
+            {
+                if (_vwbQuery.ShouldRun)
+                {
+                    BuildReport();
+                }
+            }
+        }
 
-			_vwbQuery = new VwbQuery(Request);
-			if (!IsPostBack
-				|| (Request.Params.Get("__EVENTTARGET") == ""
-				&& !ReportBuilderBlacklist.Contains(GetPostBackControl())))
-			{
-				if (_vwbQuery.ShouldRun)
-				{
-					BuildReport();
-				}
-			}
-		}
+        public Control GetPostBackControl()
+        {
+            Control control = null;
 
-		public Control GetPostBackControl()
-		{
-			Control control = null;
+            string ctrlname = Request.Params.Get("__EVENTTARGET");
+            if (!string.IsNullOrEmpty(ctrlname))
+            {
+                control = FindControl(ctrlname);
+            }
+            else
+            {
+                foreach (string ctl in Request.Form)
+                {
+                    Control c = FindControl(ctl);
+                    if (c is Button)
+                    {
+                        control = c;
+                        break;
+                    }
+                }
+            }
+            return control;
+        }
 
-			string ctrlname = Request.Params.Get("__EVENTTARGET");
-			if (!string.IsNullOrEmpty(ctrlname))
-			{
-				control = FindControl(ctrlname);
-			}
-			else
-			{
-				foreach (string ctl in Request.Form)
-				{
-					Control c = FindControl(ctl);
-					if (c is Button)
-					{
-						control = c;
-						break;
-					}
-				}
-			}
-			return control;
-		}
+        private void fillPublicationsList()
+        {
+            var dbMaster = Sitecore.Data.Database.GetDatabase("master");
 
-		private static int? GetMaxNumResults()
-		{
+            var pubItems = dbMaster.GetItem("/sitecore/content/").Children;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Code");
+            dt.Columns.Add("Name");
+            foreach (Sitecore.Data.Items.Item item in pubItems)
+            {
+                try
+                {
+                    string pubName = item.Fields["Publication Name"].Value;
+                    string pubCode = item.Fields["Publication Code"].Value;
+
+                    dt.Rows.Add(pubCode, pubName);
+                }
+                catch { }
+            }
+
+            chkPublications.DataSource = dt;
+            chkPublications.DataValueField = "Code";
+            chkPublications.DataTextField = "Name";
+            chkPublications.DataBind();
+        }
+
+        private static int? GetMaxNumResults()
+        {
             int count = 250;
             try
             {
                 count = int.Parse(HttpContext.Current.Request.QueryString["max"]);
             }
             catch { }
-			//TextNodeItem maxResults = ItemReference.MaxResultsPerSearch.InnerItem;
-			//if (maxResults != null)
-			//{
-			//	try
-			//	{
-			//		return Int32.Parse(maxResults.Text.Text);
-			//	}
-			//	catch (Exception)
-			//	{
-			//		return 60;
-			//	}
-			//}
-			return count;
-		}
+            //TextNodeItem maxResults = ItemReference.MaxResultsPerSearch.InnerItem;
+            //if (maxResults != null)
+            //{
+            //	try
+            //	{
+            //		return Int32.Parse(maxResults.Text.Text);
+            //	}
+            //	catch (Exception)
+            //	{
+            //		return 60;
+            //	}
+            //}
+            return count;
+        }
 
-		private void UpdateFields()
-		{
-			bool startDate = false;
-			if (_vwbQuery.StartDate != null && _vwbQuery.StartDate != DateTime.MinValue)
-			{
-				startDate = true;
+        private void UpdateFields()
+        {
+            bool startDate = false;
+            if (_vwbQuery.StartDate != null && _vwbQuery.StartDate != DateTime.MinValue)
+            {
+                startDate = true;
 
-				txtStart.Text = string.Format("{0:MM/dd/yyyy}", _vwbQuery.StartDate);
-				txtStartTime.Text = string.Format("{0:h:mm tt}", _vwbQuery.StartDate);
-				EnableDate();
-			}
+                txtStart.Text = string.Format("{0:MM/dd/yyyy}", _vwbQuery.StartDate);
+                txtStartTime.Text = string.Format("{0:h:mm tt}", _vwbQuery.StartDate);
+                EnableDate();
+            }
 
-			bool endDate = false;
-			if (_vwbQuery.EndDate != null && _vwbQuery.EndDate.Value.Year != 9999)
-			{
-				endDate = true;
+            bool endDate = false;
+            if (_vwbQuery.EndDate != null && _vwbQuery.EndDate.Value.Year != 9999)
+            {
+                endDate = true;
 
-				txtEnd.Text = string.Format("{0:MM/dd/yyyy}", _vwbQuery.EndDate);
-				txtEndTime.Text = string.Format("{0:h:mm tt}", _vwbQuery.EndDate);
-				EnableDate();
-			}
+                txtEnd.Text = string.Format("{0:MM/dd/yyyy}", _vwbQuery.EndDate);
+                txtEndTime.Text = string.Format("{0:h:mm tt}", _vwbQuery.EndDate);
+                EnableDate();
+            }
 
-			if (!startDate && !endDate)
-			{
-				rbNoDate.Checked = true;
+            if (!startDate && !endDate)
+            {
+                rbNoDate.Checked = true;
 
-			}
+            }
 
-			if (_vwbQuery.InProgressValue)
-			{
-				chkShowInProgressArticles.Checked = true;
-			}
+            if (_vwbQuery.InProgressValue)
+            {
+                chkShowInProgressArticles.Checked = true;
+            }
 
-		}
+            if (string.IsNullOrEmpty(_vwbQuery.PublicationCodes) == false)
+            {
+                List<string> codes = _vwbQuery.PublicationCodes.Split(',').ToList();
+                foreach (var item in codes)
+                {
+                    chkPublications.Items.FindByValue(item).Selected = true;
+                }
+            }
+        }
 
-		protected void EnableDate()
-		{
-			rbDateRange.Checked = true;
-			txtStart.Enabled = true;
-			txtEnd.Enabled = true;
-			txtEndTime.Enabled = true;
-			txtStartTime.Enabled = true;
-		}
+        protected void EnableDate()
+        {
+            rbDateRange.Checked = true;
+            txtStart.Enabled = true;
+            txtEnd.Enabled = true;
+            txtEndTime.Enabled = true;
+            txtStartTime.Enabled = true;
+        }
 
-		public void BuildReport()
-		{
-			_reportBuilder = new ReportBuilder(this, _vwbQuery);
-			tblResults.EnableViewState = false;
-			_reportBuilder.BuildTable(tblResults);
-		}
+        public void BuildReport()
+        {
+            _reportBuilder = new ReportBuilder(this, _vwbQuery);
+            tblResults.EnableViewState = false;
+            _reportBuilder.BuildTable(tblResults);
+        }
 
-		protected void RunReport(object sender, EventArgs e)
-		{
-			RunQuery(true);
-		}
+        protected void RunReport(object sender, EventArgs e)
+        {
+            RunQuery(true);
+        }
 
-		private void RunQuery(bool execute)
-		{
-			var q = _vwbQuery.Clone();
-			if (rbDateRange.Checked)
-			{
-				DateTime startDate;
-				DateTime.TryParse(txtStart.Text, out startDate);
-				DateTime endDate;
-				DateTime.TryParse(txtEnd.Text, out endDate);
+        private void RunQuery(bool execute)
+        {
+            var q = _vwbQuery.Clone();
+            if (rbDateRange.Checked)
+            {
+                DateTime startDate;
+                DateTime.TryParse(txtStart.Text, out startDate);
+                DateTime endDate;
+                DateTime.TryParse(txtEnd.Text, out endDate);
 
-				if (!endDate.Equals(DateTime.MinValue) && !startDate.Equals(DateTime.MinValue))
-				{
-					q.StartDate = startDate;
-					//endDate = endDate.AddDays(1); //setting up the range so that it include the end date
-					q.EndDate = endDate;
-				}
+                if (!endDate.Equals(DateTime.MinValue) && !startDate.Equals(DateTime.MinValue))
+                {
+                    q.StartDate = startDate;
+                    //endDate = endDate.AddDays(1); //setting up the range so that it include the end date
+                    q.EndDate = endDate;
+                }
 
-				if (q.StartDate.HasValue && !string.IsNullOrEmpty(txtStartTime.Text))
-				{
-					DateTime startTime;
-					var combinedStartTime = string.Format("{0} {1}", txtStart.Text, txtStartTime.Text);
-					if (DateTime.TryParse(combinedStartTime, out startTime))
-					{
-						q.StartDate = startTime;
-					}
-				}
+                if (q.StartDate.HasValue && !string.IsNullOrEmpty(txtStartTime.Text))
+                {
+                    DateTime startTime;
+                    var combinedStartTime = string.Format("{0} {1}", txtStart.Text, txtStartTime.Text);
+                    if (DateTime.TryParse(combinedStartTime, out startTime))
+                    {
+                        q.StartDate = startTime;
+                    }
+                }
 
-				if (q.EndDate.HasValue && !string.IsNullOrEmpty(txtEndTime.Text))
-				{
-					DateTime endTime;
-					var combinedEndTime = string.Format("{0} {1}", txtEnd.Text, txtEndTime.Text);
-					if (DateTime.TryParse(combinedEndTime, out endTime))
-					{
-						q.EndDate = endTime;
-					}
-				}
-			}
-			else
-			{
-				q.StartDate = null;
-				q.EndDate = null;
-			}
+                if (q.EndDate.HasValue && !string.IsNullOrEmpty(txtEndTime.Text))
+                {
+                    DateTime endTime;
+                    var combinedEndTime = string.Format("{0} {1}", txtEnd.Text, txtEndTime.Text);
+                    if (DateTime.TryParse(combinedEndTime, out endTime))
+                    {
+                        q.EndDate = endTime;
+                    }
+                }
+            }
+            else
+            {
+                q.StartDate = null;
+                q.EndDate = null;
+            }
 
-			if (chkShowInProgressArticles.Checked)
-			{
-				q.InProgressValue = true;
-			}
-			else
-			{ q.InProgressValue = false; }
+            if (chkShowInProgressArticles.Checked)
+            {
+                q.InProgressValue = true;
+            }
+            else
+            { q.InProgressValue = false; }
 
-			q.ShouldRun = execute;
-			q.NumResultsValue = GetMaxNumResults();
-			RedirectTo(q);
-		}
+            q.ShouldRun = execute;
 
-		/// <summary>
-		/// Populates ddColumns with all columns not already spoken for in generated report
-		/// </summary>
-		protected void BuildOptionalColumnDropdown()
-		{
-			ddColumns.Items.Add("Add New Field...");
-			foreach (var column in ColumnFactory.GetColumnFactory().GetColumnsNot(_vwbQuery.ColumnKeysInOrder))
-			{
-				ddColumns.Items.Add(new ListItem(column.GetHeader(), column.Key()));
-			}
-		}
+            List<ListItem> selected = chkPublications.Items.Cast<ListItem>().Where(li => li.Selected).ToList();
+            q.PublicationCodes = string.Join(",", selected.Select(s => s.Value));
 
-		protected string GetCurrentPageUrl()
-		{
-			bool httpsOn = Request.ServerVariables["HTTPS"].Equals("ON");
-			string http = httpsOn ? "https://" : "http://";
-			return http + Request.ServerVariables["SERVER_NAME"] + Request.ServerVariables["URL"];
-		}
+            q.NumResultsValue = GetMaxNumResults();
+            RedirectTo(q);
+        }
 
-		public void RedirectTo(VwbQuery query)
-		{
-			Response.Redirect(GetCurrentPageUrl() + "?" +
-								query.GetQueryString());
-		}
+        /// <summary>
+        /// Populates ddColumns with all columns not already spoken for in generated report
+        /// </summary>
+        protected void BuildOptionalColumnDropdown()
+        {
+            ddColumns.Items.Add("Add New Field...");
+            foreach (var column in ColumnFactory.GetColumnFactory().GetColumnsNot(_vwbQuery.ColumnKeysInOrder))
+            {
+                ddColumns.Items.Add(new ListItem(column.GetHeader(), column.Key()));
+            }
+        }
 
-		protected void PublicationSelected(object sender, EventArgs e)
-		{
-			RunQuery(false);
-		}
+        protected string GetCurrentPageUrl()
+        {
+            bool httpsOn = Request.ServerVariables["HTTPS"].Equals("ON");
+            string http = httpsOn ? "https://" : "http://";
+            return http + Request.ServerVariables["SERVER_NAME"] + Request.ServerVariables["URL"];
+        }
 
-		protected void AddColumn(object sender, EventArgs e)
-		{
-			if (ColumnFactory.GetColumnFactory().GetColumn(ddColumns.SelectedValue) == null) return;
-			_vwbQuery.ColumnKeysInOrder.Add(ddColumns.SelectedValue);
-			RedirectTo(_vwbQuery);
-		}
+        public void RedirectTo(VwbQuery query)
+        {
+            Response.Redirect(GetCurrentPageUrl() + "?" +
+                                query.GetQueryString());
+        }
 
-		protected void ResetReport(object sender, EventArgs e)
-		{
-			Response.Redirect(GetCurrentPageUrl());
-		}
+        protected void PublicationSelected(object sender, EventArgs e)
+        {
+            RunQuery(false);
+        }
 
-		protected void Logout(object sender, EventArgs e)
-		{
-			CacheManager.ClearSecurityCache(Sitecore.Context.User.Name);
-			RecentDocuments.Remove(Sitecore.Context.User.Name);
-			Sitecore.Shell.Framework.Security.Abandon();
-			string currentTicketId = TicketManager.GetCurrentTicketId();
-			if (string.IsNullOrEmpty(currentTicketId))
-				return; TicketManager.RemoveTicket(currentTicketId);
+        protected void AddColumn(object sender, EventArgs e)
+        {
+            if (ColumnFactory.GetColumnFactory().GetColumn(ddColumns.SelectedValue) == null) return;
+            _vwbQuery.ColumnKeysInOrder.Add(ddColumns.SelectedValue);
+            RedirectTo(_vwbQuery);
+        }
+
+        protected void ResetReport(object sender, EventArgs e)
+        {
+            Response.Redirect(GetCurrentPageUrl());
+        }
+
+        protected void Logout(object sender, EventArgs e)
+        {
+            CacheManager.ClearSecurityCache(Sitecore.Context.User.Name);
+            RecentDocuments.Remove(Sitecore.Context.User.Name);
+            Sitecore.Shell.Framework.Security.Abandon();
+            string currentTicketId = TicketManager.GetCurrentTicketId();
+            if (string.IsNullOrEmpty(currentTicketId))
+                return; TicketManager.RemoveTicket(currentTicketId);
 
 
-			Response.Redirect(WebUtil.GetFullUrl(Factory.GetSiteInfo("shell").LoginPage) + "?redirect=" + Request.RawUrl);
-		}
-	}
+            Response.Redirect(WebUtil.GetFullUrl(Factory.GetSiteInfo("shell").LoginPage) + "?redirect=" + Request.RawUrl);
+        }
+    }
 }
