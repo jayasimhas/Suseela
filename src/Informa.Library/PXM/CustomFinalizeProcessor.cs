dfae2e745274cdfdb9c1bc9ef9cfb2997beb2dfe
Huxu Tian <huxu.tian@velir.com>
2016-07-21 15:19:05 -0400

    [IITS2A-111]: Change sidebar article paragraph style.

---
+++ src/Informa.Library/PXM/CustomFinalizeProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Informa.Library.Utilities;
+using Sitecore.PrintStudio.PublishingEngine.Pipelines.ConvertHtmlToXml;
+
+namespace Informa.Library.PXM
+{
+	public class CustomFinalizeProcessor
+	{
+		public void Process(HtmlConvertPipelineArgs args)
+		{
+			using (var scope = Jabberwocky.Glass.Autofac.Util.AutofacConfig.ServiceLocator.BeginLifetimeScope())
+			{
+				var helper = scope.Resolve<IPxmXmlHelper>();
+				args.OutputText = helper.AddSidebarStyles(args.OutputXml.ToString());
+			}
+		}
+	}
+}
---
+++ src/Informa.Library/Utilities/PxmXmlHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Xml;
+using HtmlAgilityPack;
+using Jabberwocky.Autofac.Attributes;
+
+namespace Informa.Library.Utilities
+{
+	public interface IPxmXmlHelper
+	{
+		string AddSidebarStyles(string content);
+	}
+
+	[AutowireService]
+	public class PxmXmlHelper : IPxmXmlHelper
+	{
+		private readonly IDependencies _dependencies;
+		private const string SidebarStyling = "Sidebar styling";
+
+		[AutowireService(IsAggregateService = true)]
+		public interface IDependencies
+		{
+
+		}
+
+		public PxmXmlHelper(IDependencies dependencies)
+		{
+			_dependencies = dependencies;
+		}
+
+		public string AddSidebarStyles(string content)
+		{
+			var doc = new XmlDocument();
+			doc.LoadXml(content);
+			var inlines = doc.SelectNodes("//Inline");
+			if (inlines != null)
+			{
+				foreach (XmlNode inline in inlines)
+				{
+					var children = inline.SelectNodes("//Inline/ParagraphStyle");
+					if (children != null)
+					{
+						foreach (XmlNode child in children)
+						{
+							if (child.Attributes?["Style"] != null)
+							{
+								child.Attributes["Style"].Value = SidebarStyling;
+							}
+							else
+							{
+								var attr = doc.CreateAttribute("Style");
+								attr.Value = SidebarStyling;
+								child.Attributes?.Append(attr);
+							}
+						}
+					}
+				}
+			}
+			return doc.OuterXml.Replace("<TextFrame>", "").Replace("</TextFrame>", "");
+		}
+	}
+}
