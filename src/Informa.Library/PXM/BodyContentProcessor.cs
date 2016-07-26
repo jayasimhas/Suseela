ba43fa2357d9373b45e141c0bdd21991bf40e14f
Huxu Tian <huxu.tian@velir.com>
2016-07-26 11:39:03 -0400

    IITS2A-102 *adding iframe conversion support to the pxm html import

--- src/Informa.Library/PXM/BodyContentProcessor.cs
+++ src/Informa.Library/PXM/BodyContentProcessor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
+using Informa.Library.Utilities;
 using Informa.Library.Utilities.TokenMatcher;
 using Sitecore.PrintStudio.PublishingEngine.Pipelines.ConvertHtmlToXml;
 
@@ -16,7 +17,9 @@ namespace Informa.Library.PXM
 			using (var scope = Jabberwocky.Glass.Autofac.Util.AutofacConfig.ServiceLocator.BeginLifetimeScope())
 			{
 				var tokenToHtml = scope.Resolve<ITokenToHtml>();
+				var helper = scope.Resolve<IPxmXmlHelper>();
 				args.InputText = tokenToHtml.ReplaceAllTokens(args.InputText);
+				args.InputText = helper.ProcessIframeTag(args.InputText);
 			}
 		}
 	}
