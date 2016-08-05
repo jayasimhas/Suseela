77b0b6fb0616109fb2875ec47b6307d56b2ebbda
felix.steiny <felix.steiny@pennwell.com>
2016-08-05 19:13:19 -0400

    #IITS2A-310 Oops, forgot Sidebars

--- web/Website/ViewModels/VWB/AddIssueViewModel.cs
+++ web/Website/ViewModels/VWB/AddIssueViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Virtual_Whiteboard;
 using Informa.Library.Services.Article;
+using Informa.Library.Utilities.Extensions;
 
 namespace Informa.Web.ViewModels.VWB
 {
@@ -10,5 +12,11 @@ namespace Informa.Web.ViewModels.VWB
 		public IIssue Issue { get; set; }
 		public IEnumerable<IArticle> Articles { get; set; }
         public IArticleService ArticleService { get; set; }
-    }
+
+	    public string GetSidebars(IArticle article)
+	        => string.Join("<br>", 
+                article.Referenced_Articles
+                    .Select(a => ((IArticle) a)?.Article_Number)
+                    .Where(a => a.HasContent()));
+	}
 }
