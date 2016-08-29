bc59e36e3fb7285fad19166979eb1a7a06311566
Tamer Mehyar <tmehyar@tanasuk.com>
2016-08-29 13:45:04 +0300

    IIS-373: added sitecore config item to disable/enable functionality

--- src/Informa.Library/Utilities/References/IItemReferences.cs
+++ src/Informa.Library/Utilities/References/IItemReferences.cs
@@ -2,47 +2,49 @@ using System;
 
 namespace Informa.Library.Utilities.References
 {
-	public interface IItemReferences
-	{
-		Guid HomePage { get; }
-        
+    public interface IItemReferences
+    {
+        Guid HomePage { get; }
 
-		Guid NlmConfiguration { get; }
 
-		Guid NlmErrorDistributionList { get; }
+        Guid NlmConfiguration { get; }
+
+        Guid NlmErrorDistributionList { get; }
 
         Guid UserLockoutedEmails { get; }
 
         // Pharma Globals
         Guid NlmCopyrightStatement { get; }
-		Guid InformaBar { get; }
+        Guid InformaBar { get; }
 
-		Guid GeneratedDictionary { get; }
+        Guid GeneratedDictionary { get; }
 
         Guid DownloadTypes { get; }
 
         // Taxonomy Folders
         Guid SubjectsTaxonomyFolder { get; }
-		Guid RegionsTaxonomyFolder { get; }
-		Guid TherapyAreasTaxonomyFolder { get; }
+        Guid RegionsTaxonomyFolder { get; }
+        Guid TherapyAreasTaxonomyFolder { get; }
 
-		//Account Drop Down
-		Guid AccountCountries { get; }
-		Guid AccountJobFunctions { get; }
-		Guid AccountJobIndustries { get; }
-		Guid AccountNameSuffixes { get; }
-		Guid AccountPhoneTypes { get; }
-		Guid AccountSalutations { get; }
-        
-		// Templates
-		Guid FolderTemplate { get; }
+        //Account Drop Down
+        Guid AccountCountries { get; }
+        Guid AccountJobFunctions { get; }
+        Guid AccountJobIndustries { get; }
+        Guid AccountNameSuffixes { get; }
+        Guid AccountPhoneTypes { get; }
+        Guid AccountSalutations { get; }
 
-		Guid TaxonomyRoot { get; }
+        // Templates
+        Guid FolderTemplate { get; }
 
-		Guid SubscriptionPage { get; }
+        Guid TaxonomyRoot { get; }
 
-		Guid EmailPreferences { get; }
+        Guid SubscriptionPage { get; }
+
+        Guid EmailPreferences { get; }
 
         Guid PasswordRecoveryEmail { get; }
+
+        Guid CustomPublishingConfig { get; }
     }
 }
