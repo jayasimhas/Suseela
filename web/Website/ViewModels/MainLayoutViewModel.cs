571a74552941c63a0747740890a972b982215551
Tamer Mehyar <tmehyar@tanasuk.com>
2016-08-30 16:21:17 +0300

    IIS-422: fixed meta-tags not being output

--- web/Website/ViewModels/MainLayoutViewModel.cs
+++ web/Website/ViewModels/MainLayoutViewModel.cs
@@ -13,20 +13,21 @@ using Informa.Library.Globalization;
 using Informa.Library.Company;
 using Informa.Library.Services.Global;
 using Informa.Library.SiteDebugging;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
 
 namespace Informa.Web.ViewModels
 {
-	public class MainLayoutViewModel : GlassViewModel<I___BasePage>
-	{
-		
-		protected readonly ITextTranslator TextTranslator;
-		protected readonly IAuthenticatedUserContext AuthenticatedUserContext;
+    public class MainLayoutViewModel : GlassViewModel<I___BasePage>
+    {
+
+        protected readonly ITextTranslator TextTranslator;
+        protected readonly IAuthenticatedUserContext AuthenticatedUserContext;
         protected readonly ISiteDebuggingAllowedContext SiteDebuggingAllowedContext;
-	    protected readonly IGlobalSitecoreService GlobalService;
-	    protected readonly ISiteRootContext SiteRootContext;
-        
+        protected readonly IGlobalSitecoreService GlobalService;
+        protected readonly ISiteRootContext SiteRootContext;
+
         public readonly IUserCompanyContext UserCompanyContext;
-        
+
         public MainLayoutViewModel(
             ITextTranslator textTranslator,
             IAuthenticatedUserContext authenticatedUserContext,
@@ -34,23 +35,25 @@ namespace Informa.Web.ViewModels
             ISiteDebuggingAllowedContext siteDebuggingAllowedContext,
             IGlobalSitecoreService globalService,
             ISiteRootContext siteRootContext)
-		{
-			TextTranslator = textTranslator;
+        {
+            TextTranslator = textTranslator;
             AuthenticatedUserContext = authenticatedUserContext;
             UserCompanyContext = userCompanyContext;
             SiteDebuggingAllowedContext = siteDebuggingAllowedContext;
             GlobalService = globalService;
             SiteRootContext = siteRootContext;
+        }
 
-		}
-
-	    public bool IsSiteDebuggingAllowed => SiteDebuggingAllowedContext.IsAllowed;
+        public bool IsSiteDebuggingAllowed => SiteDebuggingAllowedContext.IsAllowed;
         public string PrintedByText => TextTranslator.Translate("Header.PrintedBy");
-		public string UserName => AuthenticatedUserContext.User?.Name ?? string.Empty;
-		public string CorporateName => UserCompanyContext?.Company?.Name;
-	    public string Title => GlobalService.GetPageTitle(GlassModel);
-	    public string BodyCssClass => SiteRootContext.GetBodyCssClass();
+        public string UserName => AuthenticatedUserContext.User?.Name ?? string.Empty;
+        public string CorporateName => UserCompanyContext?.Company?.Name;
+        public string Title => GlobalService.GetPageTitle(GlassModel);
+        public string BodyCssClass => SiteRootContext.GetBodyCssClass();
         public HtmlString PrintPageHeaderMessage => SiteRootContext.GetPrintHeaderMessage();
         public string CanonicalUrl => GlassModel?.Canonical_Link?.GetLink();
-	}
+        public string Meta_Description => GlassModel.Meta_Description;
+        public string Meta_Keywords => GlassModel.Meta_Keywords;
+        public HtmlString Meta_CustomTags => new HtmlString(GlassModel.Custom_Meta_Tags);
+    }
 }
