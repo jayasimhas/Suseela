942686e1c40bc53a07232e58f11aafa571875a03
Chris Sulham <chris.sulham@velir.com>
2016-09-06 16:07:23 -0400

    IITS2A-366: Added a prefix dictionary term for author alerts

--- src/Informa.Library/Authors/AuthorsAlertOptions.cs
+++ src/Informa.Library/Authors/AuthorsAlertOptions.cs
@@ -9,29 +9,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Informa.Library.Globalization;
 using Informa.Library.Search.ComputedFields.Facets;
 using Informa.Library.Utilities.References;
 
-namespace Informa.Library.Authors {
-    [AutowireService]
-    public class AuthorAlertOptions : IAuthorAlertOptions {
-        
-        private readonly IAuthorService AuthorService;
+namespace Informa.Library.Authors
+{
+	[AutowireService]
+	public class AuthorAlertOptions : IAuthorAlertOptions
+	{
 
-        public AuthorAlertOptions(IAuthorService authorService) {
-            AuthorService = authorService;
-        }
+		private readonly IAuthorService AuthorService;
+		private readonly ITextTranslator TextTranslator;
 
-        public ITopic_Alert_Options SetOptions(ITopic_Alert_Options options) {
+		public AuthorAlertOptions(IAuthorService authorService, ITextTranslator textTranslator)
+		{
+			AuthorService = authorService;
+			TextTranslator = textTranslator;
+		}
 
-            var a = AuthorService?.GetCurrentAuthor() ?? null;
-            if (a == null)
-                return options;
+		public ITopic_Alert_Options SetOptions(ITopic_Alert_Options options)
+		{
 
-            options.Search_Name = $"{a?.First_Name} {a?.Last_Name}" ?? string.Empty;
-            options.Related_Search = $"?{Constants.QueryString.AuthorFullName}={AuthorNamesField.ToAuthorName(a)}";
+			var a = AuthorService?.GetCurrentAuthor() ?? null;
+			if (a == null)
+				return options;
 
-            return options;
-        }
-    }
+			options.Search_Name = $"{TextTranslator.Translate("Author.TopicAlert.Prefix")} {a?.First_Name} {a?.Last_Name}" ?? string.Empty;
+			options.Related_Search = $"?{Constants.QueryString.AuthorFullName}={AuthorNamesField.ToAuthorName(a)}";
+
+			return options;
+		}
+	}
 }
