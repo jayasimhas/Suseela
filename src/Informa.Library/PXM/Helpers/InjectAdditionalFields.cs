d0ba012a8f64aaff25582fd6305621a58200d720
Mark Stiles <mark.stiles@velir.com>
2016-09-07 11:11:13 -0400

    IITS2A-368 *removed the 'pre' tag so that the title and subtitle are added as part of the body content during import

--- src/Informa.Library/PXM/Helpers/InjectAdditionalFields.cs
+++ src/Informa.Library/PXM/Helpers/InjectAdditionalFields.cs
@@ -60,11 +60,11 @@ namespace Informa.Library.PXM.Helpers
             var html = string.Empty;
             if (article.Title.HasContent())
             {
-                html += $"<pre><h1 class=\"title\">{article.Title}</h1></pre>";
+                html += $"<h1 class=\"title\">{article.Title}</h1>";
             }
             if (article.Sub_Title.HasContent())
             {
-                html += $"<pre><h2 class=\"subtitle\">{article.Sub_Title}</h2></pre>";
+                html += $"<h2 class=\"subtitle\">{article.Sub_Title}</h2>";
             }
 
             return html;
