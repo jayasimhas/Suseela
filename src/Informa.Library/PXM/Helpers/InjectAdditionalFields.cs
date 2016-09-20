b4b3520e5fc4b49734820876bc65fa86f913d964
Huxu Tian <huxu.tian@velir.com>
2016-09-20 15:23:16 -0400

    [IITS2A-371]: Show authors and title in the same frame.

--- src/Informa.Library/PXM/Helpers/InjectAdditionalFields.cs
+++ src/Informa.Library/PXM/Helpers/InjectAdditionalFields.cs
@@ -44,7 +44,7 @@ namespace Informa.Library.PXM.Helpers
             var authors = article?.Authors?.ToArray();
             if (authors == null || authors.Length == 0) { return html; }
 
-            var authorsTag = $"<pre><h3 class='authors'>{_dependencies.BylineMaker.MakePrintByLine(authors)}</h3></pre>";
+            var authorsTag = $"<h3 class='authors'>{_dependencies.BylineMaker.MakePrintByLine(authors)}</h3>";
 
             return InjectAfterRegex(RootRegex, html, authorsTag);
         }
