7baa034e85ac013acc3a3c2b84146cdcd5430d92
Tamer Mehyar <tmehyar@tanasuk.com>
2016-09-21 10:19:36 +0300

    Revert "IIS-118: fix for multi-publications."
    This reverts commit 0c55b2c844d52a4d11ae5792e312dc642840d3d8.

--- web/Website/ViewModels/IndividualRenewalMessageViewModel.cs
+++ web/Website/ViewModels/IndividualRenewalMessageViewModel.cs
@@ -21,47 +21,48 @@ namespace Informa.Web.ViewModels
         protected readonly IUserSubscriptionsContext UserSubscriptionsContext;
         protected readonly IUserProfileContext ProfileContext;
 
-		public IndividualRenewalMessageViewModel(
-				 ITextTranslator textTranslator,
-				 IIndividualSubscriptionRenewalMessageContext context,
-				 IAuthenticatedUserContext userContext,
-				 ISiteRootContext siteRootContext,
-				 IUserSubscriptionsContext userSubscriptionsContext,
-				 IUserProfileContext profileContext)
-		{
-			Context = context;
-			SiteRootContext = siteRootContext;
-			UserSubscriptionsContext = userSubscriptionsContext;
-			ProfileContext = profileContext;
 
-			ISubscription record = userContext.IsAuthenticated ? GetLatestExpiringRecord() : null;
+        public IndividualRenewalMessageViewModel(
+                ITextTranslator textTranslator,
+                IIndividualSubscriptionRenewalMessageContext context,
+                IAuthenticatedUserContext userContext,
+                ISiteRootContext siteRootContext,
+                IUserSubscriptionsContext userSubscriptionsContext,
+                IUserProfileContext profileContext)
+        {
+            Context = context;
+            SiteRootContext = siteRootContext;
+            UserSubscriptionsContext = userSubscriptionsContext;
+            ProfileContext = profileContext;
 
-			DismissText = textTranslator.Translate("Subscriptions.Renewals.Dismiss");
-			Display = DisplayMessage(record);
+            ISubscription record = userContext.IsAuthenticated ? GetLatestRecord() : null;
 
-			Message = Display ? GetMessage(record, profileContext?.Profile?.FirstName ?? string.Empty) : string.Empty;
-			Id = context.ID;
-			RenewURL = context.RenewalLinkURL;
-			RenewURLText = context.RenewalLinkText;
-		}
+            DismissText = textTranslator.Translate("Subscriptions.Renewals.Dismiss");
+            Display = DisplayMessage(record);
+            Message = Display ? GetMessage(record, profileContext?.Profile?.FirstName ?? string.Empty) : string.Empty;
+            Id = context.ID;
+            RenewURL = context.RenewalLinkURL;
+            RenewURLText = context.RenewalLinkText;
+        }
 
-		private ISubscription GetLatestExpiringRecord()
-		{
-			return UserSubscriptionsContext.Subscriptions?.Where(subscription => subscription.ProductCode.ToLower() == SiteRootContext.Item.Publication_Code.ToLower()
-						&& (subscription.ExpirationDate - DateTime.Now).TotalDays <= SiteRootContext.Item.Days_To_Expiration
-						&& SUBSCRIPTIONTYPE.Contains(subscription.SubscriptionType.ToLower())
-						&& subscription.ProductType.ToLower() == PRODUCT_TYPE).OrderByDescending(o => o.ExpirationDate).FirstOrDefault() ?? null;
-		}
+        private ISubscription GetLatestRecord()
+        {
+            return UserSubscriptionsContext.Subscriptions?.OrderByDescending(o => o.ExpirationDate).FirstOrDefault() ?? null;
+        }
 
-		private bool DisplayMessage(ISubscription subscription)
-		{
-			if (subscription == null)
-				return false;
+        private bool DisplayMessage(ISubscription subscription)
+        {
+            if (subscription == null
+                        || subscription.ProductCode.ToLower() != SiteRootContext.Item.Publication_Code.ToLower()
+                        || (subscription.ExpirationDate - DateTime.Now).TotalDays > SiteRootContext.Item.Days_To_Expiration
+                        || SUBSCRIPTIONTYPE.Contains(subscription.SubscriptionType.ToLower()) == false
+                        || subscription.ProductType.ToLower() != PRODUCT_TYPE)
+                return false;
 
-			return true;
-		}
+            return true;
+        }
 
-		private string GetMessage(ISubscription subscription, string userName)
+        private string GetMessage(ISubscription subscription, string userName)
         {
             if (subscription == null)
             {
