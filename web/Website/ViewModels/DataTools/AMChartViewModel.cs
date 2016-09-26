4b34ba6aac115f40ba76e0ac865b560a6d30de28
SAPIENT\mbirad <mbiradara@sapient.com>
2016-09-26 10:12:17 +0530

    AMcharts related changes

---
+++ web/Website/ViewModels/DataTools/AMChartViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Informa.Library.Services.Global;
+using Informa.Library.Site;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Components;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.ViewModels.DataTools
+{
+    public class AMChartViewModel: GlassViewModel<IAMcharts_Dashboard>
+    {
+        protected readonly ISiteRootContext SiteRootContext;
+        protected readonly IGlobalSitecoreService GlobalService;
+        public AMChartViewModel(ISiteRootContext siteRootContext, IGlobalSitecoreService globalService)
+        {
+            SiteRootContext = siteRootContext;
+            GlobalService = globalService;
+        }
+
+        #region AMcharts parameters
+        public string ChartType => GlassModel?.ChartType;
+        public string CategoryField => GlassModel?.CategoryField;
+        public string ValueField => GlassModel?.ValueField;
+        public string Height => GlassModel?.Height;
+        public string Width => GlassModel?.Width;
+        public string JsonPath => GlassModel?.JsonPath;
+
+        #endregion
+
+    }
+}
---
+++ web/Website/amcharts/data/Amchartrender.js
@@ -0,0 +1,17 @@
+﻿function RenderAMChartData() {
+
+    var amChartType = $('#amChartType').val();
+    var dataProvider = $('#amChartDataProvider').val();
+    var categoryField = $('#CategoryField').val();
+    var valueField = $('#ValueField').val();
+
+    AmCharts.makeChart("chartdiv", {
+        "type": amChartType,
+        "dataProvider": dataProvider,
+        "categoryField": categoryField,
+        "graphs": [{
+            "valueField": valueField,
+            "type": "column"
+        }]
+    });
+};
