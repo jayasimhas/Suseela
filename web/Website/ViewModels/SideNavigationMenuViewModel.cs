f141883b15096329faee6f9ffd8cf1f412e7d106
SAPIENT\mbirad <mbiradara@sapient.com>
2016-10-07 12:17:34 +0530

    Personalized nav changes

--- web/Website/ViewModels/SideNavigationMenuViewModel.cs
+++ web/Website/ViewModels/SideNavigationMenuViewModel.cs
@@ -37,64 +37,107 @@ namespace Informa.Web.ViewModels
             _subcriptions = userSubscriptionsContext.Subscriptions;
         }
 
+        #region Side Navigation Menu Items  
         public IEnumerable<INavigation> Navigation => SiteMainNavigationContext.Navigation;
         public IEnumerable<ISubscription> NavigationWithSubs => GetNavigationWithSubScriptions();
         public string MenuText => TextTranslator.Translate("MainNavigation.Menu");
         public string MenuButtonText => TextTranslator.Translate("MainNavigation.ToggleMenu");
         public string MyViewHelpText => TextTranslator.Translate("MainNavigation.MyViewHelpText");
-        public string MyViewEditBtnText => TextTranslator.Translate("MainNavigation.MyViewEditButtonText");        
+        public string MyViewEditBtnText => TextTranslator.Translate("MainNavigation.MyViewEditButtonText");
         public bool IsAuthenticated => AuthenticatedUserContext.IsAuthenticated;
         public IEnumerable<ISubscription> Subscriptions => _subcriptions;
         public Navigation Preferencelst { get; set; }
         public Navigation MyViewPreferences => GetUserPreferences();
+        #endregion
+
+        /// <summary>
+        /// Method to get the logged in user preferences
+        /// </summary>
+        /// <returns>list of user preferences</returns>
         public Navigation GetUserPreferences()
         {
-            var tempObj = new Navigation();
+            #region reading actual preferences
+            var navigation = new Navigation();
+            navigation.Children = new List<INavigation>();
+            var preferredChannels = new List<Navigation>();
             IUserPreferences prefChannels = new UserPreferences();
+            if (IsAuthenticated)
+            {
+                if (UserPreferences.Preferences != null &&
+                UserPreferences.Preferences.PreferredChannels != null && UserPreferences.Preferences.PreferredChannels.Count > 0)
+                {
+                    foreach (var preference in UserPreferences.Preferences.PreferredChannels)
+                    {
+                        preferredChannels.Add(new Navigation { Text = preference.Channel.ChannelName, Link = new Link { Url = preference.Channel.ChannelLink } });
+                    }
+                    navigation.Children = preferredChannels;
+                }
+                return navigation;
+            }
+            else
+            {
+                return null;
+            }
+            #endregion
+
+            #region hardcoded data
+            //var tempObj = new Navigation();
+            //var children = new List<Navigation>();
+            //tempObj.Children = new List<INavigation>();
+
+            //tempObj.Text = "My View";
+            //tempObj.Link = new Link { Url = @"http://facebook.com" };
+
+            //var Prefdchannel = new ChannelPreference();
+            //Prefdchannel.Channel = new Channel();
+            //Prefdchannel.Channel.ChannelName = "Beverages";
+            //Prefdchannel.Channel.ChannelOrder = 1;
+            //Prefdchannel.Channel.ChannelLink = @"http://google.com";
+
+            //children.Add(new Navigation { Text = Prefdchannel.Channel.ChannelName, Link = new Link { Url = Prefdchannel.Channel.ChannelLink } });
+
+            //Prefdchannel.Channel = new Channel();
+            //Prefdchannel.Channel.ChannelName = "Cocoa";
+            //Prefdchannel.Channel.ChannelOrder = 2;
+            //Prefdchannel.Channel.ChannelLink = @"http://yahoo.com";
+
+            //children.Add(new Navigation { Text = Prefdchannel.Channel.ChannelName, Link = new Link { Url = Prefdchannel.Channel.ChannelLink } });
+
+            //tempObj.Children = children;
+
+            //return tempObj;
+            #endregion
+        }
+
+        /// <summary>
+        /// Method to get the logged in user subscriptions
+        /// </summary>
+        /// <returns>list of subscriptions</returns>
+        public IEnumerable<ISubscription> GetNavigationWithSubScriptions()
+        {
+            #region reading actual subscriptions
+            //var subscriptions = new List<ISubscription>();
             //if (IsAuthenticated)
             //{
-            //if (UserPreferences.Preferences != null &&
-            //    UserPreferences.Preferences.PreferredChannels != null && UserPreferences.Preferences.PreferredChannels.Count > 0)
-            //{
-            //return preferred topics
-            //foreach (var item in UserPreferences.Preferences.PreferredChannels)
-            //{
-            var children = new List<Navigation>();
-            tempObj.Children = new List<INavigation>();
-
-            tempObj.Text = "My View";
-            tempObj.Link = new Link { Url = @"http://facebook.com" };
-
-            var Prefdchannel = new ChannelPreference();
-            Prefdchannel.Channel = new Channel();
-            Prefdchannel.Channel.ChannelName = "Beverages";
-            Prefdchannel.Channel.ChannelOrder = 1;
-            Prefdchannel.Channel.ChannelLink = @"http://google.com";
-
-            children.Add(new Navigation { Text = Prefdchannel.Channel.ChannelName, Link = new Link { Url = Prefdchannel.Channel.ChannelLink } });
-
-            Prefdchannel.Channel = new Channel();
-            Prefdchannel.Channel.ChannelName = "Cocoa";
-            Prefdchannel.Channel.ChannelOrder = 2;
-            Prefdchannel.Channel.ChannelLink = @"http://yahoo.com";
-
-            children.Add(new Navigation { Text = Prefdchannel.Channel.ChannelName, Link = new Link { Url = Prefdchannel.Channel.ChannelLink } });
-
-            tempObj.Children = children;
-
-            return tempObj;
-
+            //    if (Subscriptions != null && Subscriptions.Count() > 0)
+            //    {
+            //        foreach (var subscription in Subscriptions)
+            //        {
+            //            if (subscription != null && subscription.ExpirationDate > DateTime.Now)
+            //            {
+            //                subscriptions.Add(subscription);
+            //            }
+            //        }
+            //    }
+            //    return subscriptions;
             //}
             //else
             //{
-            //    return null;
+            //    return Enumerable.Empty<ISubscription>();
             //}
-        }
+            #endregion
 
-        public IEnumerable<ISubscription> GetNavigationWithSubScriptions()
-        {
-            //if (Subscriptions != null&& Subscriptions.Count()>0)
-            //{
+            #region hardcoded data
             var subscriptions = new List<ISubscription>();
             ISubscription subscription = new SalesforceSubscription();
             subscription.Publication = "Frozen Foods";
@@ -110,13 +153,8 @@ namespace Informa.Web.ViewModels
             subscription2.Publication = "People";
             subscription2.ExpirationDate = new DateTime(2016, 10, 25);
             subscriptions.Add(subscription2);
-            //}
             return subscriptions;
-            //}
-            //else
-            //{
-            //    return nav;
-            //}
+            #endregion
         }
     }
 }
