6122b7835a5ab2a0ba7087cecda154a0573e2c3d
Kushal Kumar Paliwal <kpaliwal2@sapient.com>
2016-10-07 11:57:31 +0530

    Word plugin build issue fix.

--- src/Informa.Library/User/UserPreference/Channel.cs
+++ src/Informa.Library/User/UserPreference/Channel.cs
@@ -1,9 +1,12 @@
-﻿namespace Informa.Library.User.UserPreference
+﻿using System.Collections.Generic;
+
+namespace Informa.Library.User.UserPreference
 {
     public class Channel : IChannel
     {
         public int ChannelId { get; set; }
         public string ChannelName { get; set; }
         public int ChannelOrder { get; set; }
+        public IList<Topic> Topics { get; set; }
     }
 }
