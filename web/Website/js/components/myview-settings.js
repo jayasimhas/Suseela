de2e3508a7121ed98d24889b7aba7bac4b8e7954
SAPIENT\makul1 <makula@sapient.com>
2016-10-13 17:35:15 +0530

    Updated code

--- web/Website/js/components/myview-settings.js
+++ web/Website/js/components/myview-settings.js
@@ -75,16 +75,20 @@ $(function(){
 	});
 	
 	$('.publicationPan').on('click', '.accordionImg a', function(){
-		var $this = $(this), pPan = $this.closest('.publicationPan'), tbody = pPan.find('tbody');
+		var $this = $(this), pPan = $this.closest('.publicationPan'), thead = pPan.find('thead'), tbody = pPan.find('tbody'), trs = tbody.find('tr'), accCont = pPan.find('.accCont');
 		if($this.hasClass('collapsed')){
 			$this.removeClass('collapsed');
+			thead.find('.mtp').removeClass('vh');
 			tbody.addClass('tbodyhidden');
+			accCont.addClass('tbodyhidden');
 			pPan.find('.smfollowingBtn').hide();
 			pPan.find('.graybg').hide();
 		}
 		else{
 			$this.addClass('collapsed');
 			tbody.removeClass('tbodyhidden');
+			accCont.removeClass('tbodyhidden');
+			thead.find('.mtp').addClass('vh');
 			pPan.find('.smfollowingBtn').show();
 			pPan.find('.graybg').show();
 		}
