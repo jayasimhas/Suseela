71f9c5f7fbb383ac88d25a4b6434a5ca43df2f5c
SAPIENT\bbisw1 <bbiswal@sapient.com>
2016-10-18 12:45:03 +0530

    Fixed Build Issue

--- WordPlugin/SitecoreTreeWalker/UI/ArticleDetailsForm/ArticleDetail.cs
+++ WordPlugin/SitecoreTreeWalker/UI/ArticleDetailsForm/ArticleDetail.cs
@@ -689,21 +689,18 @@ namespace InformaSitecoreWord.UI.ArticleDetailsForm
                 Globals.SitecoreAddin.LogException("Error when parsing article on creation!", ex);
                 return;
             }
-
-
-            //added,21Sep16
-            if (articleDetailsPageSelector.pageArticleInformationControl.IsVerticalSelected() == false)
-            {
-                Cursor = Cursors.Arrow;
-                MessageBox.Show("Please Select Vertical.",@"Informa",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
-                return;
-            }
-           
             finally
             {
                 Cursor.Current = Cursors.Default;
             }
 
+            //added,21Sep16
+            if (articleDetailsPageSelector.pageArticleInformationControl.IsVerticalSelected() == false)
+            {
+                Cursor = Cursors.Arrow;
+                MessageBox.Show("Please Select Vertical.", @"Informa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
             try
             {
                 Cursor.Current = Cursors.WaitCursor;
