d032aceab86540469ba2afbed767ed64fd2171ca
hlaks1 <hlaks1@BLRHLAKS145957>
2016-10-18 09:22:57 +0530

    missed file

---
+++ web/Website/Areas/Account/Models/User/Personalization/PersonalizeUserPreferencesRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Informa.Web.Areas.Account.Models.User.Personalization
+{
+    public class PersonalizeUserPreferencesRequest
+    {
+        public string UserPreferences { get; set; }
+    }
+}
