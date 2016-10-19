2242963cafae32e3e65e9e7cf6e7471e33e0ffc7
soniaswamy <soniaswamy.guna@gmail.com>
2016-10-19 11:59:18 +0530

    comitt the index.js in dist folder

--- web/Website/dist/js/index.js
+++ web/Website/dist/js/index.js
@@ -634,33 +634,28 @@ module.exports = exports["default"];
 },{}],4:[function(require,module,exports){
 'use strict';
 
-var articleSidebarAd, articleSidebarAdParent, lastActionFlagsBar, stickyFloor;
-
+var articleSidebarAd, articleSidebarAdParent, lastActionFlagsBar, stickyFloor, sidebarIsTaller;
 $(document).ready(function () {
-	articleSidebarAdParent = $('.article-right-rail section:last-child');
-	articleSidebarAd = articleSidebarAdParent.find('.advertising');
-	lastActionFlagsBar = $('.action-flags-bar:last-of-type');
-	sidebarIsTaller = $('.article-right-rail').height() > $('.article-left-rail').height();
+    articleSidebarAdParent = $('.article-right-rail section:last-child');
+    articleSidebarAd = articleSidebarAdParent.find('.advertising');
+    lastActionFlagsBar = $('.action-flags-bar:last-of-type');
+    sidebarIsTaller = $('.article-right-rail').height() > $('.article-left-rail').height();
 });
-
 $(window).on('scroll', function () {
-	if (articleSidebarAdParent.length) {
-
-		// pageYOffset instead of scrollY for IE / pre-Edge compatibility
-		stickyFloor = lastActionFlagsBar.offset().top - window.pageYOffset - articleSidebarAd.height();
-
-		if (articleSidebarAdParent.offset().top - window.pageYOffset <= 16) {
-			articleSidebarAdParent.addClass('advertising--sticky');
-		} else {
-			articleSidebarAdParent.removeClass('advertising--sticky');
-		}
-
-		if (stickyFloor <= 40) {
-			articleSidebarAd.css('top', stickyFloor - 40 + 'px');
-		} else {
-			articleSidebarAd.css('top', '');
-		}
-	}
+    if (articleSidebarAdParent.length && !sidebarIsTaller) {
+        // pageYOffset instead of scrollY for IE / pre-Edge compatibility
+        stickyFloor = lastActionFlagsBar.offset().top - window.pageYOffset - articleSidebarAd.height();
+        if (articleSidebarAdParent.offset().top - window.pageYOffset <= 16) {
+            articleSidebarAdParent.addClass('advertising--sticky');
+        } else {
+            articleSidebarAdParent.removeClass('advertising--sticky');
+        }
+        if (stickyFloor <= 40) {
+            articleSidebarAd.css('top', stickyFloor - 40 + 'px');
+        } else {
+            articleSidebarAd.css('top', '');
+        }
+    }
 });
 
 },{}],5:[function(require,module,exports){
