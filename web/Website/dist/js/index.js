af033cc126d15600c7e650b920c3441360f352b6
soniaswamy <soniaswamy.guna@gmail.com>
2016-10-21 12:21:34 +0530

    comitting the swapping for my view-settings

--- web/Website/dist/js/index.js
+++ web/Website/dist/js/index.js
@@ -5928,10 +5928,15 @@ exports.toggleIcons = toggleIcons;
                     id: dragSrcEl.id
                 };
                 // swap all the data
-                dragSrcEl.innerHTML = oldEl.html;
-                dragSrcEl.id = oldEl.id;
+                var that = this;
+                // swap all the data
                 this.innerHTML = newEl.html;
                 this.id = newEl.id;
+                $(dragSrcEl).hide();
+                dragSrcEl.innerHTML = oldEl.html;
+                dragSrcEl.id = oldEl.id;
+                $(dragSrcEl).insertAfter(that);
+                $(dragSrcEl).show();
                 if (settings.dropAnimation) {
                     onAnimEnd(this);
                     onAnimEnd(dragSrcEl);
