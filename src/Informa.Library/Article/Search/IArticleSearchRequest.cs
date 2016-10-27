27a34e744008d78773230efd78d00d3bcbaa76c1
scha19 <scha19@BLRSCHA1939640>
2016-10-27 13:23:51 +0530

    BIWEB-821 - Solr Configuration - My View Page

---
+++ src/Informa.Library/Article/Search/IArticleSearchRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Informa.Library.Article.Search
+{
+    using System.Collections.Generic;
+
+    public interface IArticleSearchRequest
+    {
+        int PageNo { get; }
+        int PageSize { get; }
+        IList<string> TaxonomyIds { get; }
+    }
+}
