7461ff3a441c57df7d90a0d2ce499dc5dc9563c7
Tamer Mehyar <tmehyar@tanasuk.com>
2016-10-28 14:01:33 +0200

    Revert "Merge remote-tracking branch 'origin/IIS-472' into support"
    This reverts commit f9ac7a7f11a1d50633c1386c4a0a4bd5fd4286be, reversing
    changes made to 898f680bf2514b8e6843879dc5cd734d4447ce32.

--- src/Informa.Library/CustomSitecore/Events/ArticleItemCreated.cs
+++ src/Informa.Library/CustomSitecore/Events/ArticleItemCreated.cs
@@ -24,29 +24,21 @@ namespace Informa.Library.CustomSitecore.Events
 
 		public void Process(object sender, EventArgs args)
 		{
-			try {
-				if (publicationWorkflow == null)
-					return;
+			if (publicationWorkflow == null)
+				return;
 
-				Assert.ArgumentNotNull(args, "args");
+			Assert.ArgumentNotNull(args, "args");
 
-				ItemCreatedEventArgs icArgs = Event.ExtractParameter(args, 0) as ItemCreatedEventArgs;
-				var item = icArgs.Item;
-				if (item == null)
-					return;
+			ItemCreatedEventArgs icArgs = Event.ExtractParameter(args, 0) as ItemCreatedEventArgs;
+			var item = icArgs.Item;
 
-				//If article save process
-				var itemPath = item.Paths.Path.ToLower();
-				if (itemPath.StartsWith("/sitecore/content")
-						&& !itemPath.Contains("/issues/") // detect if this is an issue clone, since the fields arent set yet on item created
-						&& item.TemplateID.Guid == Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.IArticleConstants.TemplateId.Guid)
-				{
-					changeWorkflowStateAndExecuteActions(item, new ID(publicationWorkflow.GetInitialState(item)._Id), new ID(publicationWorkflow.GetPublicationWorkflow(item)._Id));
-				}
-			}
-			catch (Exception ex)
+			//If article save process
+			var itemPath = item.Paths.Path.ToLower();
+			if (itemPath.StartsWith("/sitecore/content")
+					&& !itemPath.Contains("/issues/") // detect if this is an issue clone, since the fields arent set yet on item created
+					&& item.TemplateID.Guid == Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.IArticleConstants.TemplateId.Guid)
 			{
-				Sitecore.Diagnostics.Log.Fatal("ArticleItemCreated: " + ex.ToString(), this);
+				changeWorkflowStateAndExecuteActions(item, new ID(publicationWorkflow.GetInitialState(item)._Id), new ID(publicationWorkflow.GetPublicationWorkflow(item)._Id));
 			}
 		}
 
