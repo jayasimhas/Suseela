0cfd2872cb6e9060c3ca532c679f8b6392a1d3f6
soniaswamy <soniaswamy.guna@gmail.com>
2016-11-03 18:44:14 +0530

    IPMP-616

--- web/Website/dist/js/index.js
+++ web/Website/dist/js/index.js
@@ -3880,6 +3880,7 @@ function getParameterByName(name, url) {
 }
 
 $(document).ready(function () {
+
     //AM Charts
     if ($('#amchartData') && $('#amchartData').length) {
         var amchartVal = JSON.parse($('#amchartData').val()),
@@ -4432,16 +4433,33 @@ $(document).ready(function () {
         });
 
         $('.show-demo').click(function () {
-            $(this).closest('.js-toggle-demo').toggleClass('collapsed');
 
+            $(this).closest('.js-toggle-demo').toggleClass('collapsed');
+            //IPMP-616	
             if ($(this).parent().hasClass('collapsed')) {
+                sessionStorage.setItem("mykey", "false");
                 $('.hd').show();
                 $('.sd').hide();
+                $('.toggle-demo').show();
             } else {
+                sessionStorage.setItem("mykey", "true");
                 $('.sd').show();
                 $('.hd').hide();
+                $('.toggle-demo').hide();
+            }
+            var persistedval = sessionStorage.getItem("mykey");
+            if (persistedval == "false") {
+                $('.toggle-demo').show();
+            } else {
+                $('.toggle-demo').hide();
             }
         });
+        var persistedval = sessionStorage.getItem("mykey");
+        if (persistedval == "false") {
+            $('.toggle-demo').hide();
+        } else {
+            $('.toggle-demo').show();
+        }
     })();
 
     /* * *
