a472ea73b0b0d19369eebda131362b669dad1328
Kushal Kumar Paliwal <kpaliwal2@sapient.com>
2016-11-04 15:36:44 +0530

    Code changes for new search facet configuration.

---
+++ src/Informa.Library/Search/ComputedFields/Facets/AgencyRegulatorField.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Informa.Library.Search.Utilities;
+using Informa.Library.Services.Search.Fields.Base;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Base_Templates;
+
+namespace Informa.Library.Search.ComputedFields.Facets
+{
+    public class AgencyRegulatorField : BaseGlassComputedField<I___BaseTaxonomy>
+    {
+        public override object GetFieldValue(I___BaseTaxonomy indexItem)
+        {
+            if (indexItem?.Taxonomies != null)
+            {
+                var agencyRegulatorTaxonomyItems = indexItem.Taxonomies.Where(x => SearchTaxonomyUtil.IsAgencyRegulatorTaxonomy(x._Path));
+
+                return agencyRegulatorTaxonomyItems.Where(x => !string.IsNullOrEmpty(x.Item_Name)).Select(x => x.Item_Name.Trim()).ToList();
+            }
+            return new List<string>();
+        }
+    }
+}
