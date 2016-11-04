230ed77a5ef1eff22f5519d70b735256377ebe35
Saurabh Sinha <ssi253@sapient.com>
2016-11-04 10:34:37 +0530

    Commiting changes in index.js for Bugs

--- web/Website/js/index.js
+++ web/Website/js/index.js
@@ -584,7 +584,7 @@ $(document).ready(function(){
         observe: '.form-email-article',
         successCallback: function(form) {
             $('.js-email-article-form-wrapper').hide();
-            $('.js-email-article-recip-success').html($('.js-email-article-recip-addr').val());
+            $('.js-email-article-recip-success').html($('.js-email-article-recip-addr').val().split(';').join('; '));
             $('.js-email-article-success').show();
 
             // Reset the Email Article pop-out to its default state when closed
