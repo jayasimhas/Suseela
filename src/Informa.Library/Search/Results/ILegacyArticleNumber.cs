719247cb80ec9a004123bb402a7ca886351f40f3
Tamer Mehyar <tmehyar@tanasuk.com>
2016-11-09 14:46:54 +0200

    Fixed merging issue

--- src/Informa.Library/Search/Results/ILegacyArticleNumber.cs
+++ src/Informa.Library/Search/Results/ILegacyArticleNumber.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
+using Sitecore.ContentSearch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
