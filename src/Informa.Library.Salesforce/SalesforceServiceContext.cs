f8b6896fe3f8e3e586574a92ad2e03bb083c9fe0
Kushal Kumar Paliwal <kpaliwal2@sapient.com>
2016-11-09 13:06:02 +0530

    Placed the logging in SalesforceServiceContext class to log Beta issue.

--- src/Informa.Library.Salesforce/SalesforceServiceContext.cs
+++ src/Informa.Library.Salesforce/SalesforceServiceContext.cs
@@ -1,111 +1,118 @@
 ﻿using Informa.Library.Salesforce.EBIWebServices;
+using Newtonsoft.Json;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace Informa.Library.Salesforce
 {
-	public class SalesforceServiceContext : ISalesforceServiceContext
-	{
-		private const string InvalidSessionIdErrorKey = "INVALID_SESSION_ID";
+    public class SalesforceServiceContext : ISalesforceServiceContext
+    {
+        private const string InvalidSessionIdErrorKey = "INVALID_SESSION_ID";
 
-		protected readonly ISalesforceService Service;
-		protected readonly ISalesforceSessionContext SessionContext;
-		protected readonly ISalesforceErrorLogger ErrorLogger;
-		protected readonly ISalesforceDebugLogger DebugLogger;
-		protected readonly ISalesforceServiceContextEnabled ServiceContextEnabled;
+        protected readonly ISalesforceService Service;
+        protected readonly ISalesforceSessionContext SessionContext;
+        protected readonly ISalesforceErrorLogger ErrorLogger;
+        protected readonly ISalesforceDebugLogger DebugLogger;
+        protected readonly ISalesforceServiceContextEnabled ServiceContextEnabled;
 
-		public SalesforceServiceContext(
-			ISalesforceService service,
-			ISalesforceSessionContext sessionContext,
-			ISalesforceErrorLogger errorLogger,
-						ISalesforceDebugLogger debugLogger,
-			ISalesforceServiceContextEnabled serviceContextEnabled)
-		{
-			Service = service;
-			SessionContext = sessionContext;
-			ErrorLogger = errorLogger;
-			DebugLogger = debugLogger;
-			ServiceContextEnabled = serviceContextEnabled;
+        public SalesforceServiceContext(
+            ISalesforceService service,
+            ISalesforceSessionContext sessionContext,
+            ISalesforceErrorLogger errorLogger,
+                        ISalesforceDebugLogger debugLogger,
+            ISalesforceServiceContextEnabled serviceContextEnabled)
+        {
+            Service = service;
+            SessionContext = sessionContext;
+            ErrorLogger = errorLogger;
+            DebugLogger = debugLogger;
+            ServiceContextEnabled = serviceContextEnabled;
 
-			Service.SessionHeaderValue = new SessionHeader();
-		}
+            Service.SessionHeaderValue = new SessionHeader();
+        }
 
-		public void RefreshSession()
-		{
-			DebugLogger.Log($"Refresh Session: Header Value: {Service.SessionHeaderValue.sessionId}, ContextValue: {SessionContext.Session.Id}");
+        public void RefreshSession()
+        {
+            DebugLogger.Log($"Refresh Session: Header Value: {Service.SessionHeaderValue.sessionId}, ContextValue: {SessionContext.Session.Id}");
 
-			if (string.Equals(Service.SessionHeaderValue.sessionId, SessionContext.Session.Id))
-			{
-				SessionContext.Refresh();
-			}
+            if (string.Equals(Service.SessionHeaderValue.sessionId, SessionContext.Session.Id))
+            {
+                SessionContext.Refresh();
+            }
 
-			try
-			{
-				Service.SessionHeaderValue.sessionId = SessionContext.Session.Id;
-			}
-			catch (Exception e)
-			{
-				ErrorLogger.Log("Is this the 'Cannot change header value' issue I've been seeing?", e);
-			}
-		}
+            try
+            {
+                Service.SessionHeaderValue.sessionId = SessionContext.Session.Id;
+            }
+            catch (Exception e)
+            {
+                ErrorLogger.Log("Is this the 'Cannot change header value' issue I've been seeing?", e);
+            }
+        }
 
-		public TResult Execute<TResult>(Func<ISalesforceService, TResult> function, string CallerMemberName = "", string CallerFilePath = "",
-						int CallerLineNumber = 0)
-			where TResult : IEbiResponse
-		{
-			DebugLogger.Log($"Salesforced called by: {CallerMemberName}, File: {CallerFilePath}, Line Number {CallerLineNumber}");
+        public TResult Execute<TResult>(Func<ISalesforceService, TResult> function, string CallerMemberName = "", string CallerFilePath = "",
+                        int CallerLineNumber = 0)
+            where TResult : IEbiResponse
+        {
+            DebugLogger.Log($"Salesforced called by: {CallerMemberName}, File: {CallerFilePath}, Line Number {CallerLineNumber}");
+            ErrorLogger.Log("Salesforced called by: " + CallerMemberName + ", File:  " + CallerFilePath + ", Line Number " + CallerLineNumber, new Exception());
+            ErrorLogger.Log("Salesforce Function: " + JsonConvert.SerializeObject(function), new Exception());
+            ErrorLogger.Log("Salesforce Service Session Value: " + Service.SessionHeaderValue.sessionId, new Exception());
+            if (!ServiceContextEnabled.Enabled)
+            {
+                return default(TResult);
+            }
 
-			if (!ServiceContextEnabled.Enabled)
-			{
-				return default(TResult);
-			}
+            if (!HasSession)
+            {
+                ErrorLogger.Log("Salesforce Refreshing Session", new Exception());
+                RefreshSession();
+                ErrorLogger.Log("Salesforce Service Session Value: " + Service.SessionHeaderValue.sessionId, new Exception());
+            }
 
-			if (!HasSession)
-			{
-				RefreshSession();
-			}
+            var result = default(TResult);
+            var invalidSession = false;
 
-			var result = default(TResult);
-			var invalidSession = false;
+            try
+            {
+                result = function(Service);
+                ErrorLogger.Log("Salesforce call output - Is Successfull: " + result.IsSuccess() + " Result: " + JsonConvert.SerializeObject(result), new Exception());
+                invalidSession = result.errors != null && result.errors.Any(e => e != null && string.Equals(e.statusCode, InvalidSessionIdErrorKey, StringComparison.InvariantCultureIgnoreCase));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains(InvalidSessionIdErrorKey))
+                {
+                    ErrorLogger.Log("Invalid Session, infinite loop?", ex);
+                    invalidSession = true;
+                    SessionContext.Refresh();
+                }
 
-			try
-			{
-				result = function(Service);
+                ErrorLogger.Log("Execute", ex);
+            }
 
-				invalidSession = result.errors != null && result.errors.Any(e => e != null && string.Equals(e.statusCode, InvalidSessionIdErrorKey, StringComparison.InvariantCultureIgnoreCase));
-			}
-			catch (Exception ex)
-			{
-				if (ex.Message.Contains(InvalidSessionIdErrorKey))
-				{
-					ErrorLogger.Log("Invalid Session, infinite loop?", ex);
-					invalidSession = true;
-					SessionContext.Refresh();
-				}
+            if (invalidSession)
+            {
+                RefreshSession();
+                DebugLogger.Log($"Invalid Session from caller: {function.Method.Name}");
+                ErrorLogger.Log("Invalid Session from caller: " + function.Method.Name, new Exception());
+                return Execute(function);
+            }
 
-				ErrorLogger.Log("Execute", ex);
-			}
+            if (!result.IsSuccess() && result.errors != null)
+            {
+                foreach (var error in result.errors)
+                {
+                    DebugLogger.Log($"Request Failed: {error?.message}");
+                    ErrorLogger.Log("Request Failed: " + error?.message, new Exception());
+                }
 
-			if (invalidSession)
-			{
-				RefreshSession();
-				DebugLogger.Log($"Invalid Session from caller: {function.Method.Name}");
-				return Execute(function);
-			}
+            }
 
-			if (!result.IsSuccess() && result.errors != null)
-			{
-				foreach (var error in result.errors)
-				{
-					DebugLogger.Log($"Request Failed: {error?.message}");
-				}
+            return result;
+        }
 
-			}
-
-			return result;
-		}
-
-		public bool HasSession => !string.IsNullOrWhiteSpace(Service.SessionHeaderValue.sessionId);
-	}
+        public bool HasSession => !string.IsNullOrWhiteSpace(Service.SessionHeaderValue.sessionId);
+    }
 }
