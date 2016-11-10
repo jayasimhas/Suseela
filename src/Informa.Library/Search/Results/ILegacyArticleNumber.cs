9457262f75af36451894170c750a29814f9c3398
ladanVelir <ladan.ashrafi@velir.com>
2016-11-10 11:24:37 -0500

    fixing compiling errors

--- src/Informa.Library/Search/Results/ILegacyArticleNumber.cs
+++ src/Informa.Library/Search/Results/ILegacyArticleNumber.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
+using Sitecore.ContentSearch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace Informa.Library.Search.Results
 {
-	public interface ILegacyArticleNumber
-	{
-		[IndexField(IArticleConstants.Legacy_Article_NumberFieldName)]
-		string LegacyArticleNumber { get; set; }
-	}
+    public interface ILegacyArticleNumber
+    {
+        [IndexField(IArticleConstants.Legacy_Article_NumberFieldName)]
+        string LegacyArticleNumber { get; set; }
+    }
 }
