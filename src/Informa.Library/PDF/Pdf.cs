0c851d66daab8ca510fbd3a34eeb48a96018637e
SAPIENT\mbirad <mbiradara@sapient.com>
2016-11-29 15:59:34 +0530

    committing pdf.cs file

---
+++ src/Informa.Library/PDF/Pdf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informa.Library.PDF
+{
+    public class Pdf
+    {
+        public DateTime IssueDate { get; set; }
+        public string IssueNumber { get; set; }
+        public string PdfPageUrl { get; set; }
+        public PdfType TypeOfPdf { get; set; }
+        public string PdfTitle { get; set; }
+    }
+    public enum PdfType
+    {
+        Static = 1,
+        Manual = 2
+    }
+}
