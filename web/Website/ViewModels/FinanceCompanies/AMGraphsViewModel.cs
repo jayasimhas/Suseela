a36909020159ffaf865d87dbe6eacb5c8af9974e
hlaks1 <hlaks1@BLRHLAKS145957>
2016-12-13 11:08:32 +0530

    New files for company AM charts

---
+++ web/Website/ViewModels/FinanceCompanies/AMGraphsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.ViewModels.FinanceCompanies
+{
+    public class AMGraphsViewModel
+    {
+    }
+}
