2a28caeca198624fe5de8907f8e844b23958350d
SAPIENT\ebodkhe <ebodkhe@sapient.com>
2016-12-14 13:37:05 +0530

    no message

--- web/Website/Controllers/PersonalizedEmailController.cs
+++ web/Website/Controllers/PersonalizedEmailController.cs
@@ -3,6 +3,14 @@ using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Net;
 using System.Text;
+using Informa.Library.Site;
+using Informa.Library.Mail;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Configuration;
+using Informa.Library.Services.Global;
+using Sitecore.Configuration;
+using System.Collections.Generic;
+using Informa.Library.Utilities.Extensions;
+using System;
 
 namespace Informa.Web.Controllers
 {
@@ -10,23 +18,91 @@ namespace Informa.Web.Controllers
     public class PersonalizedEmailController : ApiController
     {
         private readonly EmailUtil _emailUtil;
-
+        
         public PersonalizedEmailController(EmailUtil emailUtil)
         {
+           
             _emailUtil = emailUtil;
         }
 
         [HttpGet]
         public HttpResponseMessage Get(string userId)
         {
+
+            var isMailSent = false;
+
             var response = new HttpResponseMessage();
             response.StatusCode = string.IsNullOrWhiteSpace(userId) ? HttpStatusCode.BadRequest : HttpStatusCode.OK;
             if (!string.IsNullOrWhiteSpace(userId))
             {
-                var emailContent = _emailUtil.CreatePersonalizedEmailBody(userId);
-                response.Content = new StringContent(emailContent, Encoding.UTF8, "text/html");
+             
+                var emailBody = GetEmailBody(userId);
+                var personalizedEmail = new Email
+                {
+                    To = userId,
+                    Subject = "Personalized Test",
+                    From = "test@sapient.com",
+                    Body = emailBody,
+                    IsBodyHtml = true
+                };
+
+                EmailSender EmailSender = new EmailSender();
+
+                var isEmailSent = EmailSender.Send(personalizedEmail);
+                 isMailSent = isEmailSent;
+                if (!isEmailSent)
+                {
+                   // _logger.Warn($"Email sender failed");
+                   // result = false;
+                }
+
+
             }
+
             return response;
         }
+
+
+
+
+        public string GetEmailBody(string senderEmail)
+        {
+            string emailHtml = string.Empty;
+            try
+            {
+                   
+ 
+                 IHtmlEmailTemplateFactory HtmlEmailTemplateFactory = new HtmlEmailTemplateFactory();
+                 var htmlEmailTemplate = HtmlEmailTemplateFactory.Create("Emailtemplatepersonalization");
+
+
+                if (htmlEmailTemplate == null)
+                {
+                    return null;
+                }
+
+                emailHtml = htmlEmailTemplate.Html;
+                var replacements = new Dictionary<string, string>
+                {
+                    ["#FirstName#"] = senderEmail,
+                    ["#Personalized_Content#"] = senderEmail,
+                    ["#email#"] = senderEmail,
+                };
+
+                emailHtml = emailHtml.ReplacePatternCaseInsensitive(replacements);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return emailHtml;
+        }
+
+
+        public string GetValue(string value, string defaultValue = null)
+        {
+            return value ?? defaultValue ?? string.Empty;
+        }
+
     }
 }
