a8ad80fb74a6d046c32e565b93f7403a53a79d15
Kushal Kumar Paliwal <kpaliwal2@sapient.com>
2016-12-26 12:53:26 +0530

    Adding missing file.

---
+++ web/Website/Areas/User/UserAreaRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Informa.Web.Areas.UserRequest
+{
+    public class UserAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "User";
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "User_default",
+                "User/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
