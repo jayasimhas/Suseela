b23a7b31cfb13e6830181abe5bdbb983ea9bf057
avisw1 <aviswanadhan@sapient.com>
2017-01-02 16:53:57 +0530

    Ts3|Search changes|Aji V

---
+++ src/Informa.Library/Search/SearchManager/MaritimeSearchManager.cs
@@ -0,0 +1,31 @@
+﻿using Glass.Mapper.Sc;
+using Informa.Library.Search.Results;
+using System;
+using Velir.Search.Core.Managers;
+
+namespace Informa.Library.Search.SearchManager
+{
+    public class MaritimeSearchManager
+    {
+        private static ISearchManager<InformaSearchResultItem> _searchManager;
+        private static object syncRoot = new Object();
+
+        public static ISearchManager<InformaSearchResultItem> CreateInstance(string indexName, ISitecoreContext context)
+        {
+            if (_searchManager == null)
+            {
+                lock (syncRoot)
+                {
+                    if (_searchManager == null)
+                    {
+                        _searchManager = new SearchManager<InformaSearchResultItem>(indexName, context);
+                    }
+                }
+            }
+
+            return _searchManager;
+        }
+    }
+}
+
+
