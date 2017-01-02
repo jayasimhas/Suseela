653d794a1fe1b71e44d36d29035487407a6f1968
avisw1 <aviswanadhan@sapient.com>
2017-01-02 16:53:57 +0530

    Merging Search changes by Aji to development

--- src/Informa.Library/Search/ComputedFields/Facets/CompaniesField.cs
+++ src/Informa.Library/Search/ComputedFields/Facets/CompaniesField.cs
@@ -2,6 +2,10 @@
 using Informa.Library.Search.Utilities;
 using Informa.Library.Services.Search.Fields.Base;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
+using Sitecore.Data.Items;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using System.Linq;
 
 namespace Informa.Library.Search.ComputedFields.Facets
 {
@@ -9,7 +13,22 @@ namespace Informa.Library.Search.ComputedFields.Facets
 	{
 		public override object GetFieldValue(IArticle indexItem)
 		{
-			if (string.IsNullOrEmpty(indexItem.Referenced_Companies))
+            Item rootItem = null;
+            var dbContext = Factory.GetDatabase("master");
+            Item item = dbContext.GetItem(new ID(indexItem._Id));
+
+            if (item != null)
+                rootItem = item.Axes.GetAncestors().FirstOrDefault(ancestor => ancestor.TemplateID.ToString() == Settings.GetSetting("VerticalTemplate.global"));
+            if (rootItem != null && rootItem.Name.Contains("Agri"))
+            {
+                if (indexItem?.Taxonomies != null)
+                {
+                    var companiesTaxonomy = indexItem.Taxonomies.Where(x => SearchTaxonomyUtil.IsCompaniesTaxonomy(x._Path));
+                    return companiesTaxonomy.Where(x => !string.IsNullOrEmpty(x.Item_Name)).Select(x => x.Item_Name.Trim()).ToList();
+                }
+            }
+
+            if (string.IsNullOrEmpty(indexItem.Referenced_Companies))
 			{
 				return new List<string>();
 			}
