40ba2d1423ec6103cc79835465bcff12f0c5d2be
SAPIENT\mbirad <mbiradara@sapient.com>
2017-01-03 11:56:03 +0530

    Committing CompanyModel.cs file

---
+++ web/Website/ViewModels/FinanceCompanies/CompanyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.ViewModels.FinanceCompanies
+{
+    public class CompanyModel
+    {
+        public string CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyLogo { get; set; }
+        public string CompanyLandingPageUrl { get; set; }
+        public string CompanyGraphPageUrl { get; set; }
+        public IEnumerable<AMGraph> Graphs { get; set; }
+    }
+}
