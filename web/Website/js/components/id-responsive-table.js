b1aa841517cc8ad4dd14d01a60ce484c3019cc85
Saurabh Sinha <ssi253@sapient.com>
2017-01-03 21:02:58 +0530

    Responsive Table Update on Links

--- web/Website/js/components/id-responsive-table.js
+++ web/Website/js/components/id-responsive-table.js
@@ -61,7 +61,7 @@
 			for(var i = 0; i < data.length; i++) {
 				var Item = data[i], index = i,
 					CompanyLink = data[i].Company.toLowerCase().split(" ").join("-");
-				Parent.find('.states_heading').append('<div class="RB16"><a href="' + CompanyLink + '">' +data[i].Company+ '</a></div>');
+				Parent.find('.states_heading').append('<div class="RB16"><a href="' + data[i].CompanyPageUrl + '">' +data[i].Company+ '</a></div>');
 				for(var key in Item) {
 					if(Array.isArray(Item[key])) {
 						Parent.find('.article[data-head="' +key+ '"]').append('<div  class="R16 TableRow'+index+'">' +Item[key][0].value+ '</div>');
