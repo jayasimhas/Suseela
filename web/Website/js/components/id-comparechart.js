658339fc3a46367c66c9fb28d020e751ebde7651
SAPIENT\makul1 <makula@sapient.com>
2017-01-11 09:05:03 +0530

    Updated  code

--- web/Website/js/components/id-comparechart.js
+++ web/Website/js/components/id-comparechart.js
@@ -2,15 +2,17 @@ $(function(){
 	$('.availableGraphs').off('click').on('click', 'li a', function () {
 		var $this = $(this), id = $this.attr('id'), getFocusId = id.split('-')[1];
 		$(window).scrollTop($('#' + getFocusId).offset().top);
-		$('#'+getFocusId).trigger('click');
+		$('#'+getFocusId).find('.chartexpand').trigger('click');
 	});
 	
 	var chartAccordionIDs = ["GWP", "NWP", "UR", "NP", "SF", "NWPNR", "SFNR", "NPSFR"];
 	$('.expandAll', '.compareChart').click(function(){
 		var eachChartData  = $('.eachChartData');
 		for(var i = 0; i < chartAccordionIDs.length; i++){
-			$(eachChartData[i]).find('.chartexpand').click();
-			$("#"+chartAccordionIDs[i]+"-DATA").removeClass('hide');
+			if(!$(eachChartData[i]).find('.chartexpand').hasClass('active')){
+				$(eachChartData[i]).find('.chartexpand').click();
+				//$("#"+chartAccordionIDs[i]+"-DATA").removeClass('hide');
+			}
 		}
 		$('.chartexpand', '.compareChart').addClass('active');
 	});
@@ -36,7 +38,7 @@ $(function(){
 	if(geturl.indexOf('graphid=') !== -1){
 		var getId = geturl.split('graphid=')[1];
 		$(window).scrollTop($('#' + getId).offset().top);
-		$('#'+getId).find('.chartexpand').trigger('click');
+		$('#'+getId).find('.chartexpand').trigger('click'); 
 	}
 });
 
