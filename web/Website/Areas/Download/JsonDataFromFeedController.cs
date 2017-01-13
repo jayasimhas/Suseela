b4804e2b37d6acfb1e0f5d416fab400d447d7209
SAPIENT\mbirad <mbiradara@sapient.com>
2017-01-13 15:37:53 +0530

    IPMP-1323 changes

---
+++ web/Website/Areas/Download/JsonDataFromFeedController.cs
@@ -0,0 +1,32 @@
+﻿using Informa.Library.Services.ExternalFeeds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Informa.Web.Areas.Download
+{
+    public class JsonDataFromFeedController : Controller
+    {
+        protected readonly ICompaniesResultService CompanyResultService;
+        public JsonDataFromFeedController(ICompaniesResultService companyResultService)
+        {
+            CompanyResultService = companyResultService;
+        }
+        // GET: Account/JsonDataFromFeed
+        public string ReadJsonShippingMovements(string feed, string area, string movementType)
+        {
+            if (!string.IsNullOrEmpty(feed) && !string.IsNullOrEmpty(area) && !string.IsNullOrEmpty(movementType))
+            {
+                string feedUrl = string.Format(feed, area, movementType);
+                return CompanyResultService.GetCompanyFeeds(feed).Result;
+            }
+            else
+            {
+                return System.IO.File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("~/Views/Casualty/ShippingMovements.json"));
+            }
+        }
+    }
+}
---
+++ web/Website/ViewModels/Casualty/GoToPortViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Glass.Mapper.Sc.Fields;
+using Informa.Library.Site;
+using Informa.Library.User.Authentication;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Components;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Informa.Web.ViewModels.Casualty
+{
+    public class GoToPortViewModel : GlassViewModel<IGo_To_Port_Component>
+    {
+        protected readonly ISiteRootContext SiterootContext;
+        private readonly IAuthenticatedUserContext AuthenticatedUserContext;
+        public GoToPortViewModel(ISiteRootContext siterootContext,
+            IAuthenticatedUserContext authenticatedUserContext)
+        {
+            SiterootContext = siterootContext;
+            AuthenticatedUserContext = authenticatedUserContext;
+        }
+
+        public bool IsUserAuthenticated => AuthenticatedUserContext.IsAuthenticated;
+        /// <summary>
+        /// HomePage URL
+        /// </summary>
+        public string homePageUrl => SiterootContext?.Item._Url;
+        public Image Logo => GlassModel?.Logo;
+        public string TitleWithResult => GlassModel?.Title_With_Result;
+        public string TitleWithoutResult => GlassModel?.Title_Without_Result;
+        public string Description => GlassModel?.Description;
+    }
+}
