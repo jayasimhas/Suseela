12d65fbe11607e8a1f123e1cf714fa358240645d
SAPIENT\mbirad <mbiradara@sapient.com>
2017-01-13 21:20:37 +0530

    Added downloadarearegistration.cs

---
+++ web/Website/Areas/Download/DownloadAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Http;
+
+
+namespace Informa.Web.Areas.Download
+{
+    public class DownloadAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Download";
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.Routes.MapHttpRoute(
+                "Download_api",
+                "Download/api/{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+
+            context.MapRoute(
+                "Download_default",
+                "Download/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
