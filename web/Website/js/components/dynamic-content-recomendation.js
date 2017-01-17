234d9bd1867ef6e654de46884b30d76ec3736f35
Saurabh Sinha <ssi253@sapient.com>
2017-01-17 11:33:06 +0530

    Merges & Acquisition & data tool landing reverted changes fix

--- web/Website/js/components/dynamic-content-recomendation.js
+++ web/Website/js/components/dynamic-content-recomendation.js
@@ -20,21 +20,20 @@
                 var Template = '';
                 if (data.articles.length > 0) {
                     for (var i = 0; i < 3; i++) {
-                    	if(data.articles[i].listableImage == null) {
-                    		Template += '<div class="section-article">' +
-                                             '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
-                                            '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
-                                             '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
-                                      '</div>';
-                                  } else {
-                                  	Template += '<div class="section-article">' +
-                                             '<img class="article-related-content__img" src="' + data.articles[i].listableImage + '">' +
-                                             '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
-                                            '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
-                                             '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
-                                      '</div>';
-                                  }
-                        
+                        if(data.articles[i].listableImage == null) {
+                            Template += '<div class="section-article">' +
+                                         '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
+                                        '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
+                                         '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
+                                  '</div>';
+                        } else {
+                            Template += '<div class="section-article">' +
+                                         '<img class="article-related-content__img" src="' + data.articles[i].listableImage + '">' +
+                                         '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
+                                        '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
+                                         '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
+                                  '</div>';
+                        }
                     }
                 }
 
@@ -47,21 +46,20 @@
                 var Template = '';
                 if (data.articles.length > 0) {
                     for (var i = 0; i < 3; i++) {
-                    	if(data.articles[i].listableImage == null) {
-                    		Template += '<div class="contentRecomm-article">' +
-                                             '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
-                                             '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
-                                             '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
-                                      '</div>';
-                    	} else {
-                        	Template += '<div class="contentRecomm-article">' +
-                                             '<img class="article-related-content__img" src="' + data.articles[i].listableImage + '">' +
-                                             '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
-                                             '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
-                                             '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
-                                      '</div>';
-                        }
-                    }
+                        if(data.articles[i].listableImage == null) {
+                            Template += '<div class="contentRecomm-article">' +
+                                         '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
+                                         '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
+                                         '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
+                                  '</div>';
+                        } else {
+                        Template += '<div class="contentRecomm-article">' +
+                                         '<img class="article-related-content__img" src="' + data.articles[i].listableImage + '">' +
+                                         '<span class="article-related-content__category"> ' + data.articles[i].listablePublication + ' </span>' +
+                                         '<h5><a class="click-utag" href="' + data.articles[i].linkableUrl + '">' + data.articles[i].listableTitle + '</a></h5>' +
+                                         '<time class="article-related-content__date">' + data.articles[i].listableDate + '</time>' +
+                                  '</div>';
+                    }   }
                 }
 
                 Template += '</div>';
