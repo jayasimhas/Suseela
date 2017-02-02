ea4e456ed66c1c793c0a3cc45e5a1186b5264927
SAPIENT\makul1 <makula@sapient.com>
2017-02-02 13:38:21 +0530

    Updated code

--- web/Website/js/components/ll-tanker-fixtures.js
+++ web/Website/js/components/ll-tanker-fixtures.js
@@ -97,16 +97,17 @@
 				$.each(value, function(key, val){
 					mobileStr += '<thead class="table_head">';
 					mobileStr += '<tr>';
-					mobileStr += '<th colspan="8" class="pad-full-10">'+key+'</th>';
+					mobileStr += '<th colspan="2" class="pad-full-10">'+key+'</th>';
 					mobileStr += '</tr>';
 					mobileStr += '</thead>';
 					
 					mobileStr += '<tbody class="visible-sm">';
 					$.each(val, function(i, v){
+						
 						for(var prop in v){
 							mobileStr += '<tr>';
-							mobileStr += '<td class="pad-10 R21_GrayColor">'+prop+'</td>';
-							mobileStr += '<td class="pad-10 R21_GrayColorVal">'+v[prop]+'</td>';
+							mobileStr += '<td class="pad-10 mobleftCol">'+prop+'</td>';
+							mobileStr += '<td class="pad-10 mobrigCol">'+v[prop]+'</td>';
 							mobileStr += '</tr>';
 						}
 					});
