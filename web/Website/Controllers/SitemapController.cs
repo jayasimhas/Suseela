b75b1166731e273b3db41eeb33ed60fe267c7d8b
SAPIENT\bbisw1 <bbiswal@sapient.com>
2017-02-03 15:51:51 +0530

    Reverted New Cache Key

--- web/Website/Controllers/SitemapController.cs
+++ web/Website/Controllers/SitemapController.cs
@@ -32,7 +32,7 @@ namespace Informa.Web.Controllers
                 SitemapQueryString = "?page=" + pageNo;
             }
             string path = HttpContext.Current.Request.Path.Replace("/", "").Replace(".xml", "") + SitemapQueryString;
-            string cacheKey = $"{HttpContext.Current.Request.Url.Host}.{path}.SitemapQATesting";
+            string cacheKey = $"{HttpContext.Current.Request.Url.Host}.{path}.Sitemap";
             string xml = CacheProvider.GetFromCache(cacheKey, () => BuildSitemapXml(path));
 
             //provide default
