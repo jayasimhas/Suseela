867e3f3677498a67b146e27e44f2a9e0795e1656
SAPIENT\mbirad <mbiradara@sapient.com>
2017-02-08 12:44:55 +0530

    CompanyAddress and CompanyProfile components made reusable on companyGraph and PeerGroup pages

--- web/Website/ViewModels/FinanceCompanies/CompanyAddressViewModel.cs
+++ web/Website/ViewModels/FinanceCompanies/CompanyAddressViewModel.cs
@@ -6,29 +6,27 @@ using Informa.Library.Globalization;
 using Informa.Library.Site;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.Company;
 using Jabberwocky.Glass.Autofac.Mvc.Models;
-
+using Glass.Mapper.Sc;
 
 namespace Informa.Web.ViewModels.FinanceCompanies
 {
-    public class CompanyAddressViewModel : GlassViewModel<ICompany_Detail_Page> 
+    public class CompanyAddressViewModel : GlassViewModel<ICompany_Detail_Page>
     {
-        protected readonly ISiteRootContext SiteRootContext;
+        
         protected readonly ITextTranslator TextTranslator;
-
-        public CompanyAddressViewModel(ISiteRootContext siteRootContext, ITextTranslator textTranslator)
+        protected readonly ISitecoreContext Context;
+        public CompanyAddressViewModel(
+            ITextTranslator textTranslator,
+            ISitecoreContext context)
         {
-            SiteRootContext = siteRootContext;
             TextTranslator = textTranslator;
+            Context = context;
         }
-
-
-        public string CompanyID => GlassModel.CompanyID;
-        public string CompanyName => GlassModel.Companyname;
-        public string CompanyAddress => GlassModel.Company_Address;
-        public string CompanyLogo => GlassModel.Company_Logo?.Src;
-        public string CompanyUrl => GlassModel.Company_Url?.Url;
-        public string CompanyIntroductionText => GlassModel.Introduction_Text;
-
-       
+        string id = HttpContext.Current.Request.QueryString["Id"];       
+        public string CompanyName => !string.IsNullOrEmpty(id) ? Context.GetItem<ICompany_Detail_Page>(id)?.Companyname : GlassModel?.Companyname;
+        public string CompanyAddress => !string.IsNullOrEmpty(id) ? Context.GetItem<ICompany_Detail_Page>(id)?.Company_Address : GlassModel?.Company_Address;
+        public string CompanyLogo => !string.IsNullOrEmpty(id) ? Context.GetItem<ICompany_Detail_Page>(id)?.Company_Logo?.Src : GlassModel?.Company_Logo?.Src;
+        public string CompanyUrl => !string.IsNullOrEmpty(id) ? Context.GetItem<ICompany_Detail_Page>(id)?.Company_Url?.Url : GlassModel?.Company_Url?.Url;
+        public string CompanyIntroductionText => !string.IsNullOrEmpty(id) ? Context.GetItem<ICompany_Detail_Page>(id)?.Introduction_Text : GlassModel?.Introduction_Text;        
     }
 }
