0d825cc2f236f60ccb8c558996402eb243b25186
SAPIENT\rnar10 <rnarayana2@sapient.com>
2017-02-14 14:42:32 +0530

    IEP-129 As a Sitecore user, I want visibility of all my entitlements so that I can use my subscriptions to their full potential

--- web/Website/Areas/Account/ViewModels/Management/SubscriptionViewModel.cs
+++ web/Website/Areas/Account/ViewModels/Management/SubscriptionViewModel.cs
@@ -4,18 +4,21 @@ using System.Linq;
 using System.Web;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Objects;
 using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages;
+using Informa.Library.User.Entitlement;
 
 namespace Informa.Web.Areas.Account.ViewModels.Management
 {
-	public class SubscriptionViewModel
-	{
-		public string Publication { get; set; }
-		public string Type { get; set; }
-		public DateTime Expiration { get; set; }
-		public bool Renewable { get; set; }
-		public bool Subscribable { get; set; }
+    public class SubscriptionViewModel
+    {
+        public string Publication { get; set; }
+        public string Type { get; set; }
+        public DateTime Expiration { get; set; }
+        public bool Renewable { get; set; }
+        public bool Subscribable { get; set; }
         public IEnumerable<SubscriptionChannelViewModel> ChannelItems { get; set; }
         public bool IsCurrentPublication { get; set; }
+
+        public EntitlementLevel Entitlement_Type { get; set; }
     }
 
     public class SubscriptionChannelViewModel
