0f8ae5b22d8b55934a60bfba5cd8d41c51a36a4f
SAPIENT\rnar10 <rnarayana2@sapient.com>
2017-02-22 14:49:30 +0530

    Info Log for new Salesforce

--- web/Website/App_Start/Registrations/SalesforceRegistrar.cs
+++ web/Website/App_Start/Registrations/SalesforceRegistrar.cs
@@ -16,36 +16,37 @@ using Informa.Library.User.Search;
 
 namespace Informa.Web.App_Start.Registrations
 {
-	public class SalesforceRegistrar
-	{
-		public static void RegisterDependencies(ContainerBuilder builder)
-		{
-			builder.RegisterType<SalesforceErrorLogger>().As<ISalesforceErrorLogger>();
-			builder.RegisterType<SalesforceDebugLogger>().As<ISalesforceDebugLogger>();
-			builder.RegisterType<SalesforceServiceConfiguration>().As<ISalesforceServiceConfiguration>().SingleInstance();
-			builder.RegisterType<SalesforceService>().As<ISalesforceService>();
-			builder.RegisterType<SalesforceServiceContextEnabledChecks>().As<ISalesforceServiceContextEnabledChecks>().InstancePerLifetimeScope();
-			builder.RegisterType<SalesforceServiceContextEnabled>().As<ISalesforceServiceContextEnabled>().InstancePerLifetimeScope();
-			builder.RegisterType<UserAgentServiceContextEnabledCheckConfiguration>().As<IUserAgentServiceContextEnabledCheckConfiguration>().SingleInstance();
-			builder.RegisterType<UserAgentServiceContextEnabledCheck>().As<ISalesforceServiceContextEnabledCheck>().InstancePerLifetimeScope();
-			builder.RegisterType<SalesforceServiceContext>().As<ISalesforceServiceContext>();
-			builder.RegisterType<SalesforceSessionContext>().As<ISalesforceSessionContext>().SingleInstance();
-			builder.RegisterType<SalesforceSessionFactory>().As<ISalesforceSessionFactory>();
-			builder.RegisterType<SalesforceSessionFactoryConfiguration>().As<ISalesforceSessionFactoryConfiguration>();
-			builder.RegisterType<SalesforceSetUserTemporaryPassword>().As<ISalesforceSetUserTemporaryPassword>();
-			builder.RegisterType<SalesforceFindUserProfile>().As<ISalesforceFindUserProfile>();
-			builder.RegisterType<SalesforceRegisterUser>().As<ISalesforceRegisterUser>();
-			builder.RegisterType<SalesforceGetUserEntitlements>().As<ISalesforceGetUserEntitlements>();
-			builder.RegisterType<SalesforceGetIPEntitlements>().As<ISalesforceGetIPEntitlements>();
-			builder.RegisterType<SalesforceSiteTypeParser>()
-				.As<ISalesforceCompanyTypeFromSiteType>()
-				.As<ISalesforceSiteTypeFromCompanyType>()
-				.As<ISalesforceCompanyTypeFromAccountType>();
-			builder.RegisterType<SalesforceEntitlmentFactory>().As<ISalesforceEntitlmentFactory>();
+    public class SalesforceRegistrar
+    {
+        public static void RegisterDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterType<SalesforceInfoLogger>().As<ISalesforceInfoLogger>();
+            builder.RegisterType<SalesforceErrorLogger>().As<ISalesforceErrorLogger>();
+            builder.RegisterType<SalesforceDebugLogger>().As<ISalesforceDebugLogger>();
+            builder.RegisterType<SalesforceServiceConfiguration>().As<ISalesforceServiceConfiguration>().SingleInstance();
+            builder.RegisterType<SalesforceService>().As<ISalesforceService>();
+            builder.RegisterType<SalesforceServiceContextEnabledChecks>().As<ISalesforceServiceContextEnabledChecks>().InstancePerLifetimeScope();
+            builder.RegisterType<SalesforceServiceContextEnabled>().As<ISalesforceServiceContextEnabled>().InstancePerLifetimeScope();
+            builder.RegisterType<UserAgentServiceContextEnabledCheckConfiguration>().As<IUserAgentServiceContextEnabledCheckConfiguration>().SingleInstance();
+            builder.RegisterType<UserAgentServiceContextEnabledCheck>().As<ISalesforceServiceContextEnabledCheck>().InstancePerLifetimeScope();
+            builder.RegisterType<SalesforceServiceContext>().As<ISalesforceServiceContext>();
+            builder.RegisterType<SalesforceSessionContext>().As<ISalesforceSessionContext>().SingleInstance();
+            builder.RegisterType<SalesforceSessionFactory>().As<ISalesforceSessionFactory>();
+            builder.RegisterType<SalesforceSessionFactoryConfiguration>().As<ISalesforceSessionFactoryConfiguration>();
+            builder.RegisterType<SalesforceSetUserTemporaryPassword>().As<ISalesforceSetUserTemporaryPassword>();
+            builder.RegisterType<SalesforceFindUserProfile>().As<ISalesforceFindUserProfile>();
+            builder.RegisterType<SalesforceRegisterUser>().As<ISalesforceRegisterUser>();
+            builder.RegisterType<SalesforceGetUserEntitlements>().As<ISalesforceGetUserEntitlements>();
+            builder.RegisterType<SalesforceGetIPEntitlements>().As<ISalesforceGetIPEntitlements>();
+            builder.RegisterType<SalesforceSiteTypeParser>()
+                .As<ISalesforceCompanyTypeFromSiteType>()
+                .As<ISalesforceSiteTypeFromCompanyType>()
+                .As<ISalesforceCompanyTypeFromAccountType>();
+            builder.RegisterType<SalesforceEntitlmentFactory>().As<ISalesforceEntitlmentFactory>();
             builder.RegisterType<SalesforceEntitlmentFactoryV2>().As<ISalesforceEntitlmentFactoryV2>();
             builder.RegisterType<SalesforceSavedSearchRepository>().As<IUserContentRepository<ISavedSearchEntity>>();
             builder.RegisterType<SalesforceUserOrder>().As<IUserOrder>();
             builder.RegisterType<HttpClientHelper>().As<IHttpClientHelper>();
         }
-	}
+    }
 }
