6ac861586c49d4fdf0e4053ff56cc05d33435adb
SAPIENT\bbisw1 <bbiswal@sapient.com>
2017-02-22 20:23:10 +0530

    ISW-933 Changed Code for Publication hidden and Description will appear in Top

--- web/Website/Areas/Article/Models/Article/AskAnalyst/EmailAnalyst.cs
+++ web/Website/Areas/Article/Models/Article/AskAnalyst/EmailAnalyst.cs
@@ -11,5 +11,6 @@
         public string RecaptchaResponse { get; set; }
         public string CompanyName { get; set; }
         public string PhoneNumber { get; set; }
+        public string PublicationName { get; set; }
     }
 }
