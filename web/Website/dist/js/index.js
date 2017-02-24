d490ddd4087a81eb9059643c74691e70972e4b32
soniaswamy <soniaswamy.guna@gmail.com>
2017-02-24 17:36:36 +0530

    Table variation for Mobile

--- web/Website/dist/js/index.js
+++ web/Website/dist/js/index.js
@@ -7187,10 +7187,11 @@ $(document).on('mouseleave', '.ID-Responsive-Table .R16, .ID-Responsive-Table .R
 
 				$.each(date, function (key, value) {
 					//console.log(key);
+					self.table += '<table class="descView">';
 					self.table += '<thead class="table_head">';
 					self.table += '<tr><th colspan="8" class="pad-full-10">' + key + '</th></tr>';
 					self.table += '<tr class="visible-lg">';
-					console.log(value[0]);
+					//console.log(value[0]);
 					var tableHead = value[0];
 					for (var key in tableHead) {
 						self.table += '<th class="pad-10">' + key + '</th>';
@@ -7207,38 +7208,41 @@ $(document).on('mouseleave', '.ID-Responsive-Table .R16, .ID-Responsive-Table .R
 						self.table += '</tr>';
 					});
 					self.table += '</tbody>';
-					$('.table').html(self.table);
+					self.table += '</table>';
+					$('#dryCargo').html(self.table);
 				});
 			});
 		},
 		renderMobile: function renderMobile() {
 			var self = this;
-			//console.log(tableObj);
 			$.each(tableObj, function (datekey, date) {
 
 				$.each(date, function (key, value) {
 					//console.log(key);
+					self.table += '<table class="mobView">';
+					self.table += '<thead class="table_head">';
+					self.table += '<tr><th colspan="8" class="pad-full-10">' + key + '</th></tr>';
+					self.table += '</thead>';
 					self.table += '<tbody class="visible-sm">';
-					self.table += '<tr>';
+
 					$.each(value, function (objData, objVal) {
-						//console.log(objVal);
-						var tableHead = objVal;
-						for (var key in tableHead) {
-							self.table += '<td class="pad-10 mobleftCol">' + key + '</th>';
-						}
+						//console.log(objVal);						
 						$.each(objVal, function (responseKey, responseVal) {
+							self.table += '<tr>';
+							self.table += '<td class="pad-10 mobleftCol">' + responseKey + '</td>';
 							self.table += '<td class="pad-10 mobrigCol">' + responseVal + '</td>';
+							self.table += '</tr>';
 						});
-						self.table += '</tr>';
-						self.table += '</tbody>';
 					});
-					$('.table').html(self.table);
+					self.table += '</tbody>';
+					self.table += '</table>';
+					$('#dryCargo').html(self.table);
 				});
 			});
 		},
 		init: function init() {
 			this.renderTable();
-			//this.renderMobile();
+			this.renderMobile();
 		}
 	};
 
