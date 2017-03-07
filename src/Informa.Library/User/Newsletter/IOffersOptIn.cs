7c12225dd7f9ce9791d01e6a6f60fc0ac398a1ad
SAPIENT\rnar10 <rnarayana2@sapient.com>
2017-03-07 09:02:36 +0530

    Committing missing files

---
+++ src/Informa.Library/User/Newsletter/IOffersOptIn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informa.Library.User.Newsletter
+{
+    public interface IOffersOptIn
+    {
+        bool OptIn { get; set; }
+        string SalesforceId { get; set; }
+    }
+}
