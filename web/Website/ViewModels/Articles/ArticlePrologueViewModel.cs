e0fc462fd41ebb84c982b7788f283e8da11e4a02
SAPIENT\bbisw1 <bbiswal@sapient.com>
2017-03-08 01:42:52 +0530

    ISW-1024 - Ask the Analyst CTA - On/Off Toggle (Fixed)

--- web/Website/ViewModels/Articles/ArticlePrologueViewModel.cs
+++ web/Website/ViewModels/Articles/ArticlePrologueViewModel.cs
@@ -1,17 +1,27 @@
-﻿using Jabberwocky.Glass.Autofac.Mvc.Models;
+﻿using Informa.Library.Globalization;
+using Informa.Library.Site;
+using Informa.Library.User.Authentication;
+using Jabberwocky.Glass.Autofac.Mvc.Models;
 using Jabberwocky.Glass.Models;
 
 namespace Informa.Web.ViewModels.Articles
 {
 	public class ArticlePrologueViewModel : GlassViewModel<IGlassBase>
 	{
-		public ArticlePrologueViewModel(
+        protected readonly ITextTranslator TextTranslator;
+        protected readonly IAuthenticatedUserContext UserContext;
+        protected readonly ISiteRootContext SiteRootContext;
+
+        public ArticlePrologueViewModel(
 			IArticleProloguePrintViewModel articleProloguePrintViewModel,
 			IArticlePrologueEmailViewModel articlePrologueEmailViewModel,
 			IArticlePrologueBookmarkViewModel articlePrologueBookmarkViewModel,
 			IArticlePrologueShareViewModel articlePrologueShareViewModel,
 			IArticleTagsViewModel articleTagsViewModel,
-            IArticlePrologueATAViewModel articleATAViewModel)
+            IArticlePrologueATAViewModel articleATAViewModel,
+             ISiteRootContext siteRootContext,
+               ITextTranslator textTranslator,
+               IAuthenticatedUserContext userContext)
 		{
 			PrintViewModel = articleProloguePrintViewModel;
 			EmailViewModel = articlePrologueEmailViewModel;
@@ -19,7 +29,10 @@ namespace Informa.Web.ViewModels.Articles
 			ShareViewModel = articlePrologueShareViewModel;
 			ArticleTagsViewModel = articleTagsViewModel;
             ATAViewModel = articleATAViewModel;
-		}
+            SiteRootContext = siteRootContext;
+            TextTranslator = textTranslator;
+            UserContext = userContext;
+        }
 
 		public IArticleProloguePrintViewModel PrintViewModel;
 
@@ -32,5 +45,7 @@ namespace Informa.Web.ViewModels.Articles
 		public IArticlePrologueShareViewModel ShareViewModel;
 
 		public IArticleTagsViewModel ArticleTagsViewModel;
-	}
+
+        public bool IsActiveAskAnalyst => SiteRootContext.Item.Is_Active_Ask_The_Analyst;
+    }
 }
