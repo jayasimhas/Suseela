f5c0ebd58bb8d0141d9ff81d5867fda0286c33be
SAPIENT\ebodkhe <ebodkhe@sapient.com>
2017-04-05 20:14:41 +0530

    CM changes

--- src/Sitecore.SharedSource.DataImporter/Providers/InformaDataMap.cs
+++ src/Sitecore.SharedSource.DataImporter/Providers/InformaDataMap.cs
@@ -1081,35 +1081,59 @@ namespace Sitecore.SharedSource.DataImporter.Providers
 
                     Publication = WebConfigurationManager.AppSettings["ArticlePrefix"].ToString();
                     string hostName = Factory.GetSiteInfo("website")?.HostName ?? WebUtil.GetHostName();
-                    XMLDataLogger.WriteLog("Host Name:" + hostName, "ArticleCleanup");
-                    url = string.Format("http://{0}/api/SearchArticlesbasedonEscenic?articleNumber={1}&EscenicId={2}", hostName, Publication, relatedArticle);
+                  //  XMLDataLogger.WriteLog("Host Name:" + hostName, "ArticleCleanup");
+                    url = string.Format("https://{0}/api/SearchArticlesbasedonEscenic?articleNumber={1}&EscenicId={2}", hostName, Publication, relatedArticle);
+               
+                using (var client = new HttpClient())
+                {
+                    
+                    ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
-                    using (var client = new HttpClient())
+
+                    //XMLDataLogger.WriteLog("HttpClient :", "ArticleCleanup");
+                    //var response;
+                    try
                     {
                         var response = client.GetStringAsync(url).Result;
+
+                       // XMLDataLogger.WriteLog("Response :" + response, "ArticleCleanup");
                         if (!(response == "null" || response == ""))
                         {
                             var resultarticles = JsonConvert.DeserializeObject<ArticleItem>(response);
                             relatedArticles = resultarticles._Id.ToString();
+                            XMLDataLogger.WriteLog("Result Article:" + relatedArticles, "ArticleCleanup");
                             using (new Sitecore.SecurityModel.SecurityDisabler())
-                            { 
+                            {
                                 Item item = MasterDB.GetItem(relatedArticles);
-
+                                XMLDataLogger.WriteLog("Sitecore Item:" + item, "ArticleCleanup");
                                 if (item != null)
                                 {
                                     XMLDataLogger.WriteLog("Article Path:" + item.Paths.ContentPath, "ArticleCleanup");
                                     XMLDataLogger.WriteLog("Article Path:" + item.Name, "ArticleCleanup");
                                     if (WebConfigurationManager.AppSettings["DeleteArticleWithSpecialChar"].Equals("true"))
-                                    {
+                                    {                                     
                                         item.Delete();
+                                        XMLDataLogger.WriteLog(relatedArticle, "ArticleNumberDeleted");
                                     }
                                 }
                             }
 
                         }
+                        else
+                        {
+
+                            XMLDataLogger.WriteLog(relatedArticle, "ArticleNotFoundOnCleanup");
+
+                        }
+
                     }
 
-            }
+                    catch (Exception ex)
+                    {
+                        XMLDataLogger.WriteLog("ex :" + ex.InnerException + ex.Source + ex.StackTrace + ex.Data, "ArticleCleanup");
+                    }
+                }
+                }
 
             return Enumerable.Empty<object>();
 
