97c12ab86c5a460bf342d6c141d03ca57f30825c
Saurabh Sinha <ssi253@sapient.com>
2017-04-10 17:31:19 +0530

    Merges & Acquisition Exception handling

--- web/Website/dist/js/index.js
+++ web/Website/dist/js/index.js
@@ -6548,8 +6548,13 @@ $(function () {
 			MergeAcquistion.init(window.jsonMergeAcquistion, $('.merge-acquistion'));
 		} else {
 			var ErrorMessage = $('#hdnErrormessage').val();
-			if (window.jsonMergeAcquistion.length == 0) {
-				var ErrorMessage = $('#hdnInfomessage').val();
+			if (window.jsonMergeAcquistion) {
+
+				if (window.jsonMergeAcquistion.length == 0) {
+					var ErrorMessage = $('#hdnInfomessage').val();
+				}
+			} else {
+				var ErrorMessage = $('#hdnErrormessage').val();
 			}
 			$('.id-merges-acquisition').html('<div class="alert-error js-form-error js-form-error-PasswordRequirements" style="display: block;">' + '<svg class="alert__icon">' + '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/img/svg-sprite.svg#alert"></use>' + '</svg>' + '<p class="page-account-contact__error">' + ErrorMessage + '</p>' + '</div>');
 		}
