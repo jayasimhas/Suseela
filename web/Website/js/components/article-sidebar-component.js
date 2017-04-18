e422cc9fa5cc61b3ee53706b28fba023948bbfda
SAPIENT\makul1 <makula@sapient.com>
2017-04-18 17:52:48 +0530

    Updated code

--- web/Website/js/components/article-sidebar-component.js
+++ web/Website/js/components/article-sidebar-component.js
@@ -9,6 +9,9 @@ $(document).ready(function () {
     articleSidebarAd = articleSidebarAdParent.find('.advertising');
     lastActionFlagsBar = $('.action-flags-bar:last-of-type');
     sidebarIsTaller = $('.article-right-rail').height() > $('.article-left-rail').height();
+	if($('.article-right-rail').length > 0) {
+        rightRail = $('.article-right-rail').offset().left;
+    }
 });
 $(window).on('scroll', function () {
     if (articleSidebarAdParent && articleSidebarAdParent.length && !sidebarIsTaller) {
@@ -16,6 +19,9 @@ $(window).on('scroll', function () {
         stickyFloor = lastActionFlagsBar.offset().top - window.pageYOffset - articleSidebarAd.height();
         if (articleSidebarAdParent.offset().top - window.pageYOffset <= 16) {
             articleSidebarAdParent.addClass('advertising--sticky');
+			if($('.article-right-rail').length > 0) {
+                articleSidebarAdParent.find('.advertising').css('left', rightRail + 'px');
+            }
         } else {
             articleSidebarAdParent.removeClass('advertising--sticky');
         }
