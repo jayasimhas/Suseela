c12fae55c92b9c7de19dcd4f9cc1d6539547c49d
SAPIENT\bbisw1 <bbiswal@sapient.com>
2017-04-19 23:02:39 +0530

    ISW-1261 - System InvalidOperationException in EmailCompany & EmailDeal - Fixed

--- web/Website/ViewModels/CompaniesAndDeals/CompaniesandDealsPrologueShareViewModel.cs
+++ web/Website/ViewModels/CompaniesAndDeals/CompaniesandDealsPrologueShareViewModel.cs
@@ -10,62 +10,63 @@ using System.Web;
 
 namespace Informa.Web.ViewModels.CompaniesAndDeals
 {
-	public class CompaniesandDealsShareViewModel : GlassViewModel<I___BasePage>
-	{
+    public class CompaniesandDealsShareViewModel : GlassViewModel<I___BasePage>
+    {
 
-		protected readonly ITextTranslator TextTranslator;
-		protected readonly IDCDReader DCDReader;
-		protected readonly IHttpContextProvider HttpContextProvider;
-		protected readonly IAuthenticatedUserContext UserContext;
+        protected readonly ITextTranslator TextTranslator;
+        protected readonly IDCDReader DCDReader;
+        protected readonly IHttpContextProvider HttpContextProvider;
+        protected readonly IAuthenticatedUserContext UserContext;
 
-		public CompaniesandDealsShareViewModel(
-								ITextTranslator textTranslator,
-								IDCDReader dcdReader,
-								IHttpContextProvider context,
-								IAuthenticatedUserContext userContext)
-		{
-			TextTranslator = textTranslator;
-			DCDReader = dcdReader;
-			HttpContextProvider = context;
-			UserContext = userContext;
-            
-			var wildcardValue = UrlUtils.GetLastUrlSement(HttpContextProvider.Current);
-			if (GlassModel._TemplateId.Equals(ICompany_PageConstants.TemplateId.Guid))
-			{
-				var Company = DCDReader.GetCompanyByRecordNumber(wildcardValue);
-				PageTitle = Company.Title;
+        public CompaniesandDealsShareViewModel(
+                                ITextTranslator textTranslator,
+                                IDCDReader dcdReader,
+                                IHttpContextProvider context,
+                                I___BasePage glassModel,
+                                IAuthenticatedUserContext userContext)
+        {
+            TextTranslator = textTranslator;
+            DCDReader = dcdReader;
+            HttpContextProvider = context;
+            UserContext = userContext;
+
+            var wildcardValue = UrlUtils.GetLastUrlSement(HttpContextProvider.Current);
+            if (glassModel._TemplateId.Equals(ICompany_PageConstants.TemplateId.Guid))
+            {
+                var Company = DCDReader.GetCompanyByRecordNumber(wildcardValue);
+                PageTitle = Company.Title;
                 CompanyId = Company.RecordNumber.ToString();
-			}
-			else if (GlassModel._TemplateId.Equals(IDeal_PageConstants.TemplateId.Guid))
-			{
-				var Deal = DCDReader.GetDealByRecordNumber(wildcardValue);
-				PageTitle = Deal.Title;
+            }
+            else if (glassModel._TemplateId.Equals(IDeal_PageConstants.TemplateId.Guid))
+            {
+                var Deal = DCDReader.GetDealByRecordNumber(wildcardValue);
+                PageTitle = Deal.Title;
                 DealId = Deal.RecordNumber.ToString();
-			}
-		}
+            }
+        }
 
-		public string AuthUserEmail => UserContext.User?.Email ?? string.Empty;
-		public string AuthUserName => UserContext.User?.Name ?? string.Empty;
+        public string AuthUserEmail => UserContext.User?.Email ?? string.Empty;
+        public string AuthUserName => UserContext.User?.Name ?? string.Empty;
 
-		public string PageTitle = string.Empty;
+        public string PageTitle = string.Empty;
         public string CompanyId = string.Empty;
         public string DealId = string.Empty;
-		public string PageUrl => $"{HttpContextProvider.Current.Request.Url.Scheme}://{HttpContextProvider.Current.Request.Url.Host}{HttpContextProvider.Current.Request.Url.PathAndQuery}";
-		public string ShareText => TextTranslator.Translate("DCD.Share");
-		public string EmailCompanyText => TextTranslator.Translate("DCD.EmailPopout.EmailCompany");
-		public string EmailDealText => TextTranslator.Translate("DCD.EmailPopout.EmailDeal");
-		public string EmailSentSuccessMessage => TextTranslator.Translate("DCD.EmailPopout.EmailSentSuccessMessage");
-		public string EmailFormInstructionsText => TextTranslator.Translate("DCD.EmailPopout.EmailFormInstructions");
-		public string GeneralError => TextTranslator.Translate("DCD.EmailPopout.GeneralError");
-		public string RecipientEmailPlaceholderText => TextTranslator.Translate("DCD.EmailPopout.RecipientEmailPlaceholder");
-		public string YourNamePlaceholderText => TextTranslator.Translate("DCD.EmailPopout.YourNamePlaceholder");
-		public string YourEmailPlaceholderText => TextTranslator.Translate("DCD.EmailPopout.YourEmailPlaceholder");
-		public string SubjectText => TextTranslator.Translate("DCD.EmailPopout.SubjectText");
-		public string AddMessageText => TextTranslator.Translate("DCD.EmailPopout.AddMessage");
-		public string CancelText => TextTranslator.Translate("DCD.EmailPopout.Cancel");
-		public string SendText => TextTranslator.Translate("DCD.EmailPopout.Send");
-		public string InvalidEmailText => TextTranslator.Translate("DCD.EmailPopout.InvalidEmail");
-		public string EmptyFieldText => TextTranslator.Translate("DCD.EmailPopout.EmptyField");
-		public string NoticeText => TextTranslator.Translate("DCD.EmailPopout.Notice");
-	}
+        public string PageUrl => $"{HttpContextProvider.Current.Request.Url.Scheme}://{HttpContextProvider.Current.Request.Url.Host}{HttpContextProvider.Current.Request.Url.PathAndQuery}";
+        public string ShareText => TextTranslator.Translate("DCD.Share");
+        public string EmailCompanyText => TextTranslator.Translate("DCD.EmailPopout.EmailCompany");
+        public string EmailDealText => TextTranslator.Translate("DCD.EmailPopout.EmailDeal");
+        public string EmailSentSuccessMessage => TextTranslator.Translate("DCD.EmailPopout.EmailSentSuccessMessage");
+        public string EmailFormInstructionsText => TextTranslator.Translate("DCD.EmailPopout.EmailFormInstructions");
+        public string GeneralError => TextTranslator.Translate("DCD.EmailPopout.GeneralError");
+        public string RecipientEmailPlaceholderText => TextTranslator.Translate("DCD.EmailPopout.RecipientEmailPlaceholder");
+        public string YourNamePlaceholderText => TextTranslator.Translate("DCD.EmailPopout.YourNamePlaceholder");
+        public string YourEmailPlaceholderText => TextTranslator.Translate("DCD.EmailPopout.YourEmailPlaceholder");
+        public string SubjectText => TextTranslator.Translate("DCD.EmailPopout.SubjectText");
+        public string AddMessageText => TextTranslator.Translate("DCD.EmailPopout.AddMessage");
+        public string CancelText => TextTranslator.Translate("DCD.EmailPopout.Cancel");
+        public string SendText => TextTranslator.Translate("DCD.EmailPopout.Send");
+        public string InvalidEmailText => TextTranslator.Translate("DCD.EmailPopout.InvalidEmail");
+        public string EmptyFieldText => TextTranslator.Translate("DCD.EmailPopout.EmptyField");
+        public string NoticeText => TextTranslator.Translate("DCD.EmailPopout.Notice");
+    }
 }
