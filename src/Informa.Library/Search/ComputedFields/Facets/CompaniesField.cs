42effe19658c9c848d5606821a4e34ec815a0cfb
avisw1 <aviswanadhan@sapient.com>
2017-04-20 17:56:09 +0530

    Company Search|AV

--- src/Informa.Library/Search/ComputedFields/Facets/CompaniesField.cs
+++ src/Informa.Library/Search/ComputedFields/Facets/CompaniesField.cs
@@ -6,34 +6,66 @@ using Sitecore.Data.Items;
 using Sitecore.Configuration;
 using Sitecore.Data;
 using System.Linq;
+using Informa.Models.Informa.Models.sitecore.templates.User_Defined.Base_Templates;
 
 namespace Informa.Library.Search.ComputedFields.Facets
 {
-	public class CompaniesField : BaseGlassComputedField<IArticle>
-	{
-		public override object GetFieldValue(IArticle indexItem)
-		{
+    public class CompaniesField : BaseGlassComputedField<IArticle>
+    {
+        public override object GetFieldValue(IArticle indexItem)
+        {
             Item rootItem = null;
             var dbContext = Factory.GetDatabase("master");
             Item item = dbContext.GetItem(new ID(indexItem._Id));
 
             if (item != null)
-                rootItem = item.Axes.GetAncestors().FirstOrDefault(ancestor => ancestor.TemplateID.ToString() == Settings.GetSetting("VerticalTemplate.global"));
-            if (rootItem != null && rootItem.Name.Contains("Agri") || rootItem.Name.Contains("Maritime"))
             {
-                if (indexItem?.Taxonomies != null)
+                rootItem = item.Axes.GetAncestors().FirstOrDefault(ancestor => ancestor.TemplateID.ToString() == Settings.GetSetting("VerticalTemplate.global"));
+           
+                if (rootItem != null && rootItem.Name.Contains("Agri") || rootItem.Name.Contains("Maritime"))
                 {
-                    var companiesTaxonomy = indexItem.Taxonomies.Where(x => SearchTaxonomyUtil.IsCompaniesTaxonomy(x._Path));
-                    return companiesTaxonomy.Where(x => !string.IsNullOrEmpty(x.Item_Name)).Select(x => x.Item_Name.Trim()).ToList();
+                    if (indexItem?.Taxonomies != null)
+                    {
+                        var companiesTaxonomy = indexItem.Taxonomies.Where(x => SearchTaxonomyUtil.IsCompaniesTaxonomy(x._Path, rootItem.Name));
+                        var companiestest = companiesTaxonomy.Where(x => !string.IsNullOrEmpty(x.Item_Name)).Select(x => x.Item_Name.Trim()).ToList();
+                        return companiesTaxonomy.Where(x => !string.IsNullOrEmpty(x.Item_Name)).Select(x => x.Item_Name.Trim()).ToList();
+                    }
                 }
-            }
+                else if (string.IsNullOrEmpty(indexItem.Referenced_Companies))
+                {
+                    return new List<string>();
+                }
+                else if (rootItem.Name.Contains("Pharma"))
+                {
+                    return SearchCompanyUtil.GetCompanyNames(indexItem.Referenced_Companies);
+                }
+            }           
+                return new List<string>();
+           
+        }
+    }
 
-            if (string.IsNullOrEmpty(indexItem.Referenced_Companies))
-			{
-				return new List<string>();
-			}
+    //public class CompaniesField : BaseGlassComputedField<I___BaseTaxonomy>
+    //{
+    //    public override object GetFieldValue(I___BaseTaxonomy indexItem)
+    //    {
+    //        Item rootItem = null;
+    //        var dbContext = Factory.GetDatabase("master");
+    //        Item item = dbContext.GetItem(new ID(indexItem._Id));
 
-			return SearchCompanyUtil.GetCompanyNames(indexItem.Referenced_Companies);
-		}
-	}
+    //        if (item != null)
+    //            rootItem = item.Axes.GetAncestors().FirstOrDefault(ancestor => ancestor.TemplateID.ToString() == Settings.GetSetting("VerticalTemplate.global"));
+    //        if (rootItem != null && rootItem.Name.Contains("Agri") || rootItem.Name.Contains("Maritime"))
+    //        {
+    //            if (indexItem?.Taxonomies != null)
+    //            {
+    //                var companyTaxonomyItems = indexItem.Taxonomies.Where(x => SearchTaxonomyUtil.IsCompaniesTaxonomy(x._Path));
+    //                return SearchTaxonomyUtil.GetHierarchicalFacetFieldValue(companyTaxonomyItems);
+    //            }
+    //        }
+    //        return new List<string>();
+
+    //    }
+    //}
+
 }
