68fef7ad20065bb738c5eed21953c213089c0ae2
Anusha <aburli@sapient.com>
2017-05-08 19:09:00 +0530

    IPMP-2537: Fix for workflow state not being applied as soon as an article is created as a non-admin user.

--- src/Informa.Library/CustomSitecore/Events/ArticleItemCreated.cs
+++ src/Informa.Library/CustomSitecore/Events/ArticleItemCreated.cs
@@ -12,25 +12,25 @@ using Informa.Models.Informa.Models.sitecore.templates.System.Workflow;
 
 namespace Informa.Library.CustomSitecore.Events
 {
-	public class ArticleItemCreated
-	{
-		private ISitePublicationWorkflow publicationWorkflow
-		{
-			get
-			{
-				try
-				{
-					return DependencyResolver.Current.GetService<ISitePublicationWorkflow>();
-				}
-				catch
-				{
-					return null;
-				}
-			}
-		}
+    public class ArticleItemCreated
+    {
+        private ISitePublicationWorkflow publicationWorkflow
+        {
+            get
+            {
+                try
+                {
+                    return DependencyResolver.Current.GetService<ISitePublicationWorkflow>();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
 
-		public void Process(object sender, EventArgs args)
-		{
+        public void Process(object sender, EventArgs args)
+        {
 
             if (publicationWorkflow == null)
                 return;
@@ -43,42 +43,43 @@ namespace Informa.Library.CustomSitecore.Events
             if (item == null)
                 return;
             try
-			{
-				
+            {
 
-				//If article save process
-				var itemPath = item.Paths.Path.ToLower();
-				if (itemPath.StartsWith("/sitecore/content")
-						&& !itemPath.Contains("/issues/") // detect if this is an issue clone, since the fields arent set yet on item created
-						&& item.TemplateID.Guid == Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.IArticleConstants.TemplateId.Guid)
-				{
-					changeWorkflowStateAndExecuteActions(item, new ID(publicationWorkflow.GetInitialState(item)._Id), new ID(publicationWorkflow.GetPublicationWorkflow(item)._Id));
-				}
-			}
-			catch (Exception ex)
-			{
+
+                //If article save process
+                var itemPath = item.Paths.Path.ToLower();
+                if (itemPath.StartsWith("/sitecore/content")
+                        && !itemPath.Contains("/issues/") // detect if this is an issue clone, since the fields arent set yet on item created
+                        && item.TemplateID.Guid == Informa.Models.Informa.Models.sitecore.templates.User_Defined.Pages.IArticleConstants.TemplateId.Guid)
+                {
+                    changeWorkflowStateAndExecuteActions(item, new ID(publicationWorkflow.GetInitialState(item)._Id), new ID(publicationWorkflow.GetPublicationWorkflow(item)._Id));
+                }
+            }
+            catch (Exception ex)
+            {
 
                 IState initialState = publicationWorkflow.GetInitialState(item);
 
                 if (initialState != null)
                     changeWorkflowStateAndExecuteActions(item, new ID(initialState._Id), new ID(publicationWorkflow.GetPublicationWorkflow(item)._Id));
 
-				Sitecore.Diagnostics.Log.Fatal(ex.ToString(), this);
+                Sitecore.Diagnostics.Log.Fatal(ex.ToString(), this);
 
-			}
-		}
+            }
+        }
 
-		private static WorkflowResult changeWorkflowStateAndExecuteActions(Item item, ID workflowStateId, ID workflowID)
-		{
-			if (item[FieldIDs.DefaultWorkflow] != workflowID.ToString())
-			{
-				using (new EditContext(item))
-				{
-					item[FieldIDs.DefaultWorkflow] = workflowID.ToString();
-				}
-			}
+        private static WorkflowResult changeWorkflowStateAndExecuteActions(Item item, ID workflowStateId, ID workflowID)
+        {
+            if (item[FieldIDs.DefaultWorkflow] != workflowID.ToString())
+            {
+                using (new EditContext(item))
+                {
+                    item[FieldIDs.DefaultWorkflow] = workflowID.ToString();
+                    item[FieldIDs.State] = workflowStateId.ToString();
+                }
+            }
 
-			return new WorkflowResult(true, "OK", workflowStateId);
-		}
-	}
+            return new WorkflowResult(true, "OK", workflowStateId);
+        }
+    }
 }
